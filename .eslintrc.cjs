258f620c469ee79d67fd05b2126181cc3363d752
Timothy Scheuering <timsch@cisco.com>
2022-10-24 12:51:42 -0400

    feat(config/eslint): modify rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -20,12 +20,14 @@ module.exports = {
   rules: {
     '@typescript-eslint/no-unused-vars': 'error',
     'import/extensions': 'off',
+    'import/no-extraneous-dependencies': ['error', { devDependencies: ['**/config/**/*.*'] }],
     'import/no-relative-packages': 'off',
     'import/prefer-default-export': 'off',
     'no-shadow': 'off',
     'no-unused-vars': 'off',
     'max-len': ['error', { code: 120 }],
-    indent: ['error', 2, { ignoredNodes: ['PropertyDefinition'] }],
+    indent: ['error', 2, { ignoredNodes: ['PropertyDefinition'], SwitchCase: 1 }],
+    'object-curly-newline': 'off',
   },
   settings: {
     'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
