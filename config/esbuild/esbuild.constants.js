ce0f371de1e5511237deb1aeab53672df89471bb
chrisadubois <chdubois@cisco.com>
2022-10-27 13:38:24 -0700

    feat(pkg): fixes #17, fixes #7, fixes #26, fixes #19 (#29)

---
+++ config/esbuild/esbuild.constants.js
@@ -0,0 +1,10 @@
+const PROJECT_PREFIX = 'MomentumDesign';
+
+const CATEGORIES = {
+  iife: 'iife',
+};
+
+export {
+  PROJECT_PREFIX,
+  CATEGORIES,
+};
---
+++ scripts/affected.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+import yargs from 'yargs';
+
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
+const { exec } = require('node:child_process');
+
+const args = yargs(process.argv.slice(2)).options({
+  command: { type: 'string' },
+  ref: { type: 'string' }
+}).parseSync();
+
+exec(`yarn workspaces list --since=${ref || 'upstream/main'} --json`, (error, output) => {
+  if (error) {
+    console.error(error);
+  }
+  console.log(`find affected output:\n${output}`);
+  const result = output
+    .split('\n')
+    .filter((out) => !!out).map((out) => JSON.parse(out).name)
+    .filter((parsed) => parsed.startsWith('@momentum-design')) // should we do the tasks on root as well ?
+    .toString();
+
+  exec(`yarn workspaces foreach --from '{${result}}' run ${args.command}`, (error, output) => {
+    if (error) {
+      console.error(error);
+    }
+    console.log(`run ${args.command} on affected:\n${output}`);
+  });
+});
