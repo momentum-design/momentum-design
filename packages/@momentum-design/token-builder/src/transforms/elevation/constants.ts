7dd36e26d6f2b65d660243592e7050917f7d3b43
Timothy Scheuering <InteractiveTimmy@users.noreply.github.com>
2022-11-17 10:53:10 -0500

    feat: allow custom token transforms (#125)

---
+++ packages/@momentum-design/token-builder/src/transforms/elevation/constants.ts
@@ -0,0 +1,18 @@
+import { CONSTANTS as COMMON_CONSTANTS } from '../../common';
+
+const TARGET_TOKEN_TYPES = [
+  'boxShadow',
+];
+
+const TARGET_TOKEN_VALUE_TYPES = [
+  'innerShadow',
+  'dropShadow',
+];
+
+const CONSTANTS = {
+  NAME: COMMON_CONSTANTS.TRANSFORMS.MD_ELEVATION,
+  TARGET_TOKEN_TYPES,
+  TARGET_TOKEN_VALUE_TYPES,
+};
+
+export default CONSTANTS;
