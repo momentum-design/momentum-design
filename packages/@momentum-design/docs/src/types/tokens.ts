03639c3710c42969c2211a7f6b9ea14e22852bce
Catalin <ctorge@cisco.com>
2022-11-23 13:14:30 +0000

    feat(tokens): refactor types

---
+++ packages/@momentum-design/docs/src/types/tokens.ts
@@ -0,0 +1,15 @@
+export type ColorTokenValue = { value?: string; type?: string; description?: string };
+
+export type UnThemedColorToken = { [key: string]: { [key: string]: ColorTokenValue } };
+
+export type ThemedColorToken = {
+  [key: string]: { light?: { [key: string]: ColorTokenValue }; dark?: { [key: string]: ColorTokenValue } };
+};
+
+export enum TokenType {
+  Color = 'color',
+  Elevation = 'elevation',
+  BorderWidth = 'border-width',
+  BorderRadius = 'border-radius',
+  Spacing = 'spacing'
+}
