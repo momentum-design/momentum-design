32c60cb59c509163c373bab420ba81139e83148f
Christoph Scheffauer <scheffauerc@gmail.com>
2023-11-09 11:53:56 +0100

    docs: added docs to github class

--- packages/@momentum-design/figma-plugin-assets-export/src/ui/models/github.ts
+++ packages/@momentum-design/figma-plugin-assets-export/src/ui/models/github.ts
@@ -5,10 +5,12 @@
 /* eslint-disable no-param-reassign */
 import { Octokit } from '@octokit/core';
 import { createPullRequest } from 'octokit-plugin-create-pull-request';
+import type { Options } from 'octokit-plugin-create-pull-request/dist-types/types';
 import { DEFAULTS } from '../../shared/export-constants';
 
 import type { Asset, AssetChunks, GitSetting, InputSetting, Mode } from '../../shared/types';
 
+// initialize octokit with createPullRequest plugin
 const MyOctokit = Octokit.plugin(createPullRequest);
 
 type Config = {
@@ -41,7 +43,9 @@ class Github {
     mode: 'ADD',
   };
 
-  octokit: any;
+  octokit: Octokit & {
+    createPullRequest: (args_0: Options) => Promise<any>;
+  };
 
   constructor(config: GitSetting, mode?: Mode, inputConfig?: InputSetting) {
     this.config = { git: config, input: inputConfig, mode };
@@ -50,6 +54,10 @@ class Github {
     });
   }
 
+  /**
+   * Validation of the config
+   * @returns error message if config is not valid
+   */
   private configErrors(): string | undefined {
     if (!this.config.git.githubOwner || !this.config.git.gitRepo || !this.config.git.githubPersonalToken) {
       return 'Github configuration is not valid!';
@@ -69,8 +77,8 @@ class Github {
   /**
    * Get data from github using contents api
    */
-  private async getData(octokit: any, path: string) {
-    return octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+  private async getData(path: string) {
+    return this.octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
       owner: this.config.git.githubOwner,
       repo: this.config.git.gitRepo,
       path,
@@ -80,8 +88,8 @@ class Github {
   /**
    * Get data from github using tree api
    */
-  private async getDataTree(octokit: any, sha: string) {
-    return octokit.request('GET /repos/{owner}/{repo}/git/trees/{tree_sha}', {
+  private async getDataTree(sha: string) {
+    return this.octokit.request('GET /repos/{owner}/{repo}/git/trees/{tree_sha}', {
       owner: this.config.git.githubOwner,
       repo: this.config.git.gitRepo,
       tree_sha: sha,
@@ -90,11 +98,14 @@ class Github {
 
   /**
    * Get all file paths from github
-   * @param octokit
-   * @returns
+   *
+   * This is necessary to get all files from the folders to calculate files to be deleted later.
+   * Using the tree api (because contents api does not return more than 1000 files).
+   *
+   * @returns array of file paths
    */
-  private async getAllFilePaths(octokit: any): Promise<Array<string>> {
-    const { data } = await this.getData(octokit, this.config.git.gitRepoFilePath);
+  private async getAllFilePaths(): Promise<Array<string>> {
+    const { data } = await this.getData(this.config.git.gitRepoFilePath);
 
     // get the all files list only from specified folders in the plugin config
     // this means if there are folder changes, like a folder is not used anymore,
@@ -117,12 +128,15 @@ class Github {
     // get all files from the folders using the tree api (because contents api does not return more than 1000 files)
     const promises = pathInformationOfFolders.map(async ({ sha, path: folderPath }) => {
       if (sha && folderPath) {
-        return this.getDataTree(octokit, sha).then(({ data }) => {
+        return this.getDataTree(sha).then(({ data }) => {
+          console.log(data);
           filePaths.push(data.tree.map((file: any) => [folderPath, file.path].join('/')));
           return data;
         });
       }
-      return new Promise<void>((resolve) => { resolve(); });
+      return new Promise<void>((resolve) => {
+        resolve();
+      });
     });
 
     await Promise.all(promises);
@@ -130,6 +144,22 @@ class Github {
     return filePaths.flat();
   }
 
+  /**
+   * Calculate the github changes chunks based on the figma asset chunks
+   *
+   * Github changes chunks are supposed to be in the format:
+   * ```
+   * [
+   *   {
+   *     files: [...]
+   *     commit: 'commitMsg-chunk0'
+   *   },
+   *   ...
+   * ]
+   * ```
+   * @param data - asset chunks (Nested array of Assets)
+   * @returns github changes chunks (right format to pass to octokit)
+   */
   private calculateChangesChunks(data: AssetChunks): ChangesChunks {
     return data?.map((assets, index) => ({
       files: assets.reduce((accum: { [key: string]: string }, cur: Asset) => {
@@ -140,6 +170,12 @@ class Github {
     }));
   }
 
+  /**
+   * Returns an array of paths of files which are changed,
+   * based on the passed in chunks `data`
+   * @param data - asset chunks
+   * @returns array of paths of files which are changed
+   */
   private calculateChangedFiles(data: AssetChunks): ChangedFiles {
     return data?.reduce((accum: Array<string>, cur: Array<Asset>) => {
       cur.forEach((asset) => {
@@ -149,13 +185,25 @@ class Github {
     }, []);
   }
 
+  /**
+   * Generate github chunks of files which should be deleted
+   *
+   * If changedFile (from `changedFiles`) is not in `allExistingFiles`,
+   * it should be deleted (= added to the chunk)
+   *
+   * @param allExistingFiles - all files from github
+   * @param changedFiles - changed files from figma
+   * @param existingChunksLength - existing chunks length to properly index the delete chunks
+   * @returns github chunks of files to be deleted
+   */
   private generateChunksOfFilesWhichShouldBeDeleted(
     allExistingFiles: Array<string>,
     changedFiles: ChangedFiles,
     existingChunksLength: number,
-  ) {
+  ): ChangesChunks {
     const chunksOfFilesToBeDeleted: Array<Array<string>> = [];
     const filesToBeDeleted: Array<string> = [];
+
     // get all files which should be deleted
     allExistingFiles.forEach((file) => {
       if (!changedFiles?.includes(file)) {
@@ -170,6 +218,7 @@ class Github {
 
     return chunksOfFilesToBeDeleted?.map((assets, index) => ({
       files: assets.reduce((accum: { [key: string]: null }, cur: string) => {
+        // null is used to delete the file
         accum[cur] = null;
         return accum;
       }, {}),
@@ -177,35 +226,21 @@ class Github {
     }));
   }
 
-  private async filterChangesChunks(changesChunks: ChangesChunks, existingFiles: Array<string>) {
-    const filteredChangesChunks: ChangesChunks = [];
-    for (const chunk of changesChunks) {
-      const filteredFiles: { [key: string]: string } = {};
-
-      for (const [path, contentOfChangedFile] of Object.entries(chunk.files)) {
-        if (existingFiles.includes(path)) {
-          const { data } = await this.getData(this.octokit, path);
-          // use atob, since it is running in the browser
-          const contentOfExistingFile = atob(data.content);
-          if (contentOfChangedFile !== contentOfExistingFile) {
-            filteredFiles[path] = contentOfChangedFile;
-          }
-        } else {
-          filteredFiles[path] = contentOfChangedFile;
-        }
-      }
-      if (Object.keys(filteredFiles).length) {
-        filteredChangesChunks.push({
-          files: filteredFiles,
-          commit: chunk.commit,
-        });
-      }
-    }
-
-    return filteredChangesChunks;
-  }
-
-  public async generateChanges(data: AssetChunks | undefined) {
+  /**
+   * Generate changes for github
+   *
+   * Function to generate the changes chunks before sending to github.
+   *
+   * Mode is taken in consideration:
+   *
+   * - Mode ADD: Only add the new files
+   * - Mode DELETE_ONLY: Only delete the files which are not in Figma anymore
+   * - Mode FULL_SYNC: Add new files and delete the files which are not in Figma anymore
+   *
+   * @param data - data from Figma
+   * @returns github changes chunks (in the right format to pass to octokit)
+   */
+  public async generateChanges(data: AssetChunks | undefined): Promise<ChangesChunks> {
     const configErrors = this.configErrors();
 
     if (configErrors) {
@@ -217,10 +252,9 @@ class Github {
     }
 
     const allChangedFiles = this.calculateChangedFiles(data);
-    const allExistingFiles = await this.getAllFilePaths(this.octokit);
+    const allExistingFiles = await this.getAllFilePaths();
 
     const changesChunks = this.calculateChangesChunks(data);
-    // const filteredChangesChunks = await this.filterChangesChunks(changesChunks, allExistingFiles);
 
     const chunksLength = changesChunks?.length;
 
@@ -232,7 +266,7 @@ class Github {
 
     const cummulatedChanges = [...changesChunks, ...chunksOfFilesToBeDeleted];
 
-    if (!changesChunks.length && !chunksOfFilesToBeDeleted.length) {
+    if (!cummulatedChanges.length) {
       return Promise.reject(new Error('No changes found!'));
     }
 
@@ -243,12 +277,17 @@ class Github {
         return chunksOfFilesToBeDeleted;
       case 'FULL_SYNC':
         return cummulatedChanges;
-
       default:
         return changesChunks;
     }
   }
 
+  /**
+   * Create a pull request on github, using the config and changes passed in
+   *
+   * @param changes - changes chunks to be sent to github
+   * @returns - Promise
+   */
   public async pullRequest(changes: ChangesChunks) {
     return this.octokit.createPullRequest({
       owner: this.config.git.githubOwner,
