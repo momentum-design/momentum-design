b2a87a16976c370b2947c46531f2ee654ae07224
kavi <101201430+kaviarasanJV@users.noreply.github.com>
2024-03-07 14:51:54 +0530

    test: jest-unit-test-cases-transformers svg glyphs transformer unit test

---
+++ packages/@momentum-design/builder/src/assets/builder/transformer/svg-glyphs-transformer.unit.test.ts
@@ -0,0 +1,96 @@
+import path from 'path';
+import { mockSVGFontBuffer } from '../../../test/fixtures/transformer.fixtures';
+import type { Formats } from '../types';
+import SvgGlyphsTransformer from './svg-glyphs-transformer';
+import Transformer from './transformer';
+import * as Utils from '../utils';
+
+const inputFilesData = { 0:
+{
+  name: 'font',
+  srcPath: 'font',
+  codepoint: '61697',
+  codepointHexa: 'f101',
+  unicode: 'testUnicode',
+} };
+describe('@momentum-design/builder - SVG glyphs Font Transformer', () => {
+  let transformer: SvgGlyphsTransformer;
+  const FONT_NAME = 'MyFont';
+  const FORMAT: Formats = { config: { fileName: FONT_NAME }, type: 'SVG_GLYPHS' };
+  beforeEach(() => {
+    transformer = new SvgGlyphsTransformer(FORMAT, '/dist');
+    // @ts-ignore
+    jest.spyOn(transformer.logger, 'debug').mockImplementation(() => { });
+  });
+
+  describe('constructor()', () => {
+    it('should extend Builder', () => {
+      expect(transformer instanceof Transformer).toBe(true);
+    });
+
+    it('should mount the format provided to the class object', () => {
+      expect(transformer.format).toBe(FORMAT);
+      expect(transformer.destination).toBe('/dist');
+    });
+  });
+
+  describe('transformFilesSync function in svg-glyphs-transformer', () => {
+    it('should mock the transformFilesSync function and track its usage', () => {
+      transformer.inputFiles = [{ srcPath: 'font', distPath: 'font', data: mockSVGFontBuffer }];
+      const transformFilesSyncSpy = jest.spyOn(transformer, 'transformFilesSync');
+      transformer.transformFilesSync();
+      expect(transformFilesSyncSpy).toHaveBeenCalledTimes(1);
+      expect(transformFilesSyncSpy).toHaveBeenCalled();
+      expect(transformer.outputFiles).toEqual([
+        {
+          data: expect.any(String),
+          distPath: path.join('/dist', 'MyFont'),
+          srcPath: '',
+        },
+      ]);
+      transformFilesSyncSpy.mockRestore();
+    });
+  });
+
+  describe('getNextCodePoint function testing sample 1', () => {
+    it('getNextCodePoint function testing', () => {
+      const getNextCodepointSpy = jest.spyOn(Utils, 'getNextCodepoint');
+      const resultCodepoint = Utils.getNextCodepoint();
+      expect(getNextCodepointSpy).toBeCalledTimes(1);
+      expect(resultCodepoint).toBe(61698);
+    });
+
+    it('getNextCodePoint function testing sample 2', () => {
+      Utils.getNextCodepoint();
+      Utils.getNextCodepoint();
+      Utils.getNextCodepoint();
+      const getNextCodepointmockedfnData = jest.fn(Utils.getNextCodepoint).mockReturnValue(61900);
+      expect(getNextCodepointmockedfnData()).toBe(61900);
+      const getNextCodepointSpy = jest.spyOn(Utils, 'getNextCodepoint');
+      expect(getNextCodepointSpy).toBeCalledTimes(4);
+      expect(Utils.getNextCodepoint()).toBe(61702);
+    });
+    it('should return the correct data from the promise of transformFilesSync() function', () => {
+      transformer.inputFiles = [{
+        srcPath: 'font',
+        distPath: 'font',
+        data: inputFilesData,
+      }];
+      const getNextCodepointmockedfn = jest.fn(Utils.getNextCodepoint).mockReturnValue(556);
+      expect(getNextCodepointmockedfn()).toBe(556);
+      const getNextCodepointSpy = jest.spyOn(Utils, 'getNextCodepoint');
+      Utils.getNextCodepoint();
+      const transformFilesSyncSpy = jest.spyOn(transformer, 'transformFilesSync');
+      transformer.transformFilesSync();
+      expect(transformFilesSyncSpy).toBeCalledTimes(1);
+      expect(getNextCodepointSpy).toHaveBeenCalled();
+      expect(transformer.outputFiles).toStrictEqual([
+        {
+          distPath: path.join('/dist', 'MyFont'),
+          srcPath: '',
+          data: expect.any(String),
+        },
+      ]);
+    });
+  });
+});
