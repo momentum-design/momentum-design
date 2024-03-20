ee9c7bebec32677b335104962f4895774c01c72e
bsengar <bsengar@cisco.com>
2024-03-20 11:06:38 +0530

    test: unit test case for swift transformer

---
+++ packages/@momentum-design/builder/src/assets/builder/transformer/swift-transformer.unit.test.ts
@@ -0,0 +1,59 @@
+import path from 'path';
+import type { Formats, SwiftFormat } from '../types';
+import Transformer from './transformer';
+import SwiftTransformer from './swift-transformer';
+import * as Utils from '../utils';
+
+const inputFilesData = { 0: {
+  name: 'accessibility-bold',
+  srcPath: 'testPath',
+  codepoint: '61697',
+  codepointHexa: 'f101',
+  unicode: 'testUnicode' } };
+describe('@momentum-design/builder - swift Transformer', () => {
+  let transformer: SwiftTransformer;
+  const FORMAT: Formats = { config: { fileName: 'MyFont', hbsPath: 'hpath' }, type: 'SWIFT' } as SwiftFormat;
+
+  beforeEach(() => {
+    transformer = new SwiftTransformer(FORMAT, '/dist');
+    // @ts-ignore
+    jest.spyOn(transformer.logger, 'debug').mockImplementation(() => { });
+  });
+
+  describe('constructor()', () => {
+    it('should extend Builder', () => {
+      expect(transformer instanceof Transformer).toBe(true);
+    });
+
+    it('should mount the format provided to the class object', () => {
+      expect(transformer.format).toBe(FORMAT);
+      expect(transformer.destination).toBe('/dist');
+    });
+  });
+
+  describe('swift transformer - transformFilesAsync function', () => {
+    it('should return the correct data from the promise of transformFilesAsync() function', async () => {
+      transformer.inputFiles = [{
+        srcPath: 'font',
+        distPath: 'font',
+        data: JSON.stringify(inputFilesData),
+      }];
+      const transformFilesAsyncSpy = jest.spyOn(transformer, 'transformFilesAsync');
+      const templateSpy = jest.fn(({ glyphsData }) => glyphsData);
+      const transformHbsSpy = jest.spyOn(Utils, 'transformHbs').mockReturnValue(
+        new Promise((resolve) => { resolve(templateSpy); }),
+      );
+      await transformer.transformFilesAsync();
+      expect(transformFilesAsyncSpy).toBeCalledTimes(1);
+      expect(transformHbsSpy).toBeCalledTimes(1);
+      expect(transformer.outputFiles).toStrictEqual([
+        {
+          data: [inputFilesData[0]],
+          distPath: path.join('/dist', 'MyFont'),
+          srcPath: '',
+        },
+      ]);
+      transformFilesAsyncSpy.mockRestore();
+    });
+  });
+});
