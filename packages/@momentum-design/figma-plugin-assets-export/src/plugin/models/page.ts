1fc6a9bc965f312f3bc24b2cb80de8452b6b5577
ggovindh <ggovindh@cisco.com>
2024-04-05 10:37:07 +0530

    feat: updated figma plugin

--- packages/@momentum-design/figma-plugin-assets-export/src/plugin/models/page.ts
+++ packages/@momentum-design/figma-plugin-assets-export/src/plugin/models/page.ts
@@ -54,7 +54,6 @@ class Page {
     const componentNodes = this.node.findAllWithCriteria({
       types: CONSTANTS.SEARCH_CRITERIA,
     });
-
     const filteredComponents = this.excludeComponents(componentNodes);
     // return component instances:
     return filteredComponents.map((node: ComponentNode) => new Component(node, this.destination, this.assetSetting));
