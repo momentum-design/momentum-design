1b1095cf97af180b56e1d6b522a60452a152a167
Bhanu Pratap Singh <31345924+bhanusengar@users.noreply.github.com>
2024-04-08 14:53:43 +0530

    fix: git release history (#560)
    * fix: git release history
    
    * fix: changes updated
    
    ---------
    
    Co-authored-by: bsengar <bsengar@cisco.com>
    Co-authored-by: Christoph Scheffauer <scheffauerc@gmail.com>

--- packages/@momentum-design/automation/src/utils/git/constants.ts
+++ packages/@momentum-design/automation/src/utils/git/constants.ts
@@ -1,15 +1,16 @@
 const MAX_COMMITS = 10;
 const COMMIT_INDEX_OFFSET = 1;
+const COMMITS_API_URL = 'https://api.github.com/repos/momentum-design/momentum-design/commits';
 
 const FORMAT = {
   commit: '%H',
-  subject: '%s',
 };
 
 const CONSTANTS = {
   COMMIT_INDEX_OFFSET,
   FORMAT,
   MAX_COMMITS,
+  COMMITS_API_URL,
 };
 
 export default CONSTANTS;
