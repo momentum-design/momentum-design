b26da98d3c7f2a34d9a0deda0a15c0469949d1f7
Gaurav Chauhan <gchauha2@cisco.com>
2024-04-16 14:46:42 +0530

    feat: component file test cases

--- packages/@momentum-design/figma-plugin-assets-export/src/plugin/models/component.unit.test.ts
+++ packages/@momentum-design/figma-plugin-assets-export/src/plugin/models/component.unit.test.ts
@@ -1,11 +1,20 @@
 /* eslint-disable no-undef */
 import { AssetSetting } from '../../shared/types';
 import Component from './component';
-import { assetSettingMock, componentNodeMock } from './fixtures/component.fixture';
+import {
+  assetSettingMock,
+  componentNodeMock,
+  replacementMapMock,
+  componentNodePathsCases,
+  assetNamePathsCases,
+  componentNodeColorCheckMock,
+} from './fixtures/component.fixture';
+import { updateObjectByPaths } from './fixtures/utils/updateObjectByPaths';
+
+const DESTINATION = 'dist';
 
 describe('@momentum-design/figma-plugin-assets-export - models.Component', () => {
   let component: Component;
-  const DESTINATION = 'dist';
   beforeEach(() => {
     component = new Component(
         componentNodeMock as unknown as ComponentNode,
@@ -23,9 +32,80 @@ describe('@momentum-design/figma-plugin-assets-export - models.Component', () =>
     });
   });
 
-  describe('convertDigitToWord', () => {
-    it('return word for the digit provided', () => {
-      expect(true).toBe(true);
+  describe('replacementMap', () => {
+    it('return approriate replacementMap', () => {
+      component = new Component(
+        componentNodeMock as unknown as ComponentNode,
+        'dist',
+        assetSettingMock as AssetSetting,
+      );
+      expect(component.replacementMap).toEqual(replacementMapMock);
     });
   });
+
+  describe('assetName', () => {
+    it('return assetName without color black, color black will be replaced with blank', () => {
+      component = new Component(
+        componentNodeColorCheckMock as unknown as ComponentNode,
+        'dist',
+        assetSettingMock as AssetSetting,
+      );
+      expect(component.assetName).toEqual('display-sf-rtl-bold-state-twofour.svg');
+    });
+  });
+  describe('asset', () => {
+    it('check the appropriate result', async () => {
+      componentNodeMock.exportAsync.mockImplementation(() => Promise.resolve({
+        path: `${DESTINATION}/display-sf-rtl-bold-state-twofour.svg`,
+        data: String.fromCharCode.apply(null, new Uint8Array(8) as any),
+      }));
+      component = new Component(
+        componentNodeMock as unknown as ComponentNode,
+        DESTINATION,
+        assetSettingMock as AssetSetting,
+      );
+      const assetData = await component.asset;
+      expect(assetData).toEqual({ path: `${DESTINATION}/display-sf-rtl-bold-state-twofour-blue.svg`, data: '' });
+    });
+    it('check the failure', async () => {
+      const figmaNotifySpy = jest.spyOn(figma, 'notify');
+      componentNodeMock.exportAsync.mockRejectedValue('exportAsync failure');
+      component = new Component(
+        componentNodeMock as unknown as ComponentNode,
+        DESTINATION,
+        assetSettingMock as AssetSetting,
+      );
+      try {
+        await component.asset;
+      } catch (err) {
+        expect(figmaNotifySpy).toHaveBeenCalled();
+        expect(err).toEqual('exportAsync failure');
+      }
+    });
+  });
+});
+test.each(componentNodePathsCases.map(({ path, value, expected }) => {
+  const componentNodeMockCopy = JSON.parse(JSON.stringify(componentNodeMock));
+  const updatedComponentNodeMock = updateObjectByPaths(componentNodeMockCopy, path, value);
+  const component = new Component(
+    updatedComponentNodeMock as unknown as ComponentNode,
+    DESTINATION,
+    assetSettingMock as AssetSetting,
+  );
+  return { component, expected };
+}))('@momentum-design/figma-plugin-assets-export - replacementMap conditions checks', ({ expected, component }) => {
+  expect(component.replacementMap[expected.key]).toEqual(expected.value);
+});
+
+test.each(assetNamePathsCases.map(({ path, value, expected }) => {
+  const assetSettingMockCopy = JSON.parse(JSON.stringify(assetSettingMock));
+  const updatedAssetSettingMock = updateObjectByPaths(assetSettingMockCopy, path, value);
+  const component = new Component(
+    componentNodeMock as unknown as ComponentNode,
+    DESTINATION,
+    updatedAssetSettingMock as AssetSetting,
+  );
+  return { component, expected };
+}))('@momentum-design/figma-plugin-assets-export - asset condition checks', ({ expected, component }) => {
+  expect(component.assetName).toEqual(expected);
 });
