314d98d18ab77036d6ce29a9a401f85a5b523183
Christoph Scheffauer <scheffauerc@gmail.com>
2024-08-06 21:15:57 +0200

    feat: add ios handlebars format to tokens

--- packages/@momentum-design/token-builder/src/common/types.ts
+++ packages/@momentum-design/token-builder/src/common/types.ts
@@ -34,6 +34,7 @@ export interface Config {
   transforms?: Array<Transform>;
   schemaFiles?: Array<string>;
   strict?: boolean;
+  iosHandlebarsPath?: string;
 }
 
 export type SchemaMap = {fileName: string, jsonSchema: SomeJSONSchema}[]
