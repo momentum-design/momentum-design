4fcfab40a033ca6162cc9131424b8d9041596e82
arjunkesava <arjun.kesava@gmail.com>
2024-08-06 18:47:12 +0530

    feat(cmd): add support for dry run mode to increment packages

--- packages/@momentum-design/automation/src/commands/increment-packages/constants.ts
+++ packages/@momentum-design/automation/src/commands/increment-packages/constants.ts
@@ -4,6 +4,10 @@ import GetPackages from '../get-packages';
 
 const OPTIONS: CommandOptions = {
   ...GetPackages.CONSTANTS.OPTIONS,
+  dryRun: {
+    describe: 'dry run mode, does not write changes to disk',
+    type: 'boolean',
+  },
   step: {
     default: [0, 0, 1],
     describe: 'values to increment the version of the filtered packages by: [major, minor, patch]',
