7803b6e9b25ffc18d3df64ea7152b28b4b55d37e
Momentum Design Automation <automation@momentum.design>
2024-08-15 15:24:07 +0000

    ci(docs): deploy documentation [skip ci] [skip release]

--- docs/chunks/jsx-runtime.40f9d82a.js
+++
@@ -1,9 +0,0 @@
-import{r as u}from"./index.c4a0e63f.js";const f={English:"en",Spanish:"es"};Object.values(f);const L={indexName:{BASE_URL:"/momentum-design/",MODE:"production",DEV:!1,PROD:!0}.ALGOLIA_INDEX_NAME,appId:{BASE_URL:"/momentum-design/",MODE:"production",DEV:!1,PROD:!0}.ALGOLIA_APP_ID,apiKey:{BASE_URL:"/momentum-design/",MODE:"production",DEV:!1,PROD:!0}.ALGOLIA_DOC_SEARCH};var m={exports:{}},n={};/**
- * @license React
- * react-jsx-runtime.production.min.js
- *
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */var O=u.exports,E=Symbol.for("react.element"),a=Symbol.for("react.fragment"),l=Object.prototype.hasOwnProperty,A=O.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,d={key:!0,ref:!0,__self:!0,__source:!0};function p(t,e,_){var r,o={},s=null,i=null;_!==void 0&&(s=""+_),e.key!==void 0&&(s=""+e.key),e.ref!==void 0&&(i=e.ref);for(r in e)l.call(e,r)&&!d.hasOwnProperty(r)&&(o[r]=e[r]);if(t&&t.defaultProps)for(r in e=t.defaultProps,e)o[r]===void 0&&(o[r]=e[r]);return{$$typeof:E,type:t,key:s,ref:i,props:o,_owner:A.current}}n.Fragment=a;n.jsx=p;n.jsxs=p;(function(t){t.exports=n})(m);export{L as A,f as K,m as j};
---
+++ docs/chunks/jsx-runtime.d8c51b86.js
@@ -0,0 +1,9 @@
+import{r as _}from"./index.c4a0e63f.js";const a={English:"en",Spanish:"es"};Object.values(a);const x={indexName:"momentum-design-en",appId:"CDACQGZ0NC",apiKey:"c93fb9e74cc3eb4784625d4b2e3dd6f7"};var c={exports:{}},n={};/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var l=_.exports,m=Symbol.for("react.element"),u=Symbol.for("react.fragment"),d=Object.prototype.hasOwnProperty,y=l.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,O={key:!0,ref:!0,__self:!0,__source:!0};function i(t,e,p){var r,o={},s=null,f=null;p!==void 0&&(s=""+p),e.key!==void 0&&(s=""+e.key),e.ref!==void 0&&(f=e.ref);for(r in e)d.call(e,r)&&!O.hasOwnProperty(r)&&(o[r]=e[r]);if(t&&t.defaultProps)for(r in e=t.defaultProps,e)o[r]===void 0&&(o[r]=e[r]);return{$$typeof:m,type:t,key:s,ref:f,props:o,_owner:y.current}}n.Fragment=u;n.jsx=i;n.jsxs=i;(function(t){t.exports=n})(c);export{x as A,a as K,c as j};
