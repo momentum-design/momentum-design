a789a451be736d10b4e997109a98ab25d01b2484
Christoph Scheffauer <scheffauerc@gmail.com>
2024-08-20 21:51:27 +0200

    fix(docs): docs icons & illustrations import (#765)
    fix: docs icons & illustrations import

--- packages/@momentum-design/docs/astro.config.mjs
+++ packages/@momentum-design/docs/astro.config.mjs
@@ -3,13 +3,17 @@ import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import react from '@astrojs/react';
 import copy from 'rollup-plugin-copy';
+import { fileURLToPath } from 'url';
 // https://astro.build/config
 import mdx from '@astrojs/mdx';
 import path from 'path';
 
-// `import.meta.resolve` provides accurate, stable paths for the icons and illustrations packages, ensuring reliable path resolution for tasks like copying files, compared to using relative paths.
-const iconsDistFolder = path.dirname(import.meta.resolve('@momentum-design/icons/dist/manifest.json'));
-const illustrationsDistFolder = path.dirname(import.meta.resolve('@momentum-design/illustrations/dist/manifest.json'));
+// `import.meta.resolve` provides accurate, stable paths for the icons and illustrations packages,
+// ensuring reliable path resolution for tasks like copying files, compared to using relative paths.
+const getAssetsFolder = (manifestEntryPoint) => path.dirname(fileURLToPath(import.meta.resolve(manifestEntryPoint)));
+
+const iconsDistFolder = getAssetsFolder('@momentum-design/icons/dist/manifest.json');
+const illustrationsDistFolder = getAssetsFolder('@momentum-design/illustrations/dist/manifest.json');
 
 // https://astro.build/config
 export default defineConfig({
