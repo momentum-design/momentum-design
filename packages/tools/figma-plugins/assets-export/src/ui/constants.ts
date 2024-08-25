e802ff592501e3d56d8251fb175b00376566267a
Deeya Upadhyay <58636958+2020Deeya@users.noreply.github.com>
2024-08-25 23:42:19 +0530

    feat(figma): Restructure figma plugin assets export package (#771)
    * feat: migrate asset-export
    
    Signed-off-by: Deeya <dishuupadhyay01@gmail.com>
    
    * fix: minor path fix
    
    * fix: renamed folder
    
    Signed-off-by: Deeya <dishuupadhyay01@gmail.com>
    
    * fix: minor update
    
    Signed-off-by: Deeya <dishuupadhyay01@gmail.com>
    
    ---------
    
    Signed-off-by: Deeya <dishuupadhyay01@gmail.com>
    Co-authored-by: Christoph Scheffauer <scheffauerc@gmail.com>

---
+++ packages/tools/figma-plugins/assets-export/src/ui/constants.ts
@@ -0,0 +1,11 @@
+const URLS = {
+  MOMENTUM_BASE: 'https://github.com/momentum-design/momentum-design/tree/main/packages/tools/figma-plugins',
+};
+
+const CONSTANTS = {
+  URLS,
+};
+
+export {
+  CONSTANTS,
+};
