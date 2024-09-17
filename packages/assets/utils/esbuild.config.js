5e465d6e4edb5f86294d12a5d4018a43f6e31313
Supriya M <sansup49@gmail.com>
2024-09-17 17:49:21 +0530

    fix: changed cjs to esm

--- packages/assets/utils/esbuild.config.js
+++ packages/assets/utils/esbuild.config.js
@@ -1,4 +1,4 @@
-const { cjs } = require("../../../config/esbuild/esbuild.config.js");
 const { join } = require('path');
+const { esm } = require('../../../config/esbuild/esbuild.config.js');
 
-cjs(`${join(process.cwd())}`, "src/index.ts", "index.js", "tsconfig.json", "browser");
+esm(`${join(process.cwd())}`, 'src/index.ts', 'index.js', 'tsconfig.json', 'browser');
