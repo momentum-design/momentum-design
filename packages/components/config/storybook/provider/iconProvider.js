e8162c15305187e2fc417b53c2ce8aeedd78b4b0
Deeya <dishuupadhyay01@gmail.com>
2024-09-24 20:16:20 +0530

    resolve pending relative paths

--- packages/components/config/storybook/provider/iconProvider.js
+++ packages/components/config/storybook/provider/iconProvider.js
@@ -2,7 +2,7 @@ import { html } from 'lit';
 
 export const withIconProvider = (story) =>
   html`<mdc-iconprovider
-        url="/icons/svg"
+        url="./icons/svg"
       >
         ${story()}
       </mdc-iconprovider>`;
