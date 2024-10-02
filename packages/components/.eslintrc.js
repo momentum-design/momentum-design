532ef02c10d2cade89d1a8b5113ad9a42d7052ec
Christoph Scheffauer <scheffauerc@gmail.com>
2024-10-02 11:10:10 +0200

    fix: adding output format react to components (#862)
    * feat: adding output format react to components
    
    * docs: updated comments
    
    * fix: install eslint in components package

--- packages/components/.eslintrc.js
+++ packages/components/.eslintrc.js
@@ -3,12 +3,14 @@ const config = require('../../.eslintrc.js');
 module.exports = {
   ...config,
   extends: [...config.extends, 'plugin:lit/recommended'],
-  parserOptions: { project: './tsconfig.json' },
+  parserOptions: { ...config.parserOptions, project: ['./tsconfig.json', './src/tsconfig.json'], projectService: true },
   rules: {
     ...config.rules,
     '@typescript-eslint/no-floating-promises': 'error',
     'no-redeclare': 'off',
     'implicit-arrow-linebreak': 'off',
     'import/no-unresolved': ['error', { ignore: ['dist'] }],
+    'json/*': 'off',
   },
+  ignorePatterns: [...config.ignorePatterns, 'prettier.config.js', '*.png', '*.css'],
 };
