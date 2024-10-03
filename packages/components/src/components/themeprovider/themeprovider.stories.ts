a3684f9752f55e6e9a756f15a26df663992f0301
Christoph Scheffauer <scheffauerc@gmail.com>
2024-10-03 09:32:44 +0200

    docs: component class style (#865)

--- packages/components/src/components/themeprovider/themeprovider.stories.ts
+++ packages/components/src/components/themeprovider/themeprovider.stories.ts
@@ -1,12 +1,18 @@
 import type { Meta, StoryObj, Args } from '@storybook/web-components';
 import { html } from 'lit';
+import { classMap } from 'lit/directives/class-map.js';
 import '.';
 import './themeprovider.stories.styles.css';
 import { THEMES } from './themeprovider.constants';
 import './themeprovider.stories.utils';
+import { classArgType, styleArgType } from '../../../config/storybook/commonArgTypes';
 
 const render = (args: Args) => html`
-  <mdc-themeprovider class="themeWrapper" theme="${args.theme}">
+  <mdc-themeprovider
+    class="${classMap({ themeWrapper: true, [args.class]: true })}"
+    style="${args.style}"
+    theme="${args.theme}"
+  >
     <p>Current theme:</p>
     <mdc-subcomponent></mdc-subcomponent>
     <div>
@@ -26,6 +32,21 @@ const meta: Meta = {
     theme: {
       options: THEMES,
       control: { type: 'radio' },
+      table: {
+        category: 'Attributes',
+      },
+    },
+    themes: {
+      table: {
+        category: 'Attributes',
+      },
+    },
+    ...classArgType,
+    ...styleArgType,
+  },
+  parameters: {
+    controls: {
+      exclude: ['Context'],
     },
   },
 };
@@ -35,5 +56,7 @@ export default meta;
 export const Primary: StoryObj = {
   args: {
     theme: THEMES[0],
+    class: 'custom-classname',
+    style: 'margin-top: 20px;',
   },
 };
