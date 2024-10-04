459c398d92609f524c2599fec73dff19a11835cb
Christoph Scheffauer <scheffauerc@gmail.com>
2024-10-04 18:09:41 +0200

    refactor: themeprovider changes to get it finished (#869)
    * refactor: themeprovider according to tech breakdown
    
    * fix: analyze

--- packages/components/config/playwright/setup/types.ts
+++ packages/components/config/playwright/setup/types.ts
@@ -1,5 +1,8 @@
 import { Locator, PageScreenshotOptions } from '@playwright/test';
+import CONSTANTS from './constants';
 
 export type ScreenShotOptions = PageScreenshotOptions & {
   element?: Locator;
 };
+
+export type ThemeClass = typeof CONSTANTS.THEME_CLASSES[keyof typeof CONSTANTS.THEME_CLASSES];
