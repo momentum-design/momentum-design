a30b159e132ef5da771373baf2da899dfbe32028
Arjun Kesava P <arjun.kesava@gmail.com>
2024-10-09 11:43:22 +0530

    chore(refactor): convert fetch promise to async await pattern for readability

--- packages/components/src/components/icon/icon.utils.ts
+++ packages/components/src/components/icon/icon.utils.ts
@@ -1,13 +1,14 @@
 /* eslint-disable implicit-arrow-linebreak */
 
-const dynamicSVGImport = async (url: string, name: string, fileExtension: string): Promise<Element> =>
-  fetch(`${url}/${name}.${fileExtension}`)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error('There was a problem while fetching the icon!');
-      }
-      return response.text();
-    })
-    .then((iconResponse) => new DOMParser().parseFromString(iconResponse, 'text/html').body.children[0]);
+const dynamicSVGImport = async (url: string, name: string, fileExtension: string): Promise<Element> => {
+  const response = await fetch(`${url}/${name}.${fileExtension}`);
+
+  if (!response.ok) {
+    throw new Error('There was a problem while fetching the icon!');
+  }
+
+  const iconResponse = await response.text();
+  return new DOMParser().parseFromString(iconResponse, 'text/html').body.children[0];
+};
 
 export { dynamicSVGImport };
