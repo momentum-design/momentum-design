d495bb75c5608400ddb9c1e2c10a73de633d5b4a
Arjun Kesava P <arjun.kesava@gmail.com>
2024-10-10 14:31:59 +0530

    chore(docs): add tsdoc for dynamic svg import method

--- packages/components/src/components/icon/icon.utils.ts
+++ packages/components/src/components/icon/icon.utils.ts
@@ -1,5 +1,15 @@
 /* eslint-disable implicit-arrow-linebreak */
 
+/**
+ * Fetches a dynamic SVG icon based on the provided `url`, `name` and `fileExtension`.
+ * It will throw an error if the response is not ok.
+ *
+ * @param url - The base url of the icon
+ * @param name - The name of the icon
+ * @param fileExtension - The file extension of the icon
+ * @returns The valid icon element
+ * @throws Error if the response is not ok
+ */
 const dynamicSVGImport = async (url: string, name: string, fileExtension: string): Promise<Element> => {
   const response = await fetch(`${url}/${name}.${fileExtension}`);
 
