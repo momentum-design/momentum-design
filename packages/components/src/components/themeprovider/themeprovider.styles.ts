bf6adc4135c1a7983ea746d0d35a62f94637f4cc
Christoph Scheffauer <scheffauerc@gmail.com>
2024-10-14 19:13:59 +0200

    fix: themeprovider-font-defaults

--- packages/components/src/components/themeprovider/themeprovider.styles.ts
+++ packages/components/src/components/themeprovider/themeprovider.styles.ts
@@ -1,13 +1,17 @@
 import { css } from 'lit';
 
-// todo: use type tokens once the full set with fallbacks is available
 const styles = css`
   :host {
-    --mdc-themeprovider-font-family: var(--mds-font-family-primary);
     --mdc-themeprovider-color-default: var(--mds-color-theme-text-primary-normal);
+    --mdc-themeprovider-font-family: var(--mds-font-family-primary);
+    --mdc-themeprovider-font-weight: 400;
+    /* adjusting Inter's letter spacing to better match the old CiscoSans */
+    --mdc-themeprovider-letter-spacing-adjustment: -0.25px;
 
-    font-family: var(--mdc-themeprovider-font-family);
     color: var(--mdc-themeprovider-color-default);
+    font-family: var(--mdc-themeprovider-font-family);
+    font-weight: var(--mdc-themeprovider-font-weight);
+    letter-spacing: var(--mdc-themeprovider-letter-spacing-adjustment);
   }
 `;
 
