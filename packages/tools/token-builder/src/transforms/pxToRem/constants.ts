4e726c9c12993216c9bc6d135b58d32880e59ed6
Supriya M <30731236+supminn@users.noreply.github.com>
2024-10-16 19:21:15 +0530

    fix: util function that converts px to rem for typography tokens (#897)
    * feat: adding transformation to convert px to rem
    
    * feat: convert px to rem for css typography
    
    * feat: created local transformation - px to rem
    
    * fix: corrected sd-config type
    
    ---------
    
    Co-authored-by: Christoph Scheffauer <scheffauerc@gmail.com>

---
+++ packages/tools/token-builder/src/transforms/pxToRem/constants.ts
@@ -0,0 +1,10 @@
+import { CONSTANTS as COMMON_CONSTANTS } from '../../common';
+
+const TARGET_TOKEN_TYPES = ['lineHeights', 'fontSizes'];
+
+const CONSTANTS = {
+  NAME: COMMON_CONSTANTS.TRANSFORMS.PX_TO_REM,
+  TARGET_TOKEN_TYPES,
+};
+
+export default CONSTANTS;
