fe7c868d39a78b3f9b205e2917a869544036c68e
Deeya <dishuupadhyay01@gmail.com>
2024-10-16 20:31:43 +0530

    feat: shared styles for focus ring

--- packages/components/src/utils/styles/index.ts
+++ packages/components/src/utils/styles/index.ts
@@ -10,4 +10,33 @@ const hostFitContentStyles = css`
   }
 `;
 
-export { hostFitContentStyles };
+const hostFocusRingStyles = css`
+  :host([focusRingVisible]) {
+    position: relative;
+    outline: 0.046875rem solid black; 
+  }
+
+  :host([focusRingVisible])::before {
+    content: "";
+    position: absolute;
+    top: -0.04625rem;    
+    left: -0.04625rem;   
+    right: -0.04625rem;  
+    bottom: -0.04625rem; 
+    outline: 0.0625rem solid blue;
+    border-radius: inherit;
+  }
+
+  :host([focusRingVisible])::after {
+    content: "";
+    position: absolute;
+    top: -0.10875rem;    
+    left: -0.10875rem;   
+    right: -0.10875rem;  
+    bottom: -0.10875rem; 
+    outline: 0.03125rem solid lightblue;
+    border-radius: inherit; 
+  }
+`;
+
+export { hostFitContentStyles, hostFocusRingStyles };
