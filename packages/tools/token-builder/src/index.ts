5987a9c792b37bcec5ad19868acd6b812d4b0475
Supriya M <30731236+supminn@users.noreply.github.com>
2024-10-16 19:21:15 +0530

    fix: util function that converts px to rem for typography tokens (#897)
    * feat: adding transformation to convert px to rem
    
    * feat: convert px to rem for css typography
    
    * feat: created local transformation - px to rem
    
    * fix: corrected sd-config type
    
    ---------
    
    Co-authored-by: Christoph Scheffauer <scheffauerc@gmail.com>

--- packages/tools/token-builder/src/index.ts
+++ packages/tools/token-builder/src/index.ts
@@ -1,7 +1,7 @@
 export { CONSTANTS } from './common';
 export { JsonMinimalFormat } from './formats';
 export { Dictionary, File, Platform, TokenBuilder } from './models';
-export { ElevationTransform } from './transforms';
+export { ElevationTransform, PxToRemTransform } from './transforms';
 
 export type { Format, Filter, Filters, ConfigFile, Config } from './common';
 export type { DictionaryConfig, FileConfig, PlatformConfig, TokenBuilderConfig } from './models';
@@ -11,4 +11,9 @@ export type {
   ElevationTransformToken,
   ElevationTransformTransformer,
   ElevationTransformType,
+  PxToRemTransformConfig,
+  PxToRemTransformMatcher,
+  PxToRemTransformToken,
+  PxToRemTransformTransformer,
+  PxToRemTransformType,
 } from './transforms';
