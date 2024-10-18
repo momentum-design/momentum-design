6a88510c4e864ee2cf7f557ab9a5b1b667670865
Deeya <dishuupadhyay01@gmail.com>
2024-10-18 23:33:42 +0530

    add focus-ring-visible class

--- packages/components/src/utils/mixins/FocusRingMixin.ts
+++ packages/components/src/utils/mixins/FocusRingMixin.ts
@@ -37,11 +37,11 @@ export const FocusRingMixin = <T extends Constructor<LitElement>>(superClass: T)
         switch (event.type) {
           case 'focus':
             this.focusRingVisible = true;
-            this.setAttribute('focusRingVisible', ''); // will be removed
+            this.classList.add('focus-ring-visible');
             break;
           case 'blur':
             this.focusRingVisible = false;
-            this.removeAttribute('focusRingVisible'); // will be removed
+            this.classList.remove('focus-ring-visible');
             break;
           default:
             break;
