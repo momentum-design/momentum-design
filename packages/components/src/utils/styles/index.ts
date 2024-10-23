4bcb686d2aa20d42ecfe9b33cc305e82ee731d41
Deeya <dishuupadhyay01@gmail.com>
2024-10-23 19:27:28 +0530

    updated styling with box-shadow

--- packages/components/src/utils/styles/index.ts
+++ packages/components/src/utils/styles/index.ts
@@ -14,37 +14,20 @@ const hostFocusRingStyles = css`
   :host {
     --mdc-focus-ring-inner-color: var(--mds-color-theme-focus-0);
     --mdc-focus-ring-middle-color: var(--mds-color-theme-focus-1);
-    --mdc-focus-ring-outer-color: var(--mds-color-theme-focus-2);
+    --mdc-focus-ring-outer-color: var(--mds-color-theme-focus-2;
 
     --mdc-focus-ring-inner-width: 0.125rem;
-    --mdc-focus-ring-middle-width: 0.125rem;
-    --mdc-focus-ring-outer-width: 0.0625rem;
+    --mdc-focus-ring-middle-width: calc(2 * var(--mdc-focus-ring-inner-width));
+    --mdc-focus-ring-outer-width: calc(0.0625rem + var(--mdc-focus-ring-middle-width));
 
     --mdc-focus-ring-middle-offset: var(--mdc-focus-ring-inner-width);
     --mdc-focus-ring-outer-offset: calc(var(--mdc-focus-ring-inner-width) + var(--mdc-focus-ring-middle-width));
   }
-
-  :host(:focus-visible) {
+  
+  :host(:focus) {
     position: relative;
-    outline: var(--mdc-focus-ring-inner-width) solid var(--mdc-focus-ring-inner-color);
-  }
-
-  :host(:focus-visible)::before,
-  :host(:focus-visible)::after {
-    content: "";
-    position: absolute;
-    inset: 0;
-    border-radius: inherit;
-  }
-
-  :host(:focus-visible)::before {
-    outline: var(--mdc-focus-ring-middle-width) solid var(--mdc-focus-ring-middle-color);
-    outline-offset: var(--mdc-focus-ring-middle-offset);
-  }
-
-  :host(:focus-visible)::after {
-    outline: var(--mdc-focus-ring-outer-width) solid var(--mdc-focus-ring-outer-color);
-    outline-offset: var(--mdc-focus-ring-outer-offset);
+    box-shadow: 0 0 0 var( --mdc-focus-ring-inner-width) var(--mdc-focus-ring-inner-color),
+      0 0 0 var(--mdc-focus-ring-middle-width) var(--mdc-focus-ring-middle-color), 0 0 0 var(--mdc-focus-ring-outer-width) var(--mdc-focus-ring-outer-color);
   }
 `;
 
