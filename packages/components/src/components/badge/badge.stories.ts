ad561acd054c75c279abe9742bcfb34f70074fe3
Arjun Kesava P <arjun.kesava@gmail.com>
2024-10-25 12:22:42 +0530

    chore(story): update storybook for badge component
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>

--- packages/components/src/components/badge/badge.stories.ts
+++ packages/components/src/components/badge/badge.stories.ts
@@ -1,17 +1,16 @@
 import type { Meta, StoryObj, Args } from '@storybook/web-components';
 import '.';
 import { html } from 'lit';
-import { DEFAULTS } from './badge.constants';
+import { BADGE_TYPE, BADGE_VARIANT, DEFAULTS } from './badge.constants';
 
 const render = (args: Args) => html`
   <mdc-badge
     type="${args.type}"
-    icon-name="${args.iconName}"
-    size="${args.size}"
-    length-unit="${args.lengthUnit}"
+    icon-name="${args['icon-name']}"
     counter="${args.counter}"
-    maxCounter="${args.maxCounter}"
+    max-counter="${args['max-counter']}"
     variant="${args.variant}"
+    overlay=${args.overlay}
   ></mdc-badge>
 `;
 
@@ -23,7 +22,58 @@ const meta: Meta = {
   parameters: {
     badges: ['wip'],
   },
-  argTypes: {},
+  argTypes: {
+    type: {
+      control: 'select',
+      options: Object.values(BADGE_TYPE),
+      table: {
+        defaultValue: {
+          summary: DEFAULTS.TYPE,
+        },
+      },
+    },
+    overlay: {
+      if: {
+        arg: 'type',
+        neq: BADGE_TYPE.ICON,
+      },
+      control: 'boolean',
+    },
+    counter: {
+      if: {
+        arg: 'type',
+        eq: BADGE_TYPE.COUNTER,
+      },
+      control: 'number',
+    },
+    'max-counter': {
+      if: {
+        arg: 'type',
+        eq: BADGE_TYPE.COUNTER,
+      },
+      control: 'number',
+    },
+    'icon-name': {
+      if: {
+        arg: 'type',
+        eq: BADGE_TYPE.ICON,
+      },
+      control: 'text',
+    },
+    variant: {
+      if: {
+        arg: 'type',
+        neq: BADGE_TYPE.COUNTER,
+      },
+      control: 'select',
+      options: Object.values(BADGE_VARIANT),
+      table: {
+        defaultValue: {
+          summary: DEFAULTS.VARIANT,
+        },
+      },
+    },
+  },
 };
 
 export default meta;
@@ -31,11 +81,10 @@ export default meta;
 export const Primary: StoryObj = {
   args: {
     type: DEFAULTS.TYPE,
-    iconName: 'error-legacy-filled',
-    size: 1,
-    lengthUnit: 'rem',
+    'icon-name': 'error-legacy-filled',
     counter: 1,
-    maxCounter: 99,
+    'max-counter': 99,
     variant: DEFAULTS.VARIANT,
+    overlay: false,
   },
 };
