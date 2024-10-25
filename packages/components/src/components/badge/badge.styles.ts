de8009f3afb7d5bb252066d25c39b51698b9647a
Arjun Kesava P <arjun.kesava@gmail.com>
2024-10-25 07:03:05 +0530

    chore(css): refactor css styles based on review comments
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>

--- packages/components/src/components/badge/badge.styles.ts
+++ packages/components/src/components/badge/badge.styles.ts
@@ -7,15 +7,19 @@ const styles = [
     :host {
       --mdc-badge-text-color: var(--mds-color-theme-text-primary-normal);
       --mdc-badge-text-background-color: var(--mds-color-theme-indicator-secure);
+      --mdc-badge-overlay-background-color: var(--mds-color-theme-background-solid-primary-normal);
       
       color: var(--mdc-badge-text-color);
     }
     .mdc-badge-overlay {
-      outline: 0.063rem solid var(--mdc-badge-text-color);
+      outline: 0.0625rem solid var(--mdc-badge-overlay-background-color);
     }
     .mdc-badge-text {
       padding: 0 0.25rem;
       border-radius: 6.25rem;
+      min-width: 1rem;
+      display: flex;
+      justify-content: center;
     }
     .mdc-badge-notification {
       width: 0.75rem;
