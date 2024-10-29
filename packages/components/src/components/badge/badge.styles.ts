c7a6b00754d7b77a7b60ea2715f2f9d7fb8ffeaa
Arjun Kesava P <arjun.kesava@gmail.com>
2024-10-29 21:27:44 +0530

    refactor: format css colors for badge variants
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>

--- packages/components/src/components/badge/badge.styles.ts
+++ packages/components/src/components/badge/badge.styles.ts
@@ -5,19 +5,24 @@ const styles = [
   hostFitContentStyles,
   css`
     :host {
-      --mdc-badge-default-foreground-color: var(--mds-color-theme-common-text-primary-normal);
+      --mdc-badge-primary-foreground-color: var(--mds-color-theme-common-text-primary-normal);
+      --mdc-badge-primary-background-color: var(--mds-color-theme-background-accent-normal);
+
       --mdc-badge-secondary-foreground-color: var(--mds-color-theme-text-secondary-normal);
-      --mdc-badge-success-foreground-color: var(--mds-color-theme-text-success-normal);
-      --mdc-badge-warning-foreground-color: var(--mds-color-theme-text-warning-normal);
-      --mdc-badge-error-foreground-color: var(--mds-color-theme-text-error-normal);
-      --mdc-badge-success-background-color: var(--mds-color-theme-background-alert-success-normal);
-      --mdc-badge-warning-background-color: var(--mds-color-theme-background-alert-warning-normal);
-      --mdc-badge-error-background-color: var(--mds-color-theme-background-alert-error-normal);
-      --mdc-badge-default-background-color: var(--mds-color-theme-background-accent-normal);
-      --mdc-badge-overlay-background-color: var(--mds-color-theme-background-solid-primary-normal);
       --mdc-badge-secondary-background-color: var(--mds-color-theme-background-alert-default-normal);
+
+      --mdc-badge-success-foreground-color: var(--mds-color-theme-text-success-normal);
+      --mdc-badge-success-background-color: var(--mds-color-theme-background-alert-success-normal);
       
-      color: var(--mdc-badge-default-foreground-color);
+      --mdc-badge-warning-foreground-color: var(--mds-color-theme-text-warning-normal);
+      --mdc-badge-warning-background-color: var(--mds-color-theme-background-alert-warning-normal);
+      
+      --mdc-badge-error-foreground-color: var(--mds-color-theme-text-error-normal);
+      --mdc-badge-error-background-color: var(--mds-color-theme-background-alert-error-normal);
+      
+      --mdc-badge-overlay-background-color: var(--mds-color-theme-background-solid-primary-normal);
+      
+      color: var(--mdc-badge-primary-foreground-color);
     }
     .mdc-badge-overlay {
       outline: 0.0625rem solid var(--mdc-badge-overlay-background-color);
@@ -28,20 +33,20 @@ const styles = [
       min-width: 1rem;
       display: flex;
       justify-content: center;
-      background-color: var(--mdc-badge-default-background-color);
+      background-color: var(--mdc-badge-primary-background-color);
     }
     .mdc-badge-dot {
       width: 0.75rem;
       height: 0.75rem;
       border-radius: 50%;
-      background-color: var(--mdc-badge-default-background-color);
+      background-color: var(--mdc-badge-primary-background-color);
     }
     .mdc-badge-icon {
       padding: 2px;
       border-radius: 50%;
     }
     .mdc-badge-icon__primary {
-      background-color: var(--mdc-badge-default-background-color);
+      background-color: var(--mdc-badge-primary-background-color);
     }
     .mdc-badge-icon__success {
       background-color: var(--mdc-badge-success-background-color);
