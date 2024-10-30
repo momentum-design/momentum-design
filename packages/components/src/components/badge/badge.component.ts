aea1fc8b05a517001379f6bcbaa7e73250e3f528
Arjun Kesava <arjun.kesava@gmail.com>
2024-10-30 17:12:23 +0530

    chore(badge): build badge component of all types (#914)
    * chore(badge): reset badge component with default props
    
    * chore(badge): complete icon, text and notification parts of badge
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore(badge): complete all 4 types of badges
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * fix(tsdoc): add hyphen after param names for tsdoc error
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore(css): update badge styles vars
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore(specs): skip e2e specs for badge component
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore(review): address review comments
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore(review): update notification badge size
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * fix(text): rm text type from badge component
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore: rm overlay for icon part in badge
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore(imports): add deps in tsdoc and restructure imports
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * fix: address review comments, refactor css, mv enums to consts
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore(css): refactor css styles based on review comments
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore(badge): update badges with new specs
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * style: update css colors for icon variants
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * refactor: add and rm tsdoc for methods, fix storybook defaults
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore: add icon states for badge
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore: address review comments
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * refactor: format css colors for badge variants
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    ---------
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    Co-authored-by: Christoph Scheffauer <scheffauerc@gmail.com>

--- packages/components/src/components/badge/badge.component.ts
+++ packages/components/src/components/badge/badge.component.ts
@@ -1,44 +1,30 @@
-import { CSSResult, html } from 'lit';
+import { CSSResult, html, PropertyValues, TemplateResult } from 'lit';
+import { classMap } from 'lit-html/directives/class-map.js';
 import { property } from 'lit/decorators.js';
 import { ifDefined } from 'lit/directives/if-defined.js';
-import { StyleInfo, styleMap } from 'lit/directives/style-map.js';
 import { Component } from '../../models';
-import { DEFAULTS, WARNING_ICON_NAME } from './badge.constants';
+import { BADGE_TYPE, ICON_NAMES_LIST, DEFAULTS, ICON_VARIANT, ICON_STATE } from './badge.constants';
 import styles from './badge.styles';
-import type { BadgeType } from './badge.types';
 
 /**
- * @slot - This is a default/unnamed slot
- *
- * @summary This is MyElement
+ * A badge is a small, visually distinct element that provides additional information
+ * or highlights the status of an item.
+ * Badges are often used to display notification dot, counts, making them a useful tool for
+ * conveying information quickly without taking up much space.
+ * @dependency mdc-icon
+ * @dependency mdc-text
  *
  * @tagname mdc-badge
  */
 class Badge extends Component {
   /**
    * Type of the badge
-   * Can be `regular`, `icon`, `text` or `warning`
+   * Can be `dot` (notification) , `icon` and `counter`
    *
-   * Default: `regular`
+   * Default: `dot`
    */
   @property({ type: String, reflect: true })
-  type?: BadgeType = DEFAULTS.TYPE;
-
-  /**
-   * Scale of the badge (works in combination with length unit)
-   *
-   * Default: `1`
-   */
-  @property({ type: Number })
-  size?: number = DEFAULTS.SIZE;
-
-  /**
-   * Length unit attribute for scale
-   *
-   * Default: `rem`
-   */
-  @property({ type: String, attribute: 'length-unit' })
-  lengthUnit?: string = DEFAULTS.LENGTH_UNIT;
+  type = DEFAULTS.TYPE;
 
   /**
    * If `type` is set to `icon`, attribute `iconName` can
@@ -50,69 +36,168 @@ class Badge extends Component {
   iconName?: string;
 
   /**
-   * If `type` is set to `text`, attribute `text` can
-   * be used to pass in any text to be displayed in the badge.
+   * badge variant
    */
   @property({ type: String })
-  text?: string;
+  variant = DEFAULTS.VARIANT;
 
-  private updateSize() {
-    // if (this.scale && this.lengthUnit) {
-    //   const value = `${this.scale}${this.lengthUnit}`;
-    //   this.style.height = value;
-    //   if (this.type !== 'text') {
-    //     this.style.width = value;
-    //   }
-    // }
+  @property({ type: Number })
+  counter?: number;
+
+  @property({ type: Number, attribute: 'max-counter' })
+  maxCounter: number = DEFAULTS.MAX_COUNTER;
+
+  @property({ type: Boolean })
+  overlay = false;
+
+  /**
+   * Aria-label attribute to be set for accessibility
+   */
+  @property({ type: String, attribute: 'aria-label' })
+  override ariaLabel: string | null = null;
+
+  /**
+   * If `type` is set to `counter` and if `counter` is greater than `maxCounter`,
+   * then it will return a string the maxCounter value as string.
+   * Otherwise, it will return a string representation of `counter`.
+   * If `counter` is not a number, it will return an empty string.
+   * @param maxCounter - the maximum limit which can be displayed on the badge
+   * @param counter - the number to be displayed on the badge
+   * @returns the string representation of the counter
+   */
+  private getCounterText(maxCounter: number, counter?: number): string {
+    if (counter === undefined || typeof counter !== 'number') {
+      return '';
+    }
+    // At any given time, the max limit should not cross 999.
+    if (counter > DEFAULTS.MAX_COUNTER_LIMIT) {
+      return `${DEFAULTS.MAX_COUNTER_LIMIT}+`;
+    }
+    if (counter > maxCounter) {
+      return `${maxCounter}+`;
+    }
+    return counter.toString();
   }
 
-  override updated(changedProperties: Map<string, any>) {
-    super.updated(changedProperties);
-    if (changedProperties.has('size') || changedProperties.has('lengthUnit')) {
-      this.updateSize();
+  /**
+   * Method to generate the badge icon template.
+   * @param iconName - name of the icon to be used.
+   * @param variant - variant of the badge.
+   * @returns the template result of the icon.
+   */
+  private getBadgeIcon(
+    iconName: string,
+    overlay: boolean,
+    iconVariant: string,
+    type?: string,
+  ): TemplateResult {
+    return html`
+      <mdc-icon
+        class="mdc-badge-icon ${classMap(this.getIconClasses(overlay, iconVariant, type))}"
+        name="${ifDefined(iconName)}"
+        length-unit="${DEFAULTS.LENGTH_UNIT}"
+        size="${DEFAULTS.ICON_SIZE}"
+      ></mdc-icon>
+    `;
+  }
+
+  /**
+   * Method to generate the badge dot template.
+   * @param overlay - boolean indicating whether the badge should have an overlay.
+   * @returns the template result of the dot with mdc-badge-dot class.
+   */
+  private getBadgeDot(overlay: boolean): TemplateResult {
+    return html`<div class="mdc-badge-dot ${classMap({ 'mdc-badge-overlay': overlay })}"></div>`;
+  }
+
+  /**
+   * This method generates the CSS classes for the badge icon.
+   * @param overlay - boolean indicating whether the badge should have an overlay.
+   * @param iconVariant - the variant of the icon badge.
+   * @param type - the type of the badge.
+   * @returns - an object containing the CSS classes for the icon.
+   */
+  private getIconClasses(overlay: boolean, iconVariant: string, type?: string): { [key: string]: boolean } {
+    const overLayClass = { 'mdc-badge-overlay': overlay };
+    const variantTypes = type === BADGE_TYPE.ICON ? ICON_VARIANT : ICON_STATE;
+    const iconVariantType = Object.values(variantTypes).includes(iconVariant)
+      ? iconVariant : DEFAULTS.VARIANT;
+    const backgroundClass = { [`mdc-badge-icon__${iconVariantType}`]: true };
+    return {
+      ...overLayClass,
+      ...backgroundClass,
+    };
+  }
+
+  /**
+   * Method to generate the badge text and counter template.
+   * @param maxCounter - the maximum limit which can be displayed on the badge
+   * @param overlay - whether the badge should have an overlay.
+   * @param counter - the number to be displayed on the badge
+   * @returns the template result of the text.
+   */
+  private getBadgeCounterText(maxCounter: number, overlay: boolean, counter?: number): TemplateResult {
+    return html`
+      <mdc-text
+        type="body-small-medium"
+        tagname="div"
+        class="mdc-badge-text ${classMap({ 'mdc-badge-overlay': overlay })}"
+      >
+        ${this.getCounterText(maxCounter, counter)}
+      </mdc-text>
+    `;
+  }
+
+  /**
+   * Method to set the role based on the aria-label provided.
+   * If the aria-label is provided, the role of the element will be 'img'.
+   * Otherwise, the role will be null.
+   */
+  private setRoleByAriaLabel(): void {
+    if (this.ariaLabel) {
+      this.role = 'img';
+    } else {
+      this.role = null;
     }
   }
 
-  iconTemplate() {
-    return html`<div class="mdc-badge-icon-container">
-      <mdc-icon name=${ifDefined(this.iconName)} size="100" length-unit="%"></mdc-icon>
-    </div>`;
+  /**
+   * Generates the badge content based on the badge type.
+   * Utilizes various helper methods to create the appropriate badge template based on the
+   * current badge type. Supports 'dot', 'icon', 'counter', 'success', 'warning', and 'error'
+   * types, returning the corresponding template result for each type.
+   * @returns the TemplateResult for the current badge type.
+   */
+  private getBadgeContentBasedOnType(): TemplateResult {
+    const { counter, iconName, maxCounter, overlay, type, variant } = this;
+    switch (type) {
+      case BADGE_TYPE.DOT:
+        return this.getBadgeDot(overlay);
+      case BADGE_TYPE.ICON:
+        return this.getBadgeIcon(iconName || '', overlay, variant, type);
+      case BADGE_TYPE.COUNTER:
+        return this.getBadgeCounterText(maxCounter, overlay, counter);
+      case BADGE_TYPE.SUCCESS:
+        return this.getBadgeIcon(ICON_NAMES_LIST.SUCCESS_ICON_NAME, overlay, ICON_STATE.SUCCESS);
+      case BADGE_TYPE.WARNING:
+        return this.getBadgeIcon(ICON_NAMES_LIST.WARNING_ICON_NAME, overlay, ICON_STATE.WARNING);
+      case BADGE_TYPE.ERROR:
+        return this.getBadgeIcon(ICON_NAMES_LIST.ERROR_ICON_NAME, overlay, ICON_STATE.ERROR);
+      default:
+        return html``;
+    }
   }
 
-  textTemplate() {
-    return html`${this.text}`;
-  }
+  public override update(changedProperties: PropertyValues): void {
+    super.update(changedProperties);
 
-  warningTemplate() {
-    return html` <mdc-icon name="${WARNING_ICON_NAME}" class="mdc-badge-warning"></mdc-icon> `;
+    if (changedProperties.has('ariaLabel')) {
+      this.setRoleByAriaLabel();
+    }
   }
 
   public override render() {
-    let content;
-    const size = `${this.size}${this.lengthUnit}`;
-    let sizeStyles: StyleInfo = { width: size, height: size };
-
-    switch (this.type) {
-      case 'regular':
-        content = html``;
-        break;
-      case 'icon':
-        content = this.iconTemplate();
-        break;
-      case 'text':
-        content = this.textTemplate();
-        // make width flexible when text -> only set the height:
-        sizeStyles = { height: sizeStyles.height };
-        break;
-      case 'warning':
-        content = this.warningTemplate();
-        break;
-      default:
-        content = html``;
-        break;
-    }
-
-    return html`<div class="mdc-badge-container" style=${styleMap(sizeStyles)}>${content}</div>`;
+    return this.getBadgeContentBasedOnType();
   }
 
   public static override styles: Array<CSSResult> = [...Component.styles, ...styles];
