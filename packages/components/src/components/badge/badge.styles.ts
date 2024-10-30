0e89cb63e6ee59d241df27f4aa410475ac24723c
Arjun Kesava <arjun.kesava@gmail.com>
2024-10-30 17:12:23 +0530

    chore(badge): build badge component of all types (#914)
    * chore(badge): reset badge component with default props
    
    * chore(badge): complete icon, text and notification parts of badge
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore(badge): complete all 4 types of badges
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * fix(tsdoc): add hyphen after param names for tsdoc error
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore(css): update badge styles vars
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore(specs): skip e2e specs for badge component
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore(review): address review comments
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore(review): update notification badge size
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * fix(text): rm text type from badge component
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore: rm overlay for icon part in badge
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore(imports): add deps in tsdoc and restructure imports
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * fix: address review comments, refactor css, mv enums to consts
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore(css): refactor css styles based on review comments
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore(badge): update badges with new specs
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * style: update css colors for icon variants
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * refactor: add and rm tsdoc for methods, fix storybook defaults
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore: add icon states for badge
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore: address review comments
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * refactor: format css colors for badge variants
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    ---------
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    Co-authored-by: Christoph Scheffauer <scheffauerc@gmail.com>

--- packages/components/src/components/badge/badge.styles.ts
+++ packages/components/src/components/badge/badge.styles.ts
@@ -5,22 +5,64 @@ const styles = [
   hostFitContentStyles,
   css`
     :host {
-      --mdc-badge-icon-background-color: var(--mds-color-theme-background-accent-normal);
-      --mdc-badge-icon-color: var(--mds-color-theme-common-text-primary-normal);
-    }
+      --mdc-badge-primary-foreground-color: var(--mds-color-theme-common-text-primary-normal);
+      --mdc-badge-primary-background-color: var(--mds-color-theme-background-accent-normal);
 
-    .mdc-badge-container {
+      --mdc-badge-secondary-foreground-color: var(--mds-color-theme-text-secondary-normal);
+      --mdc-badge-secondary-background-color: var(--mds-color-theme-background-alert-default-normal);
+
+      --mdc-badge-success-foreground-color: var(--mds-color-theme-text-success-normal);
+      --mdc-badge-success-background-color: var(--mds-color-theme-background-alert-success-normal);
+      
+      --mdc-badge-warning-foreground-color: var(--mds-color-theme-text-warning-normal);
+      --mdc-badge-warning-background-color: var(--mds-color-theme-background-alert-warning-normal);
+      
+      --mdc-badge-error-foreground-color: var(--mds-color-theme-text-error-normal);
+      --mdc-badge-error-background-color: var(--mds-color-theme-background-alert-error-normal);
+      
+      --mdc-badge-overlay-background-color: var(--mds-color-theme-background-solid-primary-normal);
+      
+      color: var(--mdc-badge-primary-foreground-color);
+    }
+    .mdc-badge-overlay {
+      outline: 0.0625rem solid var(--mdc-badge-overlay-background-color);
+    }
+    .mdc-badge-text {
+      padding: 0 0.25rem;
+      border-radius: 6.25rem;
+      min-width: 1rem;
       display: flex;
       justify-content: center;
-      align-items: center;
-      border-radius: 9999px;
-      background-color: var(--mdc-badge-icon-background-color);
-      color: var(--mdc-badge-icon-color);
+      background-color: var(--mdc-badge-primary-background-color);
     }
-
-    .mdc-badge-icon-container {
-      height: 80%;
-      width: 80%;
+    .mdc-badge-dot {
+      width: 0.75rem;
+      height: 0.75rem;
+      border-radius: 50%;
+      background-color: var(--mdc-badge-primary-background-color);
+    }
+    .mdc-badge-icon {
+      padding: 2px;
+      border-radius: 50%;
+    }
+    .mdc-badge-icon__primary {
+      background-color: var(--mdc-badge-primary-background-color);
+    }
+    .mdc-badge-icon__success {
+      background-color: var(--mdc-badge-success-background-color);
+      color: var(--mdc-badge-success-foreground-color);
+    }
+    .mdc-badge-icon__warning {
+      background-color: var(--mdc-badge-warning-background-color);
+      color: var(--mdc-badge-warning-foreground-color);
+    }
+    .mdc-badge-icon__error {
+      background-color: var(--mdc-badge-error-background-color);
+      color: var(--mdc-badge-error-foreground-color);
+    }
+    .mdc-badge-icon__secondary {
+      background-color: var(--mdc-badge-secondary-background-color);
+      color: var(--mdc-badge-secondary-foreground-color);
     }
   `,
 ];
