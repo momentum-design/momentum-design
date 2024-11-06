ca64d99c28e9bc807d92464ace20e21e4cc3b3e8
Deeya <dishuupadhyay01@gmail.com>
2024-11-06 20:33:45 +0530

    fix: add presence component story

---
+++ packages/components/src/components/presence/presence.stories.ts
@@ -0,0 +1,39 @@
+import type { Meta, StoryObj, Args } from '@storybook/web-components';
+import '.';
+import { html } from 'lit';
+import { PRESENCE_TYPE, PRESENCE_SIZE, DEFAULTS } from './presence.constants';
+import { disableControls } from '../../../config/storybook/utils';
+
+const render = (args: Args) => html`
+  <mdc-presence type="${args.type}" size="${args.size}" /><mdc-presence> </mdc-presence>
+`;
+
+const meta: Meta = {
+  title: 'Work In Progress/presence',
+  tags: ['autodocs'],
+  component: 'mdc-presence',
+  render,
+  parameters: {
+    badges: ['wip'],
+  },
+  argTypes: {
+    type: {
+      options: Object.values(PRESENCE_TYPE),
+      control: { type: 'select' },
+    },
+    size: {
+      options: Object.values(PRESENCE_SIZE),
+      control: { type: 'select' },
+    },
+    ...disableControls(['presenceSize', 'icon']),
+  },
+};
+
+export default meta;
+
+export const Primary: StoryObj = {
+  args: {
+    type: DEFAULTS.TYPE,
+    size: DEFAULTS.SIZE,
+  },
+};
