f2ad338ea83711282cfd97f9547cfc1f77e09410
Supriya M <sansup49@gmail.com>
2024-11-11 13:05:37 +0530

    feat: button component creation

--- packages/components/src/components/button/button.constants.ts
+++ packages/components/src/components/button/button.constants.ts
@@ -2,4 +2,45 @@ import utils from '../../utils/tag-name';
 
 const TAG_NAME = utils.constructTagName('button');
 
-export { TAG_NAME };
+const BUTTON_VARIANTS = {
+  PRIMARY: 'primary',
+  SECONDARY: 'secondary',
+  TERTIARY: 'tertiary',
+};
+
+const PILL_BUTTON_SIZES = {
+  40: 40,
+  32: 32,
+  28: 28,
+  24: 24,
+};
+
+const ICON_BUTTON_SIZES = {
+  64: 64,
+  52: 52,
+  20: 20,
+  ...PILL_BUTTON_SIZES,
+};
+
+const BUTTON_COLORS = {
+  POSITIVE: 'positive',
+  NEGATIVE: 'negative',
+  ACCENT: 'accent',
+  PROMOTIONAL: 'promotional',
+  DEFAULT: 'default',
+};
+
+const BUTTON_TYPE = {
+  PILL: 'pill',
+  ICON: 'icon',
+  PILL_WITH_ICON: 'pill-with-icon',
+};
+
+const DEFAULTS = {
+  VARIANT: BUTTON_VARIANTS.PRIMARY,
+  SIZE: PILL_BUTTON_SIZES[32],
+  COLOR: BUTTON_COLORS.DEFAULT,
+  TYPE: BUTTON_TYPE.PILL,
+};
+
+export { TAG_NAME, DEFAULTS, BUTTON_VARIANTS, PILL_BUTTON_SIZES, ICON_BUTTON_SIZES, BUTTON_COLORS, BUTTON_TYPE };
---
+++ packages/components/src/components/button/button.utils.ts
@@ -0,0 +1,19 @@
+import { ICON_BUTTON_SIZES, PILL_BUTTON_SIZES } from './button.constants';
+
+const isValidIconSize = (
+  size: number,
+  variant: string,
+): number | boolean => Object.values(ICON_BUTTON_SIZES).includes(size)
+    && !(size === 20 && variant !== 'tertiary');
+
+const isValidPillSize = (size: number): number | boolean => Object.values(PILL_BUTTON_SIZES).includes(size);
+
+const getIconSize = (size: number): number => {
+  switch (size) {
+    case ICON_BUTTON_SIZES[64]: return 2;
+    case ICON_BUTTON_SIZES[52]: return 1.75;
+    case ICON_BUTTON_SIZES[40]: return 1.25;
+    default: return 1;
+  }
+};
+export { isValidIconSize, isValidPillSize, getIconSize };
