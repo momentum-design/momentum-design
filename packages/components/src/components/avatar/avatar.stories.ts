64c5acfb4a5e1fec124397844ede7861b2559b4d
Arjun Kesava P <arjun.kesava@gmail.com>
2024-11-18 14:24:37 +0530

    feat: build basic types of avatar
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>

--- packages/components/src/components/avatar/avatar.stories.ts
+++ packages/components/src/components/avatar/avatar.stories.ts
@@ -1,10 +1,16 @@
 import type { Meta, StoryObj, Args } from '@storybook/web-components';
 import '.';
 import { html } from 'lit';
-import { DEFAULTS } from './avatar.constants';
 
 const render = (args: Args) => html`
-  <mdc-avatar src="${args.src}" size="${args.size}" type="${args.type}"></mdc-avatar>
+  <mdc-avatar
+    src="${args.src}"
+    size="${args.size}"
+    icon-name="${args['icon-name']}"
+    alt="${args.alt}"
+    initials="${args.initials}"
+    counter="${args.counter}"
+  ></mdc-avatar>
 `;
 
 const meta: Meta = {
@@ -21,9 +27,7 @@ export default meta;
 
 export const Primary: StoryObj = {
   args: {
-    // eslint-disable-next-line max-len
-    src: 'https://images.unsplash.com/photo-1583195764036-6dc248ac07d9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2855&q=80',
-    size: DEFAULTS.SIZE,
-    type: DEFAULTS.TYPE,
+    src: 'https://picsum.photos/256',
+    counter: 21,
   },
 };
--- packages/components/src/components/avatar/avatar.styles.ts
+++ packages/components/src/components/avatar/avatar.styles.ts
@@ -1,19 +1,31 @@
 import { css } from 'lit';
 
 const styles = css`
-  :host {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border-radius: 100vh;
-    position: relative;
-    width: fit-content;
+  .container {
+    padding: unset;
+    margin: unset;
+    background-color: gray;
+
+    width: 128px;
+    height: 128px;
+    position: absolute;
+    border-radius: 12vh;
   }
-  img {
+  .photo {
     border-radius: 100vh;
-    height: inherit;
-    width: inherit;
-    object-fit: cover;
+    height: 100%;
+    width: 100%;
+    object-fit: contain;
+  }
+  .icon {
+    height: 100%;
+    width: 100%;
+    color: white;
+  }
+  .presence {
+    position: absolute;
+    bottom: 0;
+    right: 0;
   }
 `;
 
