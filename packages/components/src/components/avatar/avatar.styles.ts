5df76d89671715d5b57ee1e6ad1a3f5df72680a4
Arjun Kesava P <arjun.kesava@gmail.com>
2024-11-19 12:18:57 +0530

    chore: add sizes, dimensions in avatar
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>

--- packages/components/src/components/avatar/avatar.styles.ts
+++ packages/components/src/components/avatar/avatar.styles.ts
@@ -1,6 +1,10 @@
 import { css } from 'lit';
 
 const styles = css`
+  :host {
+    --mdc-avatar-default-background-color: var(--mds-color-theme-avatar-default);
+    --mdc-avatar-default-foreground-color: var(--mds-color-theme-common-text-primary-normal);
+  }
   .place-center {
     display: flex;
     justify-content: center;
@@ -9,18 +13,20 @@ const styles = css`
   .container {
     padding: unset;
     margin: unset;
-    background-color: gray;
-
-    width: 128px;
-    height: 128px;
     position: absolute;
-    border-radius: 12vh;
+    border-radius: 10%;
+    background-color: unset;
+    color: var(--mdc-avatar-default-foreground-color);
+  }
+  .content {
+    background-color: var(--mdc-avatar-default-background-color);
+    border-radius: 100vh;
   }
   .photo {
     border-radius: 100vh;
     height: 100%;
     width: 100%;
-    object-fit: contain;
+    object-fit: cover;
   }
   .icon {
     height: 100%;
