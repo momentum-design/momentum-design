61927e1af2de51714f352a1bc9be82b0671fbe42
Arjun Kesava P <arjun.kesava@gmail.com>
2024-11-20 14:39:38 +0530

    chore: add typing state and minor changes
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>

--- packages/components/src/components/avatar/avatar.styles.ts
+++ packages/components/src/components/avatar/avatar.styles.ts
@@ -4,6 +4,7 @@ const styles = css`
   :host {
     --mdc-avatar-default-background-color: var(--mds-color-theme-avatar-default);
     --mdc-avatar-default-foreground-color: var(--mds-color-theme-common-text-primary-normal);
+    --mdc-avatar-loading-background-color: var(--mds-color-theme-common-overlays-secondary-normal);
   }
   .place-center {
     display: flex;
@@ -21,6 +22,9 @@ const styles = css`
   .content {
     background-color: var(--mdc-avatar-default-background-color);
     border-radius: 100vh;
+    position: relative;
+    display: grid;
+    place-items: center;
   }
   .photo {
     border-radius: 100vh;
@@ -37,6 +41,45 @@ const styles = css`
     position: absolute;
     bottom: 0;
     right: 0;
+  }
+  .loading__container {
+    position: absolute;
+    display: grid;
+    place-items: center;
+    background-color: var(--mdc-avatar-loading-background-color);
+  }
+  .loading__wrapper {
+    position: absolute;
+    display: flex;
+    justify-content: space-evenly;
+  }
+ .loading__icon {
+    vertical-align: middle;
+    animation-duration: 1.4s;
+    animation-iteration-count: infinite;
+    animation-name: loading;
+    animation-fill-mode: both;
+  }
+  .loading__icon:nth-of-type(2) {
+    animation-delay: 0.2s;
+  }
+  .loading__icon:nth-of-type(3) {
+    animation-delay: 0.4s;
+  }
+  .loading__icon:not(:last-child) {
+    margin-right: 0.5rem;
+  }
+  @keyframes loading {
+    0%,
+    100% {
+      opacity: 0.1;
+      transform: scale(0, 0);
+    }
+
+    20% {
+      opacity: 1;
+      transform: scale(1, 1);
+    }
   }
 `;
 
