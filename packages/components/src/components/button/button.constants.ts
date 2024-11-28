22b638d8c689a4e1ca425b88c3619a4ba2160ad9
Supriya M <30731236+supminn@users.noreply.github.com>
2024-11-28 21:58:36 +0530

    docs: button component along with storybook and documentation (#948)
    * chore: button component generation
    
    * chore: skiping tests
    
    * feat: button component creation
    
    * refactor: update icon for active state
    
    * fix: updated promotional color
    
    * refactor: addressing review comments
    
    * feat: storybook examples for button
    
    * fix: button sizes for icon button
    
    * fix: addressed review comments
    
    * docs: documentation for component and utils
    
    * refactor: docs based on review comment
    
    * fix: import icon tagname
    
    * refactor: updated the logic of prefix and postfix icons
    
    * docs: updated docs for icons and slots
    
    * chore: cleanup
    
    * chore: cleanup for mdc-icon ref
    
    * fix: naming convention on storybook
    
    * refactor: added types to constants
    
    * fix: types for constants
    
    * fix: handle button type change based on passed icons attr
    
    * fix: changes from review comments
    
    * fix: onkeydown and onclick handler behaviour
    
    * style(fix): removed focus ring for disabled button
    
    * fix: replaced icon with placeholder in stories
    
    * fix: adding transparent border to primary and tertiary variants
    
    * style(fix): padding for icon buttons
    
    * style(fix): removed padding; added aspect-ratio
    
    * fix(minor): updated px to rem
    
    * fix(style): adding disabled focus logic on focusring
    
    * fix: removed redundant pseudo element style
    
    * refactor(minor): rewriting event handlers
    
    * fix: button interaction events based on disabled state
    
    * refactor: updated component to support button as a form element
    
    * fix: making soft-disabled interactable
    
    * fix: type const, hide internals on storybook
    
    * fix: iconname type on button
    
    * fix: review comments addressed
    
    ---------
    
    Co-authored-by: Jason20020 <55873488+Jason20020@users.noreply.github.com>

---
+++ packages/components/src/components/button/button.constants.ts
@@ -0,0 +1,62 @@
+import utils from '../../utils/tag-name';
+
+const TAG_NAME = utils.constructTagName('button');
+
+const BUTTON_VARIANTS = {
+  PRIMARY: 'primary',
+  SECONDARY: 'secondary',
+  TERTIARY: 'tertiary',
+} as const;
+
+const PILL_BUTTON_SIZES = {
+  40: 40,
+  32: 32,
+  28: 28,
+  24: 24,
+} as const;
+
+const ICON_BUTTON_SIZES = {
+  64: 64,
+  52: 52,
+  20: 20,
+  ...PILL_BUTTON_SIZES,
+} as const;
+
+const BUTTON_COLORS = {
+  POSITIVE: 'positive',
+  NEGATIVE: 'negative',
+  ACCENT: 'accent',
+  PROMOTIONAL: 'promotional',
+  DEFAULT: 'default',
+} as const;
+
+const BUTTON_TYPE_INTERNAL = {
+  PILL: 'pill',
+  ICON: 'icon',
+  PILL_WITH_ICON: 'pill-with-icon',
+} as const;
+
+const BUTTON_TYPE = {
+  BUTTON: 'button',
+  SUBMIT: 'submit',
+  RESET: 'reset',
+} as const;
+
+const DEFAULTS = {
+  VARIANT: BUTTON_VARIANTS.PRIMARY,
+  SIZE: PILL_BUTTON_SIZES[32],
+  COLOR: BUTTON_COLORS.DEFAULT,
+  TYPE_INTERNAL: BUTTON_TYPE_INTERNAL.ICON,
+  TYPE: BUTTON_TYPE.BUTTON,
+};
+
+export {
+  TAG_NAME,
+  DEFAULTS,
+  BUTTON_VARIANTS,
+  PILL_BUTTON_SIZES,
+  ICON_BUTTON_SIZES,
+  BUTTON_COLORS,
+  BUTTON_TYPE_INTERNAL,
+  BUTTON_TYPE,
+};
