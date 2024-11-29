a5af9a85319e41ff1c64ac9f74c02881bf41f3a4
Arjun Kesava P <arjun.kesava@gmail.com>
2024-11-29 15:00:57 +0530

    chore: add type for icon name and add size for storybook
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>

--- packages/components/src/components/avatar/avatar.stories.ts
+++ packages/components/src/components/avatar/avatar.stories.ts
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj, Args } from '@storybook/web-components';
+import { repeat } from 'lit/directives/repeat.js';
 import '.';
 import { html } from 'lit';
 import { TYPE as PRESENCE_TYPE, SIZE as PRESENCE_SIZE } from '../presence/presence.constants';
@@ -46,7 +47,7 @@ export default meta;
 
 export const Image: StoryObj = {
   args: {
-    src: 'https://picsum.photos/256',
+    src: 'https://picsum.photos/id/63/256',
     alt: 'Lorem ipsum',
     initials: 'AK',
     'icon-name': 'placeholder-bold',
@@ -73,5 +74,30 @@ export const Counter: StoryObj = {
 export const Icon: StoryObj = {
   args: {
     'icon-name': 'placeholder-bold',
+  },
+};
+
+export const Size: StoryObj = {
+  render: () => html`
+    <div style="display: flex; justify-content: space-evenly; align-items: center">
+    ${repeat(Object.values(PRESENCE_SIZE), (size) => html`
+        <mdc-avatar size="${size}"></mdc-avatar>
+      `)}
+    </div>
+  `,
+};
+
+export const SizeWithPresence: StoryObj = {
+  render: (args: Args) => html`
+    <div style="display: flex; justify-content: space-evenly; align-items: center">
+      ${repeat(Object.values(PRESENCE_SIZE), (size) => html`
+        <mdc-avatar size="${size}" presence="${args.presence}" src="${args.src}" alt="${args.alt}"></mdc-avatar>
+      `)}
+    </div>
+  `,
+  args: {
+    presence: 'active',
+    src: 'https://picsum.photos/id/63/256',
+    alt: 'Lorem Ipsum',
   },
 };
