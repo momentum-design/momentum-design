0cce2ab4ad8877f8f16a9d60f1de89809a536b89
Arjun Kesava P <arjun.kesava@gmail.com>
2024-12-09 21:27:45 +0530

    chore: rm button and clickable events
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>

--- packages/components/src/components/avatar/avatar.styles.ts
+++ packages/components/src/components/avatar/avatar.styles.ts
@@ -1,56 +1,55 @@
 import { css } from 'lit';
+import { hostFitContentStyles, hostFocusRingStyles } from '../../utils/styles';
 
-const styles = css`
+const styles = [hostFitContentStyles, css`
   :host {
     --mdc-avatar-default-background-color: var(--mds-color-theme-avatar-default);
     --mdc-avatar-default-foreground-color: var(--mds-color-theme-common-text-primary-normal);
     --mdc-avatar-loading-background-color: var(--mds-color-theme-common-overlays-secondary-normal);
   }
-  :host([size="xx_large"]) .container, :host([size="xx_large"]) .content {
+  :host([size="xx_large"]) .content {
     width: 7.75rem;
     height: 7.75rem;
   }
-  :host([size="x_large"]) .container, :host([size="x_large"]) .content {
+  :host([size="x_large"]) .content {
     width: 5.5rem;
     height: 5.5rem;
   }
-  :host([size="large"]) .container, :host([size="large"]) .content {
+  :host([size="large"]) .content {
     width: 4.5rem;
     height: 4.5rem;
   }
-  :host([size="midsize"]) .container, :host([size="midsize"]) .content {
+  :host([size="midsize"]) .content {
     width: 4rem;
     height: 4rem;
   }
-  :host([size="small"]) .container, :host([size="small"]) .content {
+  :host([size="small"]) .content {
     width: 3rem;
     height: 3rem;
   }
-  :host([size="x_small"]) .container, :host([size="x_small"]) .content {
+  :host([size="x_small"]) .content {
     width: 2rem;
     height: 2rem;
   }
-  :host([size="xx_small"]) .container, :host([size="xx_small"]) .content {
+  :host([size="xx_small"]) .content {
     width: 1.5rem;
     height: 1.5rem;
   }
-  :host([size="large"]) .container > .content > .loader,
-  :host([size="midsize"]) .container > .content > .loader {
+  :host([size="large"]) .content > .loader,
+  :host([size="midsize"]) .content > .loader {
     transform: scale(0.8);
   }
-  :host([size="small"]) .container > .content > .loader {
+  :host([size="small"]) .content > .loader {
     transform: scale(0.6);
   }
-  :host([size="x_small"]) .container > .content > .loader {
+  :host([size="x_small"]) .content > .loader {
     transform: scale(0.4);
   }
-  :host([size="xx_small"]) .container > .content > .loader {
+  :host([size="xx_small"]) .content > .loader {
     transform: scale(0.3);
   }
-  .place-center {
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  :host([is-typing]) .content {
+    opacity: 0.7;
   }
   .container {
     padding: unset;
@@ -91,11 +90,11 @@ const styles = css`
     animation: loading-key 1s infinite linear alternate;
   }
   @keyframes loading-key {
-    0%  {box-shadow: 1.25rem 0 #000, -1.25rem 0 #0002; background: #000}
-    33% {box-shadow: 1.25rem 0 #000, -1.25rem 0 #0002; background: #0002}
-    66% {box-shadow: 1.25rem 0 #0002,-1.25rem 0 #000; background: #0002}
-    100%{box-shadow: 1.25rem 0 #0002,-1.25rem 0 #000; background: #000}
+    0%  {box-shadow: 1.25rem 0 #FFF, -1.25rem 0 #FFF2; background: #FFF;}
+    33% {box-shadow: 1.25rem 0 #FFF, -1.25rem 0 #FFF2; background: #FFF2;}
+    66% {box-shadow: 1.25rem 0 #FFF2, -1.25rem 0 #FFF; background: #FFF2;}
+    100%{box-shadow: 1.25rem 0 #FFF2, -1.25rem 0 #FFF; background: #FFF;}
   }
-`;
+`, hostFocusRingStyles];
 
-export default [styles];
+export default styles;
