265e81ee281b37c7777d02fefe7a84c100dff0c3
Arjun Kesava P <arjun.kesava@gmail.com>
2024-12-12 18:00:39 +0530

    refactor: switch case to record map for avatar utils
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>

--- packages/components/src/components/avatar/avatar.utils.ts
+++ packages/components/src/components/avatar/avatar.utils.ts
@@ -4,32 +4,29 @@ import type { TextType } from '../text/text.types';
 import type { AvatarSize } from './avatar.types';
 
 const getAvatarIconSize = (size: AvatarSize): number => {
-  switch (size) {
-    case AVATAR_SIZE.XX_LARGE: return 4.75;
-    case AVATAR_SIZE.X_LARGE: return 3;
-    case AVATAR_SIZE.LARGE: return 2.5;
-    case AVATAR_SIZE.MIDSIZE: return 2.25;
-    case AVATAR_SIZE.SMALL: return 1.75;
-    case AVATAR_SIZE.XX_SMALL: return 1;
-    case AVATAR_SIZE.X_SMALL:
-    default:
-      return 1.25;
-  }
+  const avatarIconSizeMap: Record<AvatarSize, number> = {
+    [AVATAR_SIZE.XX_LARGE]: 4.75,
+    [AVATAR_SIZE.X_LARGE]: 3,
+    [AVATAR_SIZE.LARGE]: 2.5,
+    [AVATAR_SIZE.MIDSIZE]: 2.25,
+    [AVATAR_SIZE.SMALL]: 1.75,
+    [AVATAR_SIZE.X_SMALL]: 1.25,
+    [AVATAR_SIZE.XX_SMALL]: 1,
+  };
+  return avatarIconSizeMap[size] || 1.25; // default size of icon
 };
 
 const getAvatarTextFontSize = (size: AvatarSize): TextType => {
-  switch (size) {
-    case AVATAR_SIZE.XX_LARGE: return FONT_TYPE.HEADING_XLARGE_MEDIUM;
-    case AVATAR_SIZE.X_LARGE: return FONT_TYPE.HEADING_LARGE_MEDIUM;
-    case AVATAR_SIZE.LARGE: return FONT_TYPE.HEADING_MIDSIZE_MEDIUM;
-    case AVATAR_SIZE.MIDSIZE:
-    case AVATAR_SIZE.SMALL:
-      return FONT_TYPE.HEADING_SMALL_MEDIUM;
-    case AVATAR_SIZE.XX_SMALL: return FONT_TYPE.BODY_SMALL_MEDIUM;
-    case AVATAR_SIZE.X_SMALL:
-    default:
-      return FONT_TYPE.BODY_MIDSIZE_MEDIUM;
-  }
+  const avatarTextFontSizeMap: Record<AvatarSize, TextType> = {
+    [AVATAR_SIZE.XX_LARGE]: FONT_TYPE.HEADING_XLARGE_MEDIUM,
+    [AVATAR_SIZE.X_LARGE]: FONT_TYPE.HEADING_LARGE_MEDIUM,
+    [AVATAR_SIZE.LARGE]: FONT_TYPE.HEADING_MIDSIZE_MEDIUM,
+    [AVATAR_SIZE.MIDSIZE]: FONT_TYPE.HEADING_SMALL_MEDIUM,
+    [AVATAR_SIZE.SMALL]: FONT_TYPE.HEADING_SMALL_MEDIUM,
+    [AVATAR_SIZE.XX_SMALL]: FONT_TYPE.BODY_SMALL_MEDIUM,
+    [AVATAR_SIZE.X_SMALL]: FONT_TYPE.BODY_MIDSIZE_MEDIUM,
+  };
+  return avatarTextFontSizeMap[size] || FONT_TYPE.BODY_MIDSIZE_MEDIUM; // default size of text font
 };
 
 export {
