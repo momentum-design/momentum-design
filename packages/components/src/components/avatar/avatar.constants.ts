2d778688c9d1177041cd86523aa3d8c88fd85941
Arjun Kesava <arjun.kesava@gmail.com>
2024-12-13 16:50:01 +0530

    chore: avatar component and storybook (#972)
    * feat: build basic types of avatar
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore: add typing and clickable props to avatar
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore: add sizes, dimensions in avatar
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore: add typing state and minor changes
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore: add aria-label
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * refactor: add fallback handle for image load
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore: add tsdoc for props and methods and storybook changes
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore: resolve type issues and add storybook changes
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * fix: rm position of avatar
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore: add type for icon name and add size for storybook
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * fix: address review comments, add new css
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore: update onclick method
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore: update onerror method
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore: add opacity for typing state
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore: update loading spinner, review comments
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore: address review comments
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore: address review comments
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore: address review comments
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore: rm button and clickable events
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore: rm button import and rm focus events
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * refactor: organize imports
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * refactor: address comments
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * style: unused css classes and variables
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore: add loading wrapper and minor changes
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * refactor: address comments, handle counter zero
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * fix: reset load state for img
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * fix: resolve counter 0 issue
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore: add converter for counter attribute
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore: updated docs; fixed storybook minor issue
    
    * refactor: switch case to record map for avatar utils
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * fix: rm custom attribute exp for counter
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * fix: reset load state for img when src is empty and fix counter
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * fix: add css props in component and disable on storybook
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * fix: update avatar class document
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    ---------
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    Co-authored-by: Supriya M <sansup49@gmail.com>

--- packages/components/src/components/avatar/avatar.constants.ts
+++ packages/components/src/components/avatar/avatar.constants.ts
@@ -1,12 +1,28 @@
 import utils from '../../utils/tag-name';
+import type { IconNames } from '../icon/icon.types';
+import { SIZE as AVATAR_SIZE } from '../presence/presence.constants';
 
 const TAG_NAME = utils.constructTagName('avatar');
 
-const LENGTH_UNIT = 'rem';
-
-const DEFAULTS = {
-  TYPE: 'photo' as const,
-  SIZE: 1.5,
+const AVATAR_TYPE = {
+  COUNTER: 'counter',
+  ICON: 'icon',
+  PHOTO: 'photo',
+  TEXT: 'text',
 } as const;
 
-export { TAG_NAME, DEFAULTS, LENGTH_UNIT };
+const MAX_COUNTER = 99;
+const ICON_NAME: IconNames = 'user-regular';
+
+const DEFAULTS = {
+  TYPE: AVATAR_TYPE.PHOTO,
+  SIZE: AVATAR_SIZE.X_SMALL,
+  ICON_NAME,
+} as const;
+
+export {
+  TAG_NAME,
+  DEFAULTS,
+  AVATAR_TYPE,
+  MAX_COUNTER,
+};
