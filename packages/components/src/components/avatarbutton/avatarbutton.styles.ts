a909731c2bf319bcf27330edefe3fbb7798bdbb2
Supriya M <sansup49@gmail.com>
2024-12-23 13:40:44 +0530

    style: focus ring on avatarbutton

--- packages/components/src/components/avatarbutton/avatarbutton.styles.ts
+++ packages/components/src/components/avatarbutton/avatarbutton.styles.ts
@@ -1,8 +1,13 @@
 import { css } from 'lit';
+import { hostFitContentStyles, hostFocusRingStyles } from '../../utils/styles';
 
-const styles = css`
+const styles = [hostFitContentStyles, css`
   :host {
+    padding: unset;
+    margin: unset;
+    outline: none;
+    border-radius: 0.25rem;
   }
-`;
+`, hostFocusRingStyles];
 
-export default [styles];
+export default styles;
