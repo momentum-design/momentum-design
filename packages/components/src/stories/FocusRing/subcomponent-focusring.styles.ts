543f98bd1343674c9baefbd72dbddc6e54e9c760
Arjun Kesava P <arjun.kesava@gmail.com>
2025-01-10 12:02:27 +0530

    chore: update focus ring styles for specific id
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>

--- packages/components/src/stories/FocusRing/subcomponent-focusring.styles.ts
+++ packages/components/src/stories/FocusRing/subcomponent-focusring.styles.ts
@@ -8,7 +8,7 @@ const styles = [
       outline: none;
     }
   `,
-  hostFocusRingStyles,
+  ...hostFocusRingStyles(),
 ];
 
 export default styles;
--- packages/components/src/utils/styles/index.ts
+++ packages/components/src/utils/styles/index.ts
@@ -1,4 +1,4 @@
-import { css } from 'lit';
+import { css, unsafeCSS } from 'lit';
 
 const hostFitContentStyles = css`
   :host {
@@ -10,36 +10,54 @@ const hostFitContentStyles = css`
   }
 `;
 
-const hostFocusRingStyles = css`
-  :host {
-    --mdc-focus-ring-inner-color: var(--mds-color-theme-focus-default-0);
-    --mdc-focus-ring-middle-color: var(--mds-color-theme-focus-default-1);
-    --mdc-focus-ring-outer-color: var(--mds-color-theme-focus-default-2); 
+const hostFocusRingStyles = (className?: string) => {
+  const baseHostStyleVariables = css`
+    :host {
+      --mdc-focus-ring-inner-color: var(--mds-color-theme-focus-default-0);
+      --mdc-focus-ring-middle-color: var(--mds-color-theme-focus-default-1);
+      --mdc-focus-ring-outer-color: var(--mds-color-theme-focus-default-2);
 
-    --mdc-focus-ring-inner-width: 0.125rem;
-    --mdc-focus-ring-middle-width: calc(2 * var(--mdc-focus-ring-inner-width));
-    --mdc-focus-ring-outer-width: calc(0.0625rem + var(--mdc-focus-ring-middle-width));
+      --mdc-focus-ring-inner-width: 0.125rem;
+      --mdc-focus-ring-middle-width: calc(2 * var(--mdc-focus-ring-inner-width));
+      --mdc-focus-ring-outer-width: calc(0.0625rem + var(--mdc-focus-ring-middle-width));
 
-    --mdc-focus-ring-middle-offset: var(--mdc-focus-ring-inner-width);
-    --mdc-focus-ring-outer-offset: calc(var(--mdc-focus-ring-inner-width) + var(--mdc-focus-ring-middle-width));
-  }
-  :host([disabled]:focus) {
-    box-shadow: none;
-  }
-  :host(:focus) {
-    position: relative;
-    box-shadow: 
-        0 0 0 var(--mdc-focus-ring-inner-width) var(--mdc-focus-ring-inner-color),
-        0 0 0 var(--mdc-focus-ring-middle-width) var(--mdc-focus-ring-middle-color),
-        0 0 0 var(--mdc-focus-ring-outer-width) var(--mdc-focus-ring-outer-color);
-  }
-
-  /* High Contrast Mode */
-  @media (forced-colors: active) {
-    :host(:focus) {
-      outline: 0.125rem solid var(--mds-color-theme-focus-default-0);
+      --mdc-focus-ring-middle-offset: var(--mdc-focus-ring-inner-width);
+      --mdc-focus-ring-outer-offset: calc(var(--mdc-focus-ring-inner-width) + var(--mdc-focus-ring-middle-width));
     }
+  `;
+  const boxShadow = css`0 0 0 var(--mdc-focus-ring-inner-width) var(--mdc-focus-ring-inner-color),
+    0 0 0 var(--mdc-focus-ring-middle-width) var(--mdc-focus-ring-middle-color),
+    0 0 0 var(--mdc-focus-ring-outer-width) var(--mdc-focus-ring-outer-color)
+  `;
+  if (className) {
+    return [
+      baseHostStyleVariables,
+      css`
+        .${unsafeCSS(className)}:focus-visible {
+          outline: none;
+        }
+        :host([disabled]) .${unsafeCSS(className)}:focus {
+          box-shadow: none;
+        }
+        .${unsafeCSS(className)}:focus {
+          position: relative;
+          box-shadow: ${boxShadow};
+        }
+      `,
+    ];
   }
-`;
+  return [
+    baseHostStyleVariables,
+    css`
+      :host([disabled]:focus) {
+        box-shadow: none;
+      }
+      :host(:focus) {
+        position: relative;
+        box-shadow: ${boxShadow};
+      }
+    `,
+  ];
+};
 
 export { hostFitContentStyles, hostFocusRingStyles };
