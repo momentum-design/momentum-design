2bdf739c2c8b8b8f4c0d1f9982aeac82ff4daf72
Arjun Kesava P <arjun.kesava@gmail.com>
2025-01-16 14:33:34 +0530

    chore: init commit of checkbox component
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>

---
+++ packages/components/src/components/checkbox/checkbox.stories.ts
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from '@storybook/web-components';
+import '.';
+import { html } from 'lit';
+import { classArgType, styleArgType } from '../../../config/storybook/commonArgTypes';
+
+const render = () => html`<mdc-checkbox></mdc-checkbox>`;
+
+const meta: Meta = {
+  title: 'Work In Progress/checkbox',
+  tags: ['autodocs'],
+  component: 'mdc-checkbox',
+  render,
+  parameters: {
+    badges: ['wip'],
+  },
+  argTypes: {
+    ...classArgType,
+    ...styleArgType,
+  },
+};
+
+export default meta;
+
+export const Example: StoryObj = {
+  args: {
+    class: 'custom-classname',
+    style: 'margin-top: 20px;',
+  },
+};
