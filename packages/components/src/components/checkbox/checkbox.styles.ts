6e6b3c7219606aaa8e44a88dd77c4cc28330c2f9
Arjun Kesava P <arjun.kesava@gmail.com>
2025-01-17 21:10:49 +0530

    chore: set a11y and styles for checkbox
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>

--- packages/components/src/components/checkbox/checkbox.styles.ts
+++ packages/components/src/components/checkbox/checkbox.styles.ts
@@ -2,6 +2,26 @@ import { css } from 'lit';
 
 const styles = css`
   :host {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    height: 2rem;
+  }
+  .mdc-checkbox__native-control {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    width: 1rem;
+    height: 1rem;
+    border: 1px solid var(--mds-color-theme-outline-button-normal);
+    border-radius: 0.125rem;
+    background: none;
+  }
+  .mdc-checkbox__native-control:checked {
+    -webkit-appearance: revert;
+    -moz-appearance: revert;
+    appearance: revert;
+    accent-color: var(--mds-color-theme-control-active-normal);
   }
 `;
 
