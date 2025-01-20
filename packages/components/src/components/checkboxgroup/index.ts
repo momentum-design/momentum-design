c9db73799cd5ed2f7dd7317c1e1d60f44724a642
Arjun Kesava P <arjun.kesava@gmail.com>
2025-01-20 11:43:04 +0530

    refactor: add generate comp script for checkboxgroup component
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>

---
+++ packages/components/src/components/checkboxgroup/index.ts
@@ -0,0 +1,12 @@
+import Checkboxgroup from './checkboxgroup.component';
+import { TAG_NAME } from './checkboxgroup.constants';
+
+Checkboxgroup.register(TAG_NAME);
+
+declare global {
+    interface HTMLElementTagNameMap {
+        ['mdc-checkboxgroup']: Checkboxgroup
+    }
+}
+
+export default Checkboxgroup;
