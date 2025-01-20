58e3e46927bd5a9ffe927d5e48df1d6814defcc9
Arjun Kesava P <arjun.kesava@gmail.com>
2025-01-20 17:17:05 +0530

    chore: add parent checkbox header in checkbox group
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>

--- packages/components/src/components/checkboxgroup/checkboxgroup.stories.ts
+++ packages/components/src/components/checkboxgroup/checkboxgroup.stories.ts
@@ -1,10 +1,10 @@
-import type { Meta, StoryObj, Args } from '@storybook/web-components';
+import type { Meta, StoryObj } from '@storybook/web-components';
 import '.';
 import { html } from 'lit';
 import { classArgType, styleArgType } from '../../../config/storybook/commonArgTypes';
 
-const render = (args: Args) => html`
-  <mdc-checkboxgroup args.property="${args.property}"></mdc-checkboxgroup>`;
+const render = () => html`
+  <mdc-checkboxgroup></mdc-checkboxgroup>`;
 
 const meta: Meta = {
   title: 'Work In Progress/checkboxgroup',
@@ -27,4 +27,45 @@ export const Example: StoryObj = {
     class: 'custom-classname',
     style: 'margin-top: 20px;',
   },
+};
+
+export const Label: StoryObj = {
+  render: () => html`
+    <mdc-checkboxgroup label="Select one bro">
+      <mdc-checkbox label="Checkbox Label"></mdc-checkbox>
+      <mdc-checkbox label="Checkbox Label"></mdc-checkbox>
+      <mdc-checkbox label="Checkbox Label"></mdc-checkbox>
+      <mdc-checkbox label="Checkbox Label"></mdc-checkbox>
+      <mdc-checkbox label="Checkbox Label"></mdc-checkbox>
+      <mdc-checkbox label="Checkbox Label"></mdc-checkbox>
+    </mdc-checkboxgroup>
+  `,
+};
+
+export const Parent: StoryObj = {
+  render: () => html`
+    <mdc-checkboxgroup type="parent" label="Tomato is good">
+      <mdc-checkbox label="1 Checkbox Label"></mdc-checkbox>
+      <mdc-checkbox label="1 Checkbox Label"></mdc-checkbox>
+      <mdc-checkbox label="1 Checkbox Label"></mdc-checkbox>
+      <mdc-checkbox label="1 Checkbox Label"></mdc-checkbox>
+    </mdc-checkboxgroup>
+  `,
+};
+
+export const Nested: StoryObj = {
+  render: () => html`
+    <mdc-checkboxgroup type="parent" label="Tomato is good">
+      <mdc-checkbox label="1 Checkbox Label"></mdc-checkbox>
+      <mdc-checkboxgroup type="parent" label="Orange is good">
+        <mdc-checkbox label="1 Checkbox Label"></mdc-checkbox>
+        <mdc-checkbox label="1 Checkbox Label"></mdc-checkbox>
+      </mdc-checkboxgroup>
+      <mdc-checkbox label="1 Checkbox Label"></mdc-checkbox>
+      <mdc-checkbox label="1 Checkbox Label"></mdc-checkbox>
+      <mdc-checkbox label="1 Checkbox Label"></mdc-checkbox>
+      <mdc-checkbox label="1 Checkbox Label"></mdc-checkbox>
+    </mdc-checkboxgroup>
+    
+  `,
 };
