47634354b4c4e5201e10646268e8272bae876fc3
shakti97 <shaktisingh230797@gmail.com>
2025-01-29 15:12:51 +0530

    chore: update event handling in radio btn

--- packages/components/src/components/radio/radio.component.ts
+++ packages/components/src/components/radio/radio.component.ts
@@ -1,6 +1,4 @@
 import { CSSResult, html } from 'lit';
-import { classMap } from 'lit-html/directives/class-map.js';
-import { v4 as uuidv4 } from 'uuid';
 import { property } from 'lit/decorators.js';
 import { ifDefined } from 'lit/directives/if-defined.js';
 import { DisabledMixin } from '../../utils/mixins/DisabledMixin';
@@ -14,9 +12,7 @@ import FormfieldWrapper from '../formfieldwrapper/formfieldwrapper.component';
  * These are often used in forms, settings, and selection in lists.
  *
  * A radio component contains an optional label, optional info icon and an optional helper text.
- *
- * @dependency mdc-icon
- * @dependency mdc-labelandhelper
+ * @dependency mdc-formfieldwrapper
  *
  * @tagname mdc-radio
  */
@@ -35,67 +31,104 @@ class Radio extends NameMixin(ValueMixin(DisabledMixin(FormfieldWrapper))) {
   */
   @property({ type: Boolean, reflect: true }) readonly = false;
 
-  /**
-  * The unique id of the input field. It is used to link the input field with the label.
-  * @default `mdc-input-${uuidv4()}`
-  */
-  @property({ type: String }) override id = `mdc-input-${uuidv4()}`;
+  override firstUpdated() {
+    this.updateTabIndex();
+  }
 
   /**
-   * Triggers a change event on the radio element.
-   * This is used to dispatch the change event.
+   * Returns all radios within the same group (name).
    */
-  private triggerChangeEvent(): void {
-    const changeEvent = new CustomEvent('change', {
-      bubbles: true,
-      composed: true,
-      cancelable: true,
-      detail: {
-        checked: this.checked,
-      },
+  private getAllRadiosWithinSameGroup(): Radio[] {
+    return Array.from(document.querySelectorAll(`mdc-radio[name="${this.name}"]`));
+  }
+
+  private dispatchChangeEvent(event: Event): void {
+    const EventConstructor = event.constructor as typeof Event;
+    this.dispatchEvent(new EventConstructor(event.type, event));
+  }
+
+  /**
+   * Handles the change event on the radio element.
+   * This will toggle the state of the radio element.
+   * Dispatches the change event.
+   */
+  private handleChange(event: Event): void {
+    if (this.disabled || this.readonly) return;
+
+    const radios = this.getAllRadiosWithinSameGroup();
+    radios.forEach((radio) => {
+      /**
+        *  Uncheck all radios in the same group (name)
+      */
+      const radioHostElement = radio;
+      const radioElement = radio.shadowRoot?.querySelector('input');
+      if (radioElement) {
+        radioHostElement.checked = false;
+        radioElement.checked = false;
+      }
     });
-    this.dispatchEvent(changeEvent);
+    this.checked = true;
+    this.dispatchChangeEvent(event);
   }
 
   /**
-   * Toggles the state of the radio element.
-   * If the radio element is not disabled,
-   * then we will toggle its checked state
-   * and also trigger a change event.
-   */
-  private toggleState(): void {
-    if (!this.disabled && !this.readonly) {
-      this.checked = !this.checked;
-      this.triggerChangeEvent();
-    }
-  }
-
-  /**
-   * Handles the keydown event on the radio.
-   * If the key is 'Enter' or 'Space', the radio is toggled.
-   *
-   * @param event - The keyboard event.
+   * Handles the keydown event(Arrow Up/Down/Left/Right) on the radio element.
+   * This will move focus to the next or previous radio element in the group.
    */
   private handleKeyDown(event: KeyboardEvent): void {
-    if (this.disabled && this.readonly) {
-      return;
+    if (this.disabled || this.readonly) return;
+
+    const radios = this.getAllRadiosWithinSameGroup();
+    const enabledRadios = radios.filter((radio) => !radio.disabled && !radio.readonly);
+    const currentIndex = enabledRadios.indexOf(this);
+
+    if (['ArrowDown', 'ArrowRight'].includes(event.key)) {
+      // Move focus to the next radio
+      const nextIndex = (currentIndex + 1) % enabledRadios.length;
+      enabledRadios[nextIndex].shadowRoot?.querySelector('input')?.focus();
+      enabledRadios[nextIndex].handleChange(event);
+    } else if (['ArrowUp', 'ArrowLeft'].includes(event.key)) {
+      // Move focus to the previous radio
+      const prevIndex = (currentIndex - 1 + enabledRadios.length) % enabledRadios.length;
+      enabledRadios[prevIndex].shadowRoot?.querySelector('input')?.focus();
+      enabledRadios[prevIndex].handleChange(event);
     }
-    if (['Enter', ' '].includes(event.key)) {
-      event.preventDefault();
-      this.toggleState();
-    }
+    this.updateTabIndex();
+    this.dispatchChangeEvent(event);
+  }
+
+  /**
+   * Update tab index for all radios in the same group (name)
+   * If any radio group is checked, it will have a tab index of 0
+   * If no radio group is checked, the first enabled radio will have a tab index of 0
+   */
+  private updateTabIndex(): void {
+    const radios = this.getAllRadiosWithinSameGroup();
+    const checked = radios.find((radio) => radio.checked);
+    const firstEnabledRadio = radios.find((radio) => !radio.disabled);
+    radios.forEach((radio) => {
+      const inputElement = radio.shadowRoot?.querySelector('input');
+      if (inputElement) {
+        inputElement.tabIndex = -1;
+        if (radio === checked) {
+          inputElement.tabIndex = 0;
+        } else if (!checked && radio === firstEnabledRadio) {
+          inputElement.tabIndex = 0;
+        }
+      }
+    });
   }
 
   public override render() {
     return html`
       <div class="mdc-radio__container">
-        <div class="mdc-radio__icon-container ${classMap({ 'mdc-focus-ring': !this.disabled && !this.readonly })}">
+        <div class="mdc-radio__icon-container mdc-focus-ring">
           <input
             id="${this.id}"
             type="radio"
             name="${ifDefined(this.name)}"
             value="${ifDefined(this.value)}"
-            @click=${this.toggleState}
+            @change=${this.handleChange}
             @keydown=${this.handleKeyDown}
             ?checked="${this.checked}"
             ?readonly="${this.readonly}"
@@ -104,7 +137,6 @@ class Radio extends NameMixin(ValueMixin(DisabledMixin(FormfieldWrapper))) {
             aria-checked="${this.checked}"
             aria-disabled="${this.disabled}"
             aria-label="${ifDefined(this.label || this.name)}"
-            tabindex="${this.disabled ? -1 : 0}"
           />
           <span class="mdc-radio__icon"></span>
         </div>
