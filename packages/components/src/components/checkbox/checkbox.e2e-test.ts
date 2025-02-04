878c084dae1939dfeb6be01707a6c61146edb8e9
Arjun Kesava P <arjun.kesava@gmail.com>
2025-02-04 16:33:05 +0530

    fix: rm unused await and update screenshots with a margin
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>

--- packages/components/src/components/checkbox/checkbox.e2e-test.ts
+++ packages/components/src/components/checkbox/checkbox.e2e-test.ts
@@ -43,54 +43,63 @@ test('mdc-checkbox', async ({ componentsPage }) => {
    */
   await test.step('visual-regression', async () => {
     const checkboxStickerSheet = new StickerSheet(componentsPage, 'mdc-checkbox');
-    await checkboxStickerSheet.setAttributes({
+    checkboxStickerSheet.setAttributes({
       'data-aria-label': 'This is aria label text',
+      style: 'margin: 0.25rem',
     });
     await checkboxStickerSheet.createMarkupWithCombination({}, true);
-    await checkboxStickerSheet.setAttributes({
+    checkboxStickerSheet.setAttributes({
       label: 'I agree to the terms',
+      style: 'margin: 0.25rem',
     });
     await checkboxStickerSheet.createMarkupWithCombination({}, true);
-    await checkboxStickerSheet.setAttributes({
+    checkboxStickerSheet.setAttributes({
       label: 'Selected Checkbox Label',
       checked: true,
+      style: 'margin: 0.25rem',
     });
     await checkboxStickerSheet.createMarkupWithCombination({}, true);
-    await checkboxStickerSheet.setAttributes({
+    checkboxStickerSheet.setAttributes({
       label: 'Indeterminate Checkbox Label',
       indeterminate: true,
+      style: 'margin: 0.25rem',
     });
     await checkboxStickerSheet.createMarkupWithCombination({}, true);
-    await checkboxStickerSheet.setAttributes({
+    checkboxStickerSheet.setAttributes({
       label: 'Selected Checkbox Label',
       'help-text': 'This is a help text',
       checked: true,
+      style: 'margin: 0.25rem',
     });
     await checkboxStickerSheet.createMarkupWithCombination({}, true);
-    await checkboxStickerSheet.setAttributes({
+    checkboxStickerSheet.setAttributes({
       label: 'Indeterminate Checkbox Label',
       'help-text': 'This is a help text',
       indeterminate: true,
+      style: 'margin: 0.25rem',
     });
     await checkboxStickerSheet.createMarkupWithCombination({}, true);
-    await checkboxStickerSheet.setAttributes({
+    checkboxStickerSheet.setAttributes({
       label: 'Disabled Checkbox Label',
       'help-text': 'This is a help text',
       disabled: true,
+      style: 'margin: 0.25rem',
     });
     await checkboxStickerSheet.createMarkupWithCombination({}, true);
-    await checkboxStickerSheet.setAttributes({
+    checkboxStickerSheet.setAttributes({
       label: 'Disabled Selected Checkbox Label',
       'help-text': 'This is a help text',
       disabled: true,
       checked: true,
+      style: 'margin: 0.25rem',
     });
     await checkboxStickerSheet.createMarkupWithCombination({}, true);
-    await checkboxStickerSheet.setAttributes({
+    checkboxStickerSheet.setAttributes({
       label: 'Disabled Indeterminate Checkbox Label',
       'help-text': 'This is a help text',
       disabled: true,
       indeterminate: true,
+      style: 'margin: 0.25rem',
     });
     await checkboxStickerSheet.createMarkupWithCombination({}, true);
     await checkboxStickerSheet.mountStickerSheet();
@@ -106,9 +115,7 @@ test('mdc-checkbox', async ({ componentsPage }) => {
    * ACCESSIBILITY
    */
   await test.step('accessibility', async () => {
-    await componentsPage.accessibility.checkForA11yViolations('checkbox-default', true, {
-      rules: ['target-size'],
-    });
+    await componentsPage.accessibility.checkForA11yViolations('checkbox-default');
   });
 
   /**
@@ -118,20 +125,19 @@ test('mdc-checkbox', async ({ componentsPage }) => {
     const checkbox = await setup({ componentsPage });
 
     await test.step('should fallback to default icon and size to 32 when no attributes are passed', async () => {
-      await componentsPage.setAttributes(checkbox, {});
       await expect(checkbox).toHaveAttribute('name');
       await expect(checkbox).toHaveAttribute('value');
     });
 
     await test.step('should have label element when the label attribute is passed', async () => {
       await componentsPage.setAttributes(checkbox, { label: 'Checkbox label' });
-      const label = await componentsPage.page.locator('label');
+      const label = componentsPage.page.locator('label');
       await expect(label).toHaveText('Checkbox label');
     });
 
     await test.step('should have mdc-text element when the help-text attribute is passed', async () => {
       await componentsPage.setAttributes(checkbox, { 'help-text': 'This is a help text' });
-      const mdcText = await componentsPage.page.locator('mdc-text');
+      const mdcText = componentsPage.page.locator('mdc-text');
       const textContent = await mdcText.textContent();
       expect(textContent?.trim()).toBe('This is a help text');
     });
@@ -140,7 +146,7 @@ test('mdc-checkbox', async ({ componentsPage }) => {
       when the indeterminate attribute is passed`, async () => {
       await componentsPage.removeAttribute(checkbox, 'checked');
       await componentsPage.setAttributes(checkbox, { indeterminate: '' });
-      const icon = await componentsPage.page.locator('mdc-icon');
+      const icon = componentsPage.page.locator('mdc-icon');
       await icon.waitFor();
       await expect(icon).toHaveAttribute('name', 'minus-bold');
     });
@@ -149,14 +155,14 @@ test('mdc-checkbox', async ({ componentsPage }) => {
       when the checked attribute is passed`, async () => {
       await componentsPage.removeAttribute(checkbox, 'indeterminate');
       await componentsPage.setAttributes(checkbox, { checked: '' });
-      const icon = await componentsPage.page.locator('mdc-icon');
+      const icon = componentsPage.page.locator('mdc-icon');
       await icon.waitFor();
       await expect(icon).toHaveAttribute('name', 'check-bold');
     });
 
     await test.step('should have disabled attribute when the disabled attribute is passed', async () => {
       await componentsPage.setAttributes(checkbox, { disabled: '' });
-      await expect(checkbox).toHaveAttribute('disabled');
+      await expect(checkbox.locator('input[type="checkbox"]')).toBeDisabled();
     });
   });
 
@@ -180,20 +186,20 @@ test('mdc-checkbox', async ({ componentsPage }) => {
       await expect(checkbox).toBeFocused();
 
       await componentsPage.page.keyboard.press('Space');
-      await expect(checkbox).toHaveAttribute('checked');
+      await expect(checkbox.locator('input[type="checkbox"]')).toBeChecked();
 
       await componentsPage.page.keyboard.press('Space');
-      await expect(checkbox).not.toHaveAttribute('checked');
+      await expect(checkbox.locator('input[type="checkbox"]')).not.toBeChecked();
     });
 
     await test.step('checkbox should be checked when clicked', async () => {
       const checkbox = await setup({ componentsPage });
 
       await checkbox.click();
-      await expect(checkbox).toHaveAttribute('checked');
+      await expect(checkbox.locator('input[type="checkbox"]')).toBeChecked();
 
       await checkbox.click();
-      await expect(checkbox).not.toHaveAttribute('checked');
+      await expect(checkbox.locator('input[type="checkbox"]')).not.toBeChecked();
     });
 
     await test.step('checkbox should not be Focused or checked when its disabled', async () => {
@@ -203,7 +209,7 @@ test('mdc-checkbox', async ({ componentsPage }) => {
       await expect(checkbox).not.toBeFocused();
 
       await checkbox.click();
-      await expect(checkbox).not.toHaveAttribute('checked');
+      await expect(checkbox.locator('input[type="checkbox"]')).not.toBeChecked();
     });
   });
 });
