e62171d7d0d1c75c2c44d3bf5ffd8f09b0176540
jsoter <jsoter@cisco.com>
2025-02-05 16:19:11 -0500

    fix: completed storybook

--- packages/components/src/components/virtualizedlist/virtualizedlist.types.ts
+++ packages/components/src/components/virtualizedlist/virtualizedlist.types.ts
@@ -1,12 +1,8 @@
 import { VirtualItem } from '@tanstack/virtual-core';
 import { StyleInfo } from 'lit/directives/style-map.js';
 
-export type ScrollDirection = 'forward' | 'backward' | null
-
-interface VirtualizerUpdatedDetail {
+export interface SetListDataProps {
   virtualItems: Array<VirtualItem>;
   measureElement: (node: Element | null | undefined) => void;
   listStyle: Readonly<StyleInfo>;
 }
-
-export type VirtualizerUpdatedEvent = CustomEvent<VirtualizerUpdatedDetail>;
