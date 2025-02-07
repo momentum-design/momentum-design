1c0e76d814e5c8da9f57b71598d9ab59dc4761ca
shakti97 <shaktisingh230797@gmail.com>
2025-02-07 11:14:31 +0530

    chore: added label and description to the group

--- packages/components/src/components/radio-group/radio-group.component.ts
+++ packages/components/src/components/radio-group/radio-group.component.ts
@@ -1,19 +1,36 @@
 import { CSSResult, html } from 'lit';
-import styles from './radio-group.styles';
+import { property } from 'lit/decorators.js';
 import { Component } from '../../models';
+import styles from './radio-group.styles';
 
 /**
- * radio-group component, which ...
+ * Radio Group Component - This is the wrapper component for radio buttons which are grouped together.
  *
  * @tagname mdc-radio-group
  *
  * @slot default - This is a default/unnamed slot
  *
- * @cssprop --custom-property-name - Description of the CSS custom property
  */
 class RadioGroup extends Component {
+  /**
+   * The label of the radio group.
+   * @default ''
+   */
+  @property({ type: String }) label = '';
+
+  /**
+   * The description of the radio group.
+   * @default ''
+   */
+  @property({ type: String }) description = '';
+
   public override render() {
-    return html`<slot></slot>`;
+    return html`
+        ${this.label ? html`<span id="mdc-radio-group__label">${this.label}</span>` : ''}
+        ${this.description ? html`<span id="mdc-radio-group__label">${this.description}</span>` : ''}
+        <div class="mdc-radio-group" role="radiogroup" aria-labelledby="${this.label ? 'radio-group-label' : ''}">
+          <slot></slot>
+        </div>`;
   }
 
   public static override styles: Array<CSSResult> = [...Component.styles, ...styles];
