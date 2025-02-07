1ae378cc4c9b22a21d852c510d26ba68c4b54cb0
Supriya M <30731236+supminn@users.noreply.github.com>
2025-02-07 23:43:32 +0530

    refactor: input component extending internal label and helper (#1031)
    * feat: helper text with label as internal component
    
    * chore: updated story
    
    * fix: review comments addressed
    
    * chore: updated storybook
    
    * refactor: input component using internal helper label
    
    * fix: updated function name
    
    * fix: function name
    
    * feat: mixin for required and readonly
    
    * fix: updated default value of id
    
    * refactor: review comments addressed
    
    * style: helper text color
    
    * fix: prettier cleaning
    
    * fix: indentation issue
    
    * remove class and style
    
    * refactor: renamed the class and element
    
    * feat: mixin for value and name properties
    
    * fix: type imports
    
    * refactor: updated based on parent PR changes
    
    * fix: type import
    
    * fix: property name
    
    * chore: renamed labelandhelper to formfieldwrapper
    
    * fix: focus issue when button is hidden
    
    * fix: removed type; updatede component width
    
    * fix: making trailing button optional
    
    * style: fix focused style
    
    * refactor: support for all attributes that native input supports
    
    * fix: removed label-info-text attribute
    
    * chore: minor fix
    
    * fix: support for form internals
    
    * fix: form support for mdc input
    
    * style: fix for disabled state
    
    * style: fixes based on figma color spec
    
    * fix: icon fixes on helper text types
    
    * refactor: updated css variables and documentation
    
    * fix: disable controls on css variables
    
    * style: updating icon colors
    
    * fix: addressing review comments
    
    * fix: review comments
    
    * fix: aria label for trailing button
    
    * refactor: actions for input
    
    * refactor: storybook example with form
    
    * refactor: review comments addressed with fixes
    
    * chore: renamed data aria label
    
    * fix: adding name attribute
    
    * style: disabled fixes
    
    * fix: validation and spacing
    
    * refactor: using constants from exisiting components
    
    * fix: typo for aria label
    
    * refactor: updated onchange dispatch event
    
    * chore: cleanup state
    
    * fix: auto merge issue
    
    * fix: aria label to read prefix text
    
    * fix: styling and a11y
    
    * fix: prefix text error message removed
    
    * fix: a11y issue for prefix text
    
    ---------
    
    Co-authored-by: Arjun Kesava <arjun.kesava@gmail.com>
    Co-authored-by: Deeya Upadhyay <58636958+2020Deeya@users.noreply.github.com>

---
+++ packages/components/src/components/input/input.constants.ts
@@ -0,0 +1,34 @@
+import utils from '../../utils/tag-name';
+import { BUTTON_VARIANTS, ICON_BUTTON_SIZES } from '../button/button.constants';
+import { VALIDATION } from '../formfieldwrapper/formfieldwrapper.constants';
+import { TYPE, VALID_TEXT_TAGS } from '../text/text.constants';
+
+const TAG_NAME = utils.constructTagName('input');
+
+const AUTO_CAPITALIZE = {
+  OFF: 'off',
+  NONE: 'none',
+  ON: 'on',
+  SENTENCES: 'sentences',
+  WORDS: 'words',
+  CHARACTERS: 'characters',
+} as const;
+
+const PREFIX_TEXT_OPTIONS = {
+  MAX_LENGTH: 10,
+  HELPERTEXT: 'Prefix text should not exceed 10 characters',
+  VALIDATION: VALIDATION.ERROR,
+};
+
+const DEFAULTS = {
+  VALIDATION: VALIDATION.DEFAULT,
+  ICON_SIZE_VALUE: 1,
+  ICON_SIZE_UNIT: 'rem',
+  CLEAR_BUTTON_ICON: 'cancel-bold',
+  CLEAR_BUTTON_VARIANT: BUTTON_VARIANTS.TERTIARY,
+  CLEAR_BUTTON_SIZE: ICON_BUTTON_SIZES[20],
+  PREFIX_TEXT_TYPE: TYPE.BODY_MIDSIZE_REGULAR,
+  PREFIX_TEXT_TAG: VALID_TEXT_TAGS.SPAN,
+};
+
+export { TAG_NAME, PREFIX_TEXT_OPTIONS, AUTO_CAPITALIZE, DEFAULTS };
--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -10,6 +10,7 @@ import Bullet from './components/bullet';
 import Marker from './components/marker';
 import Divider from './components/divider';
 import AvatarButton from './components/avatarbutton';
+import Input from './components/input';
 import Link from './components/link';
 import Checkbox from './components/checkbox';
 
@@ -28,6 +29,7 @@ export {
   Marker,
   Divider,
   AvatarButton,
+  Input,
   Link,
   Checkbox,
 };
