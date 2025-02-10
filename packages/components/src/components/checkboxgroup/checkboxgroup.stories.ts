b30ce8a31bf8a5ec5af57c85fc36fdc3c3ce1794
Arjun Kesava P <arjun.kesava@gmail.com>
2025-02-10 18:11:16 +0530

    chore: add form example for checkboxgroup story
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>

--- packages/components/src/components/checkboxgroup/checkboxgroup.stories.ts
+++ packages/components/src/components/checkboxgroup/checkboxgroup.stories.ts
@@ -1,16 +1,66 @@
-import type { Meta, StoryObj } from '@storybook/web-components';
-import '.';
+import { action } from '@storybook/addon-actions';
+import type { Args, Meta, StoryObj } from '@storybook/web-components';
 import { html } from 'lit';
+import '.';
 import { classArgType, styleArgType } from '../../../config/storybook/commonArgTypes';
+import { disableControls } from '../../../config/storybook/utils';
+import '../button';
+import { TYPE } from './checkboxgroup.constants';
+
+const render = (args: Args) => html`
+  <mdc-checkboxgroup
+    label="${args.label}"
+    type="${args.type}"
+    .disabled="${args.disabled}"
+    data-aria-label="${args['data-aria-label']}"
+    header-text="${args['header-text']}"
+  >
+    <mdc-checkbox label="Checkbox Label"></mdc-checkbox>
+    <mdc-checkbox label="Checkbox Label"></mdc-checkbox>
+    <mdc-checkbox label="Checkbox Label"></mdc-checkbox>
+    <mdc-checkbox label="Checkbox Label"></mdc-checkbox>
+    <mdc-checkbox label="Checkbox Label"></mdc-checkbox>
+    <mdc-checkbox label="Checkbox Label"></mdc-checkbox>
+  </mdc-checkboxgroup>
+`;
 
 const meta: Meta = {
   title: 'Work In Progress/checkboxgroup',
   tags: ['autodocs'],
+  render,
   component: 'mdc-checkboxgroup',
   parameters: {
     badges: ['wip'],
   },
   argTypes: {
+    label: {
+      control: 'text',
+      if: {
+        arg: 'type',
+        eq: TYPE.PARENT,
+      },
+    },
+    'header-text': {
+      control: 'text',
+      if: {
+        arg: 'type',
+        neq: TYPE.PARENT,
+      },
+    },
+    disabled: {
+      control: 'boolean',
+      if: {
+        arg: 'type',
+        eq: TYPE.PARENT,
+      },
+    },
+    'data-aria-label': {
+      control: 'text',
+    },
+    type: {
+      control: 'radio',
+      options: Object.values(TYPE),
+    },
     ...classArgType,
     ...styleArgType,
   },
@@ -19,16 +69,11 @@ const meta: Meta = {
 export default meta;
 
 export const Example: StoryObj = {
-  render: () => html`
-    <mdc-checkboxgroup header-text="Checkbox group label">
-      <mdc-checkbox label="Checkbox Label"></mdc-checkbox>
-      <mdc-checkbox label="Checkbox Label"></mdc-checkbox>
-      <mdc-checkbox label="Checkbox Label"></mdc-checkbox>
-      <mdc-checkbox label="Checkbox Label"></mdc-checkbox>
-      <mdc-checkbox label="Checkbox Label"></mdc-checkbox>
-      <mdc-checkbox label="Checkbox Label"></mdc-checkbox>
-    </mdc-checkboxgroup>
-  `,
+  args: {
+    'header-text': 'Checkbox group label',
+    label: 'Select all checkbox groups',
+    type: TYPE.NONE,
+  },
 };
 
 export const List: StoryObj = {
@@ -42,6 +87,9 @@ export const List: StoryObj = {
       <mdc-checkbox label="Thor"></mdc-checkbox>
     </mdc-checkboxgroup>
   `,
+  argTypes: {
+    ...disableControls(['type', 'header-text', 'data-aria-label']),
+  },
 };
 
 export const ParentList: StoryObj = {
@@ -59,4 +107,41 @@ export const ParentList: StoryObj = {
       <mdc-checkbox label="Weather control"></mdc-checkbox>
     </mdc-checkboxgroup>
   `,
+  argTypes: {
+    ...disableControls(['type', 'header-text', 'data-aria-label']),
+  },
+};
+
+export const FormField: StoryObj = {
+  render: () => {
+    const handleSubmit = (event: Event) => {
+      event.preventDefault();
+      const formData = new FormData(event.target as HTMLFormElement);
+      const selectedValues = formData.getAll('super-power');
+      action('Form Submitted')({ value: selectedValues });
+    };
+
+    return html`
+      <form @submit=${handleSubmit}>
+        <div style="display: flex; flex-direction: column; gap: 1rem;">
+          <mdc-checkboxgroup
+            data-aria-label="Super powers (check all that apply)"
+            label="Super powers (check all that apply)"
+            type="parent"
+          >
+            <mdc-checkbox label="Flight" value="flight" name="super-power"></mdc-checkbox>
+            <mdc-checkbox label="Mind Control" value="mind-control" name="super-power"></mdc-checkbox>
+            <mdc-checkbox label="Super genius" disabled value="super-genius" name="super-power"></mdc-checkbox>
+            <mdc-checkbox label="Super strength" value="super-strength" name="super-power"></mdc-checkbox>
+            <mdc-checkbox label="Tactics" value="tactics" name="super-power"></mdc-checkbox>
+            <mdc-checkbox label="Weather control" disabled value="weather-control" name="super-power"></mdc-checkbox>
+          </mdc-checkboxgroup>
+          <mdc-button type="submit">Submit</mdc-button>
+        </div>
+      </form>
+  `;
+  },
+  argTypes: {
+    ...disableControls(['type', 'header-text', 'data-aria-label']),
+  },
 };
