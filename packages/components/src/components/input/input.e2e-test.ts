489a2894180ff0e940eec1ca091a871bacd31a7f
Supriya M <30731236+supminn@users.noreply.github.com>
2025-02-11 08:52:51 +0530

    test: e2e test cases for input component (#1080)
    * feat: helper text with label as internal component
    
    * chore: updated story
    
    * fix: review comments addressed
    
    * chore: updated storybook
    
    * refactor: input component using internal helper label
    
    * fix: updated function name
    
    * fix: function name
    
    * feat: mixin for required and readonly
    
    * fix: updated default value of id
    
    * refactor: review comments addressed
    
    * style: helper text color
    
    * fix: prettier cleaning
    
    * fix: indentation issue
    
    * remove class and style
    
    * refactor: renamed the class and element
    
    * feat: mixin for value and name properties
    
    * fix: type imports
    
    * refactor: updated based on parent PR changes
    
    * fix: type import
    
    * fix: property name
    
    * chore: renamed labelandhelper to formfieldwrapper
    
    * fix: focus issue when button is hidden
    
    * fix: removed type; updatede component width
    
    * fix: making trailing button optional
    
    * style: fix focused style
    
    * refactor: support for all attributes that native input supports
    
    * fix: removed label-info-text attribute
    
    * test(wip): input component attributes
    
    * test: test cases for attributes
    
    * chore: minor fix
    
    * test: visual regression snapshots
    
    * fix: support for form internals
    
    * fix: form support for mdc input
    
    * style: fix for disabled state
    
    * chore: updated snapshots
    
    * test: interaction testing and form validation
    
    * test: updated test cases
    
    * style: fixes based on figma color spec
    
    * fix: icon fixes on helper text types
    
    * chore: updated snapshots based on icon changes on helper text type
    
    * refactor: updated css variables and documentation
    
    * fix: disable controls on css variables
    
    * style: updating icon colors
    
    * chore: updated snapshot order
    
    * fix: addressing review comments
    
    * fix: review comments
    
    * fix: aria label for trailing button
    
    * refactor: actions for input
    
    * refactor: storybook example with form
    
    * chore: updated test file
    
    * refactor: review comments addressed with fixes
    
    * chore: renamed data aria label
    
    * fix: adding name attribute
    
    * style: disabled fixes
    
    * fix: validation and spacing
    
    * refactor: using constants from exisiting components
    
    * fix: typo for aria label
    
    * refactor: updated onchange dispatch event
    
    * chore: cleanup state
    
    * fix: auto merge issue
    
    * fix: attribute name
    
    * fix: aria label to read prefix text
    
    * fix: styling and a11y
    
    * test: updated with aria labels
    
    * refactor: adding tests from review comments
    
    ---------
    
    Co-authored-by: Arjun Kesava <arjun.kesava@gmail.com>
    Co-authored-by: Deeya Upadhyay <58636958+2020Deeya@users.noreply.github.com>

--- packages/components/src/components/input/input.e2e-test.ts
+++ packages/components/src/components/input/input.e2e-test.ts
@@ -1,66 +1,385 @@
-import { test } from '../../../config/playwright/setup';
+/* eslint-disable no-await-in-loop */
+/* eslint-disable no-restricted-syntax */
+import { expect } from '@playwright/test';
+import { ComponentsPage, test } from '../../../config/playwright/setup';
+import StickerSheet from '../../../config/playwright/setup/utils/Stickersheet';
+import { VALIDATION } from '../formfieldwrapper/formfieldwrapper.constants';
+import { getHelperIcon } from '../formfieldwrapper/formfieldwrapper.utils';
 
-test.beforeEach(async ({ componentsPage }) => {
+type SetupOptions = {
+  componentsPage: ComponentsPage;
+  id?: string;
+  value?: string;
+  placeholder?: string;
+  required?: boolean;
+  readonly?: boolean;
+  disabled?: boolean;
+  maxlength?: number;
+  minlength?: number;
+  prefixText?: string;
+  leadingIcon?: string;
+  trailingButton?: boolean;
+  label?: string;
+  helpText?: string;
+  helpTextType?: string;
+  autocapitalize?: string;
+  autofocus?: boolean;
+  autocomplete?: string;
+  dirname?: string;
+  pattern?: string;
+  list?: string;
+  size?: number;
+  dataAriaLabel?: string;
+  clearAriaLabel?: string;
+};
+
+const setup = async (args: SetupOptions, isForm = false) => {
+  const { componentsPage, ...restArgs } = args;
   await componentsPage.mount({
     html: `
-        <mdc-input />
-      `,
+    ${isForm ? '<form>' : ''}
+      <mdc-input
+      id="${restArgs.id}"
+      ${restArgs.value ? `value="${restArgs.value}"` : ''}
+      ${restArgs.placeholder ? `placeholder="${restArgs.placeholder}"` : ''}
+      ${restArgs.required ? 'required' : ''}
+      ${restArgs.readonly ? 'readonly' : ''}
+      ${restArgs.disabled ? 'disabled' : ''}
+      ${restArgs.maxlength ? `maxlength="${restArgs.maxlength}"` : ''}
+      ${restArgs.minlength ? `minlength="${restArgs.minlength}"` : ''}
+      ${restArgs.prefixText ? `prefix-text="${restArgs.prefixText}"` : ''}
+      ${restArgs.leadingIcon ? `leading-icon="${restArgs.leadingIcon}"` : ''}
+      ${restArgs.label ? `label="${restArgs.label}"` : ''}
+      ${restArgs.helpText ? `help-text="${restArgs.helpText}"` : ''}
+      ${restArgs.helpTextType ? `help-text-type="${restArgs.helpTextType}"` : ''}
+      ${restArgs.trailingButton ? 'trailing-button' : ''}
+      ${restArgs.autocapitalize ? `autocapitalize="${restArgs.autocapitalize}"` : ''}
+      ${restArgs.autofocus ? 'autofocus' : ''}
+      ${restArgs.autocomplete ? `autocomplete="${restArgs.autocomplete}"` : ''}
+      ${restArgs.dirname ? `dirname="${restArgs.dirname}"` : ''}
+      ${restArgs.pattern ? `pattern="${restArgs.pattern}"` : ''}
+      ${restArgs.list ? `list="${restArgs.list}"` : ''}
+      ${restArgs.size ? `size="${restArgs.size}"` : ''}
+      ${restArgs.dataAriaLabel ? `data-aria-label="${restArgs.dataAriaLabel}"` : ''}
+      ${restArgs.clearAriaLabel ? `data-aria-label="${restArgs.clearAriaLabel}"` : ''}
+      ></mdc-input>
+    ${isForm ? '<mdc-button type="submit" size="24">Submit</mdc-button></form>' : ''}
+    `,
+    clearDocument: true,
   });
-});
+  if (isForm) {
+    const form = componentsPage.page.locator('form');
+    await form.waitFor();
+    return form;
+  }
+  const text = componentsPage.page.locator('mdc-input');
+  await text.waitFor();
+  return text;
+};
 
-test.skip('mdc-input', async ({ componentsPage }) => {
-  const input = componentsPage.page.locator('mdc-input');
-
-  // initial check for the input be visible on the screen:
-  await input.waitFor();
-
-  /**
-   * ACCESSIBILITY
-   */
-  await test.step('accessibility', async () => {
-    await componentsPage.accessibility.checkForA11yViolations('input-default');
-  });
-
-  /**
-   * VISUAL REGRESSION
-   */
-  await test.step('visual-regression', async () => {
-    await test.step('matches screenshot of element', async () => {
-      await componentsPage.visualRegression.takeScreenshot('mdc-input', { element: input });
-    });
+test.use({ viewport: { width: 800, height: 1500 } });
+test('mdc-input', async ({ componentsPage, browserName }) => {
+  const input = await setup({
+    componentsPage,
+    id: 'test-mdc-input',
+    placeholder: 'Placeholder',
+    maxlength: 10,
+    minlength: 5,
+    prefixText: 'Prefix',
+    dataAriaLabel: 'prefix', // aria-label for prefix text to be read by screen reader
+    leadingIcon: 'placeholder-bold',
+    label: 'Label',
+    helpText: 'Help Text',
   });
 
   /**
    * ATTRIBUTES
    */
   await test.step('attributes', async () => {
-    await test.step('attribute X should be present on component by default', async () => {
-      // TODO: add test here
+    await test.step('attributes should be present on component', async () => {
+      await expect(input).toHaveAttribute('id', 'test-mdc-input');
+      await expect(input).toHaveAttribute('placeholder', 'Placeholder');
+      await expect(input).toHaveAttribute('label', 'Label');
+      const label = await input.locator('label');
+      await expect(label).toHaveText('Label');
+      await expect(input).toHaveAttribute('help-text', 'Help Text');
+      const helpText = await input.locator('mdc-text[part="help-text"]');
+      await expect(helpText).toHaveText('Help Text');
+      await expect(input).toHaveAttribute('prefix-text', 'Prefix');
+      await expect(input).toHaveAttribute('data-aria-label', 'prefix');
+      const inputEl = await input.locator('input');
+      await expect(inputEl).toHaveAttribute('aria-label', 'prefix');
+      await expect(input).toHaveAttribute('leading-icon', 'placeholder-bold');
+      const icon = await input.locator('mdc-icon');
+      await expect(icon).toHaveAttribute('name', 'placeholder-bold');
+    });
+
+    await test.step('attributes required should be present on component', async () => {
+      await componentsPage.setAttributes(input, { required: '' });
+      await expect(input).toHaveAttribute('required');
+      await componentsPage.removeAttribute(input, 'required');
+    });
+
+    await test.step('attributes readonly should be present on component', async () => {
+      await componentsPage.setAttributes(input, { readonly: '' });
+      await expect(input).toHaveAttribute('readonly');
+      await componentsPage.removeAttribute(input, 'readonly');
+    });
+
+    await test.step('attributes disabled should be present on component', async () => {
+      await componentsPage.setAttributes(input, { disabled: '' });
+      await expect(input).toHaveAttribute('disabled');
+      await componentsPage.removeAttribute(input, 'disabled');
+    });
+
+    await test.step('attributes help-text-type should be present on component', async () => {
+      for (const type of Object.values(VALIDATION)) {
+        await componentsPage.setAttributes(input, { 'help-text-type': type });
+        await expect(input).toHaveAttribute('help-text-type', type);
+        const iconEl = await input.locator('mdc-icon[part="helper-icon"]');
+        const icon = getHelperIcon(type);
+        if (icon) {
+          await expect(iconEl).toHaveAttribute('name', icon);
+        }
+      }
+      await componentsPage.removeAttribute(input, 'help-text-type');
+    });
+
+    await test.step('attributes size, minlength and maxlength should be present on component', async () => {
+      await componentsPage.setAttributes(input, { maxlength: '10', minlength: '5', size: '10' });
+      await expect(input).toHaveAttribute('maxlength', '10');
+      await expect(input).toHaveAttribute('minlength', '5');
+      await expect(input).toHaveAttribute('size', '10');
+      await componentsPage.removeAttribute(input, 'maxlength');
+      await componentsPage.removeAttribute(input, 'minlength');
+      await componentsPage.removeAttribute(input, 'size');
+    });
+
+    await test.step('attribute trailing-button should be present on component', async () => {
+      await componentsPage.setAttributes(input, { 'trailing-button': '', 'clear-aria-label': 'clear' });
+      await expect(input).toHaveAttribute('trailing-button');
+      const trailingButton = input.locator('mdc-button[part="trailing-button"]');
+      await expect(trailingButton).toHaveAttribute('aria-label', 'clear');
+      await componentsPage.removeAttribute(input, 'trailing-button');
+    });
+
+    await test.step('attribute autofocus should be present on component', async () => {
+      await componentsPage.setAttributes(input, { autofocus: '' });
+      await expect(input).toHaveAttribute('autofocus');
+      await componentsPage.removeAttribute(input, 'autofocus');
+    });
+
+    await test.step('attribute autocapitalize should be present on component', async () => {
+      await componentsPage.setAttributes(input, { autocapitalize: 'sentences' });
+      await expect(input).toHaveAttribute('autocapitalize', 'sentences');
+      await componentsPage.removeAttribute(input, 'autocapitalize');
+    });
+
+    await test.step('attribute autocomplete should be present on component', async () => {
+      await componentsPage.setAttributes(input, { autocomplete: 'on' });
+      await expect(input).toHaveAttribute('autocomplete', 'on');
+      await componentsPage.removeAttribute(input, 'autocomplete');
+    });
+
+    await test.step('attribute direname should be present on component', async () => {
+      await componentsPage.setAttributes(input, { direname: 'ltr' });
+      await expect(input).toHaveAttribute('direname', 'ltr');
+      await componentsPage.removeAttribute(input, 'direname');
+    });
+
+    await test.step('attribute direname should be present on component', async () => {
+      await componentsPage.setAttributes(input, { direname: 'ltr' });
+      await expect(input).toHaveAttribute('direname', 'ltr');
+      await componentsPage.removeAttribute(input, 'direname');
+    });
+
+    await test.step('attribute pattern should be present on component', async () => {
+      await componentsPage.setAttributes(input, { pattern: '[A-Za-z]{3}' });
+      await expect(input).toHaveAttribute('pattern', '[A-Za-z]{3}');
+      await componentsPage.removeAttribute(input, 'pattern');
+    });
+
+    await test.step('attribute list should be present on component', async () => {
+      await componentsPage.setAttributes(input, { list: 'browsers' });
+      await expect(input).toHaveAttribute('list', 'browsers');
+      await componentsPage.removeAttribute(input, 'list');
     });
   });
 
   /**
    * INTERACTIONS
    */
+  // test form submission with input field marked as required
   await test.step('interactions', async () => {
-    await test.step('mouse/pointer', async () => {
-      await test.step('component should fire callback x when clicking on it', async () => {
-        // TODO: add test here
-      });
+    const inputEl = await input.locator('input');
+    await test.step('component should be focusable with tab', async () => {
+      await componentsPage.actionability.pressTab();
+      await expect(input).toBeFocused();
+      await inputEl.fill('test');
+      await expect(inputEl).toHaveValue('test');
+      if (browserName !== 'firefox') {
+        await componentsPage.actionability.pressTab();
+        await expect(input).not.toBeFocused();
+      }
     });
 
-    await test.step('focus', async () => {
-      await test.step('component should be focusable with tab', async () => {
-        // TODO: add test here
+    await test.step('readonly component should be focusable with tab', async () => {
+      await componentsPage.setAttributes(input, {
+        readonly: '',
+        value: 'Readonly',
+        'trailing-button': '',
+        'clear-aria-label': 'clear',
       });
-
-      // add additional tests here, like tabbing through several parts of the component
+      const trailingButton = input.locator('mdc-button').first();
+      await componentsPage.actionability.pressTab();
+      await expect(input).toBeFocused();
+      await expect(inputEl).toHaveValue('Readonly');
+      if (browserName !== 'firefox') {
+        await componentsPage.actionability.pressTab();
+        await expect(input).not.toBeFocused();
+        await expect(trailingButton).not.toBeFocused();
+      }
+      await componentsPage.removeAttribute(input, 'readonly');
     });
 
-    await test.step('keyboard', async () => {
-      await test.step('component should fire callback x when pressing y', async () => {
-        // TODO: add test here
-      });
+    await test.step('focus on input and trailing button when value is present during interactions', async () => {
+      await componentsPage.setAttributes(input, { 'trailing-button': '', value: '', 'clear-aria-label': 'clear' });
+      const trailingButton = input.locator('mdc-button[part="trailing-button"]');
+      await componentsPage.actionability.pressTab();
+      await expect(input).toBeFocused();
+      await expect(trailingButton).toHaveClass('hidden');
+      await inputEl.fill('test');
+      await expect(inputEl).toHaveValue('test');
+      await expect(trailingButton).not.toHaveClass('hidden');
+      await componentsPage.actionability.pressTab();
+      await expect(inputEl).not.toBeFocused();
+      await expect(trailingButton).toBeFocused();
+      await trailingButton.click();
+      await expect(input).toBeFocused();
+      await expect(inputEl).toHaveValue('');
     });
+
+    await test.step('trailing button will not be focusable in readonly state', async () => {
+      await componentsPage.setAttributes(input, {
+        value: 'this is readonly data',
+        readonly: '',
+      });
+      await componentsPage.actionability.releaseFocus(input);
+      const trailingButton = input.locator('mdc-button[part="trailing-button"]');
+      await expect(inputEl).toHaveValue('this is readonly data');
+      await expect(trailingButton).not.toHaveClass('hidden');
+      await componentsPage.actionability.pressTab();
+      await expect(input).toBeFocused();
+      await componentsPage.actionability.pressTab();
+      // disabled when input is in readonly state
+      await expect(trailingButton).not.toBeFocused();
+      await componentsPage.removeAttribute(input, 'readonly');
+      await componentsPage.removeAttribute(input, 'trailing-buton');
+    });
+
+    await test.step('component should not be focusable when disabled', async () => {
+      await componentsPage.setAttributes(input, { disabled: '', value: 'Disabled' });
+      await componentsPage.actionability.pressTab();
+      await expect(input).not.toBeFocused();
+      await expect(inputEl).toHaveValue('Disabled');
+      await componentsPage.removeAttribute(input, 'disabled');
+    });
+
+    await test.step('component in form should be validated for required and maxlength when submitted', async () => {
+      const form = await setup({
+        componentsPage,
+        id: 'test-mdc-input',
+        placeholder: 'Placeholder',
+        required: true,
+        maxlength: 10,
+      }, true);
+      const mdcInput = await form.locator('mdc-input');
+      const submitButton = await form.locator('mdc-button');
+      const inputEl = mdcInput.locator('input');
+      await componentsPage.actionability.pressTab();
+      await expect(mdcInput).toBeFocused();
+      await componentsPage.actionability.pressTab();
+      await expect(submitButton).toBeFocused();
+      await submitButton.click();
+      const validationMessage = await inputEl.evaluate((element) => {
+        const input = element as HTMLInputElement;
+        return input.validationMessage;
+      });
+      if (browserName === 'webkit') {
+        expect(validationMessage).toContain('Fill out this field');
+      } else {
+        expect(validationMessage).toContain('Please fill out this field.');
+      }
+      await inputEl.fill('This is a long text');
+      await expect(inputEl).toHaveValue('This is a '); // maxlength is 10; truncates rest of the value.
+      await submitButton.click();
+    });
+  });
+
+  /**
+   * VISUAL REGRESSION
+   */
+  await test.step('visual-regression', async () => {
+    const attributes = {
+      id: 'test-mdc-input',
+      placeholder: 'Placeholder',
+      label: 'Label',
+      'help-text': 'Help Text',
+    };
+    const inputStickerSheet = new StickerSheet(componentsPage, 'mdc-input');
+
+    await inputStickerSheet.setAttributes(attributes);
+    await inputStickerSheet.createMarkupWithCombination({
+      'help-text-type': VALIDATION,
+    });
+
+    // disabled input field with value
+    await inputStickerSheet.setAttributes({ ...attributes,
+      value: 'Disabled',
+      disabled: true,
+    });
+    await inputStickerSheet.createMarkupWithCombination({});
+
+    // input with value and leading icon
+    await inputStickerSheet.setAttributes({ ...attributes,
+      value: 'Leading Icon',
+      'leading-icon': 'placeholder-bold',
+    });
+    await inputStickerSheet.createMarkupWithCombination({});
+
+    // input with value and prefix text
+    await inputStickerSheet.setAttributes({ ...attributes,
+      value: 'Text Content',
+      'prefix-text': 'Prefix',
+    });
+    await inputStickerSheet.createMarkupWithCombination({});
+
+    // input with value and trailing button
+    await inputStickerSheet.setAttributes({ ...attributes,
+      value: 'Clear button',
+      'trailing-button': true,
+      'clear-aria-label': 'clear',
+    });
+    await inputStickerSheet.createMarkupWithCombination({});
+
+    // readonly input field with value
+    await inputStickerSheet.setAttributes({ ...attributes,
+      value: 'Readonly value',
+      readonly: true,
+    });
+    await inputStickerSheet.createMarkupWithCombination({});
+    await inputStickerSheet.mountStickerSheet();
+    const container = await inputStickerSheet.getWrapperContainer();
+
+    await test.step('matches screenshot of element', async () => {
+      await componentsPage.visualRegression.takeScreenshot('mdc-input', { element: container });
+    });
+  });
+
+  /**
+   * ACCESSIBILITY
+   */
+  await test.step('accessibility', async () => {
+    await componentsPage.accessibility.checkForA11yViolations('input-default');
   });
 });
