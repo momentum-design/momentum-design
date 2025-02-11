2f6ad81ea4658387b887aba7210c66fd0de77161
Supriya M <30731236+supminn@users.noreply.github.com>
2025-02-11 08:52:51 +0530

    test: e2e test cases for input component (#1080)
    * feat: helper text with label as internal component
    
    * chore: updated story
    
    * fix: review comments addressed
    
    * chore: updated storybook
    
    * refactor: input component using internal helper label
    
    * fix: updated function name
    
    * fix: function name
    
    * feat: mixin for required and readonly
    
    * fix: updated default value of id
    
    * refactor: review comments addressed
    
    * style: helper text color
    
    * fix: prettier cleaning
    
    * fix: indentation issue
    
    * remove class and style
    
    * refactor: renamed the class and element
    
    * feat: mixin for value and name properties
    
    * fix: type imports
    
    * refactor: updated based on parent PR changes
    
    * fix: type import
    
    * fix: property name
    
    * chore: renamed labelandhelper to formfieldwrapper
    
    * fix: focus issue when button is hidden
    
    * fix: removed type; updatede component width
    
    * fix: making trailing button optional
    
    * style: fix focused style
    
    * refactor: support for all attributes that native input supports
    
    * fix: removed label-info-text attribute
    
    * test(wip): input component attributes
    
    * test: test cases for attributes
    
    * chore: minor fix
    
    * test: visual regression snapshots
    
    * fix: support for form internals
    
    * fix: form support for mdc input
    
    * style: fix for disabled state
    
    * chore: updated snapshots
    
    * test: interaction testing and form validation
    
    * test: updated test cases
    
    * style: fixes based on figma color spec
    
    * fix: icon fixes on helper text types
    
    * chore: updated snapshots based on icon changes on helper text type
    
    * refactor: updated css variables and documentation
    
    * fix: disable controls on css variables
    
    * style: updating icon colors
    
    * chore: updated snapshot order
    
    * fix: addressing review comments
    
    * fix: review comments
    
    * fix: aria label for trailing button
    
    * refactor: actions for input
    
    * refactor: storybook example with form
    
    * chore: updated test file
    
    * refactor: review comments addressed with fixes
    
    * chore: renamed data aria label
    
    * fix: adding name attribute
    
    * style: disabled fixes
    
    * fix: validation and spacing
    
    * refactor: using constants from exisiting components
    
    * fix: typo for aria label
    
    * refactor: updated onchange dispatch event
    
    * chore: cleanup state
    
    * fix: auto merge issue
    
    * fix: attribute name
    
    * fix: aria label to read prefix text
    
    * fix: styling and a11y
    
    * test: updated with aria labels
    
    * refactor: adding tests from review comments
    
    ---------
    
    Co-authored-by: Arjun Kesava <arjun.kesava@gmail.com>
    Co-authored-by: Deeya Upadhyay <58636958+2020Deeya@users.noreply.github.com>

--- packages/components/src/components/input/input.component.ts
+++ packages/components/src/components/input/input.component.ts
@@ -360,6 +360,7 @@ class Input extends DataAriaLabelMixin(ValueMixin(NameMixin(FormfieldWrapper))) 
       }
       return html`
       <mdc-button 
+        part='trailing-button'
         class='${!this.value ? 'hidden' : ''}'
         prefix-icon='${DEFAULTS.CLEAR_BUTTON_ICON}'
         variant='${DEFAULTS.CLEAR_BUTTON_VARIANT}'
