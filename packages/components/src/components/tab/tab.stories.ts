8db100d0dfadab415369a028ba75d736b0dd09a4
Pankaj Kumar <98078859+kpankaj3@users.noreply.github.com>
2025-02-12 17:22:29 +0530

    chore: Tab component + storybook (#1076)
    * chore: init tab component
    
    * chore: set default role to be tab
    
    * refactor: use Buttonsimple as parent class in Tab component + updated Buttonsimple
    
    * refactor: tab component - use constants from Text component
    
    * refactor: tab component - use ifDefined in args and import badge component in storybook page
    
    * chore: tab component - add css variables in component description and storypage
    
    * fix: tab component - do not add aria-pressed attribute on tab active as role is tab
    
    * refactor: refactor the setAriaSelected function in button simple with setActive
    
    * fix: update attribute tabIndex in some stories
    
    * Revert "fix: update attribute tabIndex in some stories"
    
    This reverts commit 21a504dfe5ee3baf1b8b6ebd5804b63d7f19f908.
    
    * chore: add new Glass tab type + fix: pressed state not showing on click
    
    * fix: remove role from storybook
    
    * chore: use drop shadow to render line tab active state
    
    * chore: use div to render line tab active indicator
    
    * fix: executeAction getting called twice on keyboard enter press in Buttonsimple
    
    * refactor: indicator line for active line tab variant
    
    * fix: tab size active
    
    * refactor: tab text provided by text attribute
    
    * refactor: use IconNameMixin in tab
    
    * fix: several fixes
    
    ---------
    
    Co-authored-by: Devesh <deveshpr@cisco.com>
    Co-authored-by: Christoph Scheffauer <scheffauerc@gmail.com>

---
+++ packages/components/src/components/tab/tab.stories.ts
@@ -0,0 +1,184 @@
+import type { Meta, StoryObj, Args } from '@storybook/web-components';
+import '.';
+import '../badge';
+import { html, nothing } from 'lit';
+import { action } from '@storybook/addon-actions';
+import { ifDefined } from 'lit/directives/if-defined.js';
+import { TAB_VARIANTS } from './tab.constants';
+import { disableControls, hideControls, readOnlyControls } from '../../../config/storybook/utils';
+
+const render = (args: Args) => html`<div role="tablist">
+  <mdc-tab
+    @click="${action('onclick')}"
+    @keydown="${action('onkeydown')}"
+    @keyup="${action('onkeyup')}"
+    @focus="${action('onfocus')}"
+    ?active="${args.active}"
+    aria-label="${ifDefined(args.text ? nothing : 'Label')}"
+    ?disabled="${args.disabled}"
+    icon-name="${ifDefined(args['icon-name'])}"
+    tabIndex="${ifDefined(args.tabIndex)}"
+    text="${ifDefined(args.text)}"
+    variant="${ifDefined(args.variant)}"
+    >${args.showBadge ? html`<mdc-badge slot="badge" type="counter" counter="1"></mdc-badge>` : nothing}</mdc-tab>
+</div>`;
+
+const meta: Meta = {
+  title: 'Work In Progress/tab',
+  tags: ['autodocs'],
+  component: 'mdc-tab',
+  render,
+  parameters: {
+    badges: ['wip'],
+  },
+  argTypes: {
+    active: {
+      control: 'boolean',
+      description: 'Tab can be active or inactive. Active tab means the tab is selected.',
+    },
+    disabled: {
+      control: 'boolean',
+    },
+    'icon-name': {
+      control: 'text',
+    },
+    showBadge: {
+      control: 'boolean',
+      description: 'This is an internal argument to show the badge in the story',
+      table: { disable: true },
+    },
+    tabIndex: {
+      control: 'number',
+    },
+    text: {
+      control: 'text',
+    },
+    variant: {
+      control: 'select',
+      options: Object.values(TAB_VARIANTS),
+    },
+    ...disableControls([
+      '--mdc-tab-content-gap',
+      '--mdc-tab-height',
+      '--mdc-tab-glass-active-background-color-disabled',
+      '--mdc-tab-glass-active-background-color-hover',
+      '--mdc-tab-glass-active-background-color-normal',
+      '--mdc-tab-glass-active-background-color-pressed',
+      '--mdc-tab-glass-active-color',
+      '--mdc-tab-glass-active-color-disabled',
+      '--mdc-tab-glass-border-radius',
+      '--mdc-tab-glass-inactive-background-color-disabled',
+      '--mdc-tab-glass-inactive-background-color-hover',
+      '--mdc-tab-glass-inactive-background-color-normal',
+      '--mdc-tab-glass-inactive-background-color-pressed',
+      '--mdc-tab-glass-inactive-color',
+      '--mdc-tab-glass-inactive-color-disabled',
+      '--mdc-tab-line-active-background-color-disabled',
+      '--mdc-tab-line-active-background-color-hover',
+      '--mdc-tab-line-active-background-color-normal',
+      '--mdc-tab-line-active-background-color-pressed',
+      '--mdc-tab-line-active-color',
+      '--mdc-tab-line-active-color-disabled',
+      '--mdc-tab-line-active-indicator-color',
+      '--mdc-tab-line-active-indicator-color-disabled',
+      '--mdc-tab-line-active-indicator-height',
+      '--mdc-tab-line-active-indicator-width',
+      '--mdc-tab-line-border-bottom-left-radius',
+      '--mdc-tab-line-border-bottom-right-radius',
+      '--mdc-tab-line-border-top-left-radius',
+      '--mdc-tab-line-border-top-right-radius',
+      '--mdc-tab-line-inactive-background-color-disabled',
+      '--mdc-tab-line-inactive-background-color-hover',
+      '--mdc-tab-line-inactive-background-color-normal',
+      '--mdc-tab-line-inactive-background-color-pressed',
+      '--mdc-tab-line-inactive-color',
+      '--mdc-tab-line-inactive-color-disabled',
+      '--mdc-tab-padding-left',
+      '--mdc-tab-padding-right',
+      '--mdc-tab-pill-active-background-color-disabled',
+      '--mdc-tab-pill-active-background-color-hover',
+      '--mdc-tab-pill-active-background-color-normal',
+      '--mdc-tab-pill-active-background-color-pressed',
+      '--mdc-tab-pill-active-color',
+      '--mdc-tab-pill-active-color-disabled',
+      '--mdc-tab-pill-border-radius',
+      '--mdc-tab-pill-inactive-background-color-disabled',
+      '--mdc-tab-pill-inactive-background-color-hover',
+      '--mdc-tab-pill-inactive-background-color-normal',
+      '--mdc-tab-pill-inactive-background-color-pressed',
+      '--mdc-tab-pill-inactive-color',
+      '--mdc-tab-pill-inactive-color-disabled',
+    ]),
+    ...hideControls([
+      'role',
+      'size',
+      'soft-disabled',
+      'type',
+    ]),
+  },
+};
+
+export default meta;
+
+const defaultArgs = {
+  active: false,
+  disabled: false,
+  'icon-name': 'placeholder-bold',
+  role: 'tab',
+  showBadge: false,
+  tabIndex: 0,
+  text: 'Label',
+  variant: 'pill',
+};
+
+export const Example: StoryObj = {
+  render,
+  args: {
+    ...defaultArgs,
+    showBadge: true,
+  },
+};
+
+export const GlassTab: StoryObj = {
+  render,
+  args: {
+    ...defaultArgs,
+    variant: 'glass',
+  },
+  argTypes: {
+    ...readOnlyControls(['variant']),
+  },
+};
+
+export const LineTab: StoryObj = {
+  render,
+  args: {
+    ...defaultArgs,
+    variant: 'line',
+  },
+  argTypes: {
+    ...readOnlyControls(['variant']),
+  },
+};
+
+export const PillTab: StoryObj = {
+  render,
+  args: {
+    ...defaultArgs,
+    variant: 'pill',
+  },
+  argTypes: {
+    ...readOnlyControls(['variant']),
+  },
+};
+
+export const IconOnlyTab: StoryObj = {
+  render,
+  args: {
+    ...defaultArgs,
+    text: '',
+  },
+  argTypes: {
+    ...hideControls(['text']),
+  },
+};
