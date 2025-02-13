1bafa9216e0c24e869ebfb6850ed139e03130429
Arjun Kesava P <arjun.kesava@gmail.com>
2025-02-13 12:00:52 +0530

    perf: add e2e specs for checkbox group component
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>

--- packages/components/src/components/checkboxgroup/checkboxgroup.e2e-test.ts
+++ packages/components/src/components/checkboxgroup/checkboxgroup.e2e-test.ts
@@ -1,20 +1,46 @@
+import { expect } from '@playwright/test';
+import type { ComponentsPage } from '../../../config/playwright/setup';
 import { test } from '../../../config/playwright/setup';
+import StickerSheet from '../../../config/playwright/setup/utils/Stickersheet';
 
-test.beforeEach(async ({ componentsPage }) => {
+type SetupOptions = {
+  componentsPage: ComponentsPage,
+  'header-text'?: string;
+  children: string;
+}
+
+const headerText = 'Select all powers';
+const children = `
+  <mdc-checkbox label="Checkbox Label"></mdc-checkbox>
+  <mdc-checkbox label="Checkbox Label"></mdc-checkbox>
+  <mdc-checkbox label="Checkbox Label"></mdc-checkbox>
+`;
+const avengersChildren = `
+  <mdc-checkbox label="Black Widow"></mdc-checkbox>
+  <mdc-checkbox label="Captain America"></mdc-checkbox>
+  <mdc-checkbox label="Hawkye"></mdc-checkbox>
+  <mdc-checkbox label="The Hulk"></mdc-checkbox>
+  <mdc-checkbox label="Iron Man"></mdc-checkbox>
+  <mdc-checkbox label="Thor"></mdc-checkbox>
+`;
+const setup = async (args: SetupOptions) => {
+  const { componentsPage, ...restArgs } = args;
   await componentsPage.mount({
     html: `
-        <mdc-checkboxgroup />
-      `,
+      <mdc-checkboxgroup
+        ${restArgs['header-text'] ? `header-text="${restArgs['header-text']}"` : ''}
+      >
+        ${restArgs.children}
+      </mdc-checkboxgroup>
+    `,
+    clearDocument: true,
   });
-});
-
-// TODO: add e2e tests here
-test.skip('mdc-checkboxgroup', async ({ componentsPage }) => {
   const checkboxgroup = componentsPage.page.locator('mdc-checkboxgroup');
-
-  // initial check for the checkboxgroup be visible on the screen:
   await checkboxgroup.waitFor();
+  return checkboxgroup;
+};
 
+test('mdc-checkboxgroup', async ({ componentsPage }) => {
   /**
    * ACCESSIBILITY
    */
@@ -26,8 +52,17 @@ test.skip('mdc-checkboxgroup', async ({ componentsPage }) => {
    * VISUAL REGRESSION
    */
   await test.step('visual-regression', async () => {
+    const checkboxgroupStickerSheet = new StickerSheet(componentsPage, 'mdc-checkboxgroup');
+    checkboxgroupStickerSheet.setAttributes({
+      'header-text': headerText,
+    });
+    checkboxgroupStickerSheet.setChildren(avengersChildren);
+    await checkboxgroupStickerSheet.createMarkupWithCombination({}, true);
+    await checkboxgroupStickerSheet.mountStickerSheet();
     await test.step('matches screenshot of element', async () => {
-      await componentsPage.visualRegression.takeScreenshot('mdc-checkboxgroup', { element: checkboxgroup });
+      await componentsPage.visualRegression.takeScreenshot('mdc-checkboxgroup', {
+        element: checkboxgroupStickerSheet.getWrapperContainer(),
+      });
     });
   });
 
@@ -35,8 +70,12 @@ test.skip('mdc-checkboxgroup', async ({ componentsPage }) => {
    * ATTRIBUTES
    */
   await test.step('attributes', async () => {
-    await test.step('attribute X should be present on component by default', async () => {
-      // TODO: add test here
+    const checkboxgroup = await setup({ componentsPage, children });
+    await test.step('attribute `header-text` should be present on component when set', async () => {
+      await componentsPage.setAttributes(checkboxgroup, { 'header-text': headerText });
+      const mdcText = componentsPage.page.locator('mdc-text');
+      const textContent = await mdcText.textContent();
+      expect(textContent?.trim()).toBe(headerText);
     });
   });
 
@@ -44,23 +83,49 @@ test.skip('mdc-checkboxgroup', async ({ componentsPage }) => {
    * INTERACTIONS
    */
   await test.step('interactions', async () => {
-    await test.step('mouse/pointer', async () => {
-      await test.step('component should fire callback x when clicking on it', async () => {
-        // TODO: add test here
-      });
-    });
-
-    await test.step('focus', async () => {
-      await test.step('component should be focusable with tab', async () => {
-        // TODO: add test here
-      });
-
-      // add additional tests here, like tabbing through several parts of the component
-    });
-
     await test.step('keyboard', async () => {
-      await test.step('component should fire callback x when pressing y', async () => {
-        // TODO: add test here
+      await test.step('component should travel through children with tab', async () => {
+        const checkboxgroup = await setup({ componentsPage, children });
+
+        await componentsPage.actionability.pressTab();
+        await expect(checkboxgroup.locator('mdc-checkbox').first()).toBeFocused();
+
+        await componentsPage.actionability.pressTab();
+        await expect(checkboxgroup.locator('mdc-checkbox').nth(1)).toBeFocused();
+
+        await componentsPage.actionability.pressTab();
+        await expect(checkboxgroup.locator('mdc-checkbox').last()).toBeFocused();
+      });
+
+      await test.step('component should travel through children with arrow down and arrow up keys', async () => {
+        const checkboxgroup = await setup({ componentsPage, children: avengersChildren });
+
+        await componentsPage.actionability.pressTab();
+        await expect(checkboxgroup.locator('mdc-checkbox').first()).toBeFocused();
+
+        await componentsPage.page.keyboard.press('ArrowDown');
+        await expect(checkboxgroup.locator('mdc-checkbox').nth(1)).toBeFocused();
+
+        await componentsPage.page.keyboard.press('ArrowDown');
+        await expect(checkboxgroup.locator('mdc-checkbox').nth(2)).toBeFocused();
+
+        await componentsPage.page.keyboard.press('ArrowDown');
+        await expect(checkboxgroup.locator('mdc-checkbox').nth(3)).toBeFocused();
+
+        await componentsPage.page.keyboard.press('ArrowDown');
+        await expect(checkboxgroup.locator('mdc-checkbox').nth(4)).toBeFocused();
+
+        await componentsPage.page.keyboard.press('ArrowUp');
+        await expect(checkboxgroup.locator('mdc-checkbox').nth(3)).toBeFocused();
+
+        await componentsPage.page.keyboard.press('ArrowUp');
+        await expect(checkboxgroup.locator('mdc-checkbox').nth(2)).toBeFocused();
+
+        await componentsPage.page.keyboard.press('ArrowUp');
+        await expect(checkboxgroup.locator('mdc-checkbox').nth(1)).toBeFocused();
+
+        await componentsPage.page.keyboard.press('ArrowUp');
+        await expect(checkboxgroup.locator('mdc-checkbox').first()).toBeFocused();
       });
     });
   });
