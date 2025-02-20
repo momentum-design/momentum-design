3601cdf3fe66384583edf153d3bdf25fcf5ee7c8
Devesh <deveshpr@cisco.com>
2025-02-20 15:48:47 +0530

    chore: initial tablist implementation

---
+++ packages/components/src/components/tablist/tablist.styles.ts
@@ -0,0 +1,15 @@
+import { css } from 'lit';
+
+const styles = css`
+  :host {
+    display: flex;
+    align-items: center;
+    gap: 12px;
+  }
+
+  :host .hide-button {
+    display: none;
+  }
+`;
+
+export default [styles];
