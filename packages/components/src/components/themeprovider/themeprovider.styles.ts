605271f9cd94486c9292b7120dd257dd3ec857f0
Supriya M <sansup49@gmail.com>
2025-02-25 22:14:19 +0530

    style: scollbar styling

--- packages/components/src/components/themeprovider/themeprovider.styles.ts
+++ packages/components/src/components/themeprovider/themeprovider.styles.ts
@@ -17,6 +17,49 @@ const styles = css`
 
     font-feature-settings: var(--mdc-themeprovider-font-feature-settings);
   }
+   
+/* Scrollbar Theme */
+
+  /* width */
+  ::-webkit-scrollbar {
+    width: 1rem;
+  }
+
+  /* Track */
+  ::-webkit-scrollbar-track {
+    border-radius: 100vh;
+    background: var(--mds-color-theme-scrollbar-background-secondary-normal);
+  }
+
+  /* Handle */
+  ::-webkit-scrollbar-thumb {
+    background-color: var(--mds-color-theme-scrollbar-button-normal);
+    border: 0.25rem solid transparent;
+    border-radius: 0.5rem;
+    background-clip: content-box;
+  }
+
+  /* Handle on hover */
+  ::-webkit-scrollbar-thumb:hover {
+    background: var(--mds-color-theme-scrollbar-button-hover);
+    border: 0.25rem solid transparent;
+    border-radius: 0.5rem;
+    background-clip: content-box;
+  }
+
+  /* Handle on press */
+  ::-webkit-scrollbar-thumb:active {
+    background: var(--mds-color-theme-scrollbar-button-pressed);
+    border: 0.25rem solid transparent;
+    border-radius: 0.5rem;
+    height: 6.25rem;
+    background-clip: content-box;
+  }
+
+  /* Corner */
+  ::-webkit-scrollbar-corner {
+    background: inherit;
+  }
 `;
 
 export default [styles];
