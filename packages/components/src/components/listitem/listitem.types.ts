1d7018ec4af26533741facfc85a4c5a72cfeab0e
Arjun Kesava P <arjun.kesava@gmail.com>
2025-02-27 11:06:35 +0530

    chore: convert variants const to type in listitem
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>

---
+++ packages/components/src/components/listitem/listitem.types.ts
@@ -0,0 +1,6 @@
+import type { ValueOf } from '../../utils/types';
+import { LISTITEM_VARIANTS } from './listitem.constants';
+
+type ListItemVariants = ValueOf<typeof LISTITEM_VARIANTS>;
+
+export { ListItemVariants };
