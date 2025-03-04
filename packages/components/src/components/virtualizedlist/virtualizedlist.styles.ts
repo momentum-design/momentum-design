fe115027809efaea001625b7a524ceaccd8895f3
Pankaj Kumar <98078859+kpankaj3@users.noreply.github.com>
2025-03-04 11:26:22 +0530

    refactor: Refactor css classes with parts in Avatar and Virtualized List (#1117)
    * refactor: avatar component - use parts in place of classes
    
    * refactor: virtaulized list component - use parts in place of classes
    
    * fix: update parts names in virtualized list
    
    ---------
    
    Co-authored-by: Supriya M <30731236+supminn@users.noreply.github.com>

--- packages/components/src/components/virtualizedlist/virtualizedlist.styles.ts
+++ packages/components/src/components/virtualizedlist/virtualizedlist.styles.ts
@@ -2,17 +2,15 @@ import { css } from 'lit';
 
 const styles = [
   css`
-    :host {
-      .mdc-virtualizedlist-scroll {  
-        height: 100%;
-        width: 100%;
-        overflow-y: auto;
-      }
-      .mdc-virtualizedlist-container {
-        position: relative;
-        width: 100%;
-      }
-    }
+  :host::part(scroll) {
+      height: 100%;
+      width: 100%;
+      overflow-y: auto;
+  }
+  :host::part(container) {
+      position: relative;
+      width: 100%;
+  }
   `,
 ];
 
