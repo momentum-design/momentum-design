c43505ccd73d7e8c3d6e88f611ace546b9d68c2b
Devesh <deveshpr@cisco.com>
2025-03-05 15:39:29 +0530

    chore: use rems

--- packages/components/src/components/tablist/tablist.styles.ts
+++ packages/components/src/components/tablist/tablist.styles.ts
@@ -2,6 +2,15 @@ import { css } from 'lit';
 
 const styles = css`
   :host {
+    --mdc-focus-ring-inner-width: 0.125rem;
+    --mdc-focus-ring-middle-width: calc(2 * var(--mdc-focus-ring-inner-width));
+    --mdc-focus-ring-outer-width: calc(0.0625rem + var(--mdc-focus-ring-middle-width));
+
+    --mdc-tablist-gap: 0.5rem;
+    --mdc-tablist-container-width: 100%;
+    --mdc-container-button-padding: 2.75rem;
+
+
     display: flex;
     align-items: center;
 
@@ -11,23 +20,23 @@ const styles = css`
   }
   
   :host .tab_list__container {
-    width: 100%;
+    width: var(--mdc-tablist-container-width);
   }
 
   :host .show-left-button-padding {
-    padding-left: 44px;
+    padding-left: var(--mdc-container-button-padding);
   }
 
   :host .show-right-button-padding {
-    padding-right: 44px;
+    padding-right: var(--mdc-container-button-padding);
   }
 
   :host .tab_list {
     display: flex;
     position: relative;
-    gap: 8px;
     overflow-x: hidden;
-    padding: 4px;
+    gap: var(--mdc-tablist-gap);
+    padding: var(--mdc-focus-ring-outer-width);
   }
 
   :host .hide-button {
@@ -39,8 +48,6 @@ const styles = css`
     position: absolute;
     z-index: 1;
   }
-
-
 `;
 
 export default [styles];
