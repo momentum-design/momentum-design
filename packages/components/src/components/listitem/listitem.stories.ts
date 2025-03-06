86ae67be6cdb8a1f5535137935be87e582671a63
Arjun Kesava <arjun.kesava@gmail.com>
2025-03-06 13:07:16 +0530

    perf: build list and listitem components (#1112)
    * perf: add list item component attributes, story and else
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * perf: build list and listitem components
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore: add header text for list parent
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore: convert variants const to type in listitem
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * perf: add keyboard interaction for list
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore: add role for list and listitem
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * perf: update query selector with new listitem
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * fix: import and dependency
    
    * fix: styling text using parts instead of slot
    
    * fix: tertiary button without color
    
    * chore: update focus navigation for list and listitem
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore: address review comments, update disabled state, add rtl for trailing
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore: organize imports
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * fix: a11y issues on list and list-item
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore: address review comments
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    ---------
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    Co-authored-by: Supriya M <sansup49@gmail.com>

---
+++ packages/components/src/components/listitem/listitem.stories.ts
@@ -0,0 +1,155 @@
+import type { Args, Meta, StoryObj } from '@storybook/web-components';
+import { html } from 'lit';
+import { hideControls } from '../../../config/storybook/utils';
+import '.';
+import { classArgType, styleArgType } from '../../../config/storybook/commonArgTypes';
+import '../avatar';
+import '../list';
+import '../badge';
+import '../button';
+import '../checkbox';
+import '../icon';
+import '../toggle';
+import { LISTITEM_VARIANTS } from './listitem.constants';
+
+const render = (args: Args) => html`
+  <mdc-list>
+    <mdc-listitem
+      ?disabled="${args.disabled}"
+      variant="${args.variant}"
+      label="${args.label}"
+      secondary-label="${args['secondary-label']}"
+      tertiary-label="${args['tertiary-label']}"
+      side-header-text="${args['side-header-text']}"
+      subline-text="${args['subline-text']}"
+    >
+      <mdc-checkbox slot="leading-controls" data-aria-label="mock label" checked></mdc-checkbox>
+      <mdc-avatar slot="leading-controls" src="https://picsum.photos/id/63/256" presence="active"></mdc-avatar>
+      <mdc-icon slot="leading-controls" name="placeholder-bold"></mdc-icon>
+      <mdc-icon slot="trailing-controls" name="placeholder-bold"></mdc-icon>
+      <mdc-button slot="trailing-controls" variant="secondary">Label</mdc-button>
+      <mdc-toggle slot="trailing-controls" data-aria-label="mock label" size="compact"></mdc-toggle>
+      <mdc-badge slot="trailing-controls" type="dot"></mdc-badge>
+    </mdc-listitem>
+  </mdc-list>
+`;
+
+const meta: Meta = {
+  title: 'Work In Progress/listitem',
+  tags: ['autodocs'],
+  component: 'mdc-listitem',
+  render,
+  parameters: {
+    badges: ['wip'],
+  },
+  argTypes: {
+    variant: {
+      control: 'select',
+      options: Object.values(LISTITEM_VARIANTS),
+    },
+    label: {
+      control: 'text',
+    },
+    'secondary-label': {
+      control: 'text',
+    },
+    'tertiary-label': {
+      control: 'text',
+    },
+    'side-header-text': {
+      control: 'text',
+    },
+    'subline-text': {
+      control: 'text',
+    },
+    disabled: {
+      control: 'boolean',
+    },
+    ...hideControls([
+      'role',
+      '--mdc-listitem-background-color-active',
+      '--mdc-listitem-background-color-hover',
+      '--mdc-listitem-default-background-color',
+      '--mdc-listitem-primary-label-color',
+      '--mdc-listitem-secondary-label-color',
+    ]),
+    ...classArgType,
+    ...styleArgType,
+  },
+};
+
+export default meta;
+
+export const Example: StoryObj = {
+  args: {
+    variant: LISTITEM_VARIANTS.FULL_WIDTH,
+    label: 'Label Text',
+    'secondary-label': 'Secondary Label',
+    'tertiary-label': 'Teritary Label',
+    'side-header-text': 'Header',
+    'subline-text': 'Subline',
+    disabled: false,
+  },
+};
+
+export const TrailingIcon: StoryObj = {
+  render: () => html`
+    <mdc-listitem>
+      <mdc-icon slot="trailing-controls" name="placeholder-bold"></mdc-icon>
+    </mdc-listitem>
+  `,
+};
+
+export const LabelOnly: StoryObj = {
+  render: () => html`<mdc-listitem label="Primary Label"></mdc-listitem>`,
+};
+
+export const ListWithLabelAndLeadingAvatar: StoryObj = {
+  render: () => html`
+    <mdc-listitem label="Primary Label">
+      <mdc-avatar slot="leading-controls" src="https://picsum.photos/id/237/256" presence="busy"></mdc-avatar>
+    </mdc-listitem>`,
+};
+
+export const ListWithLabelAndTrailingBadge: StoryObj = {
+  render: () => html`
+    <mdc-listitem label="Primary Label">
+      <mdc-badge slot="trailing-controls" type="dot"></mdc-badge>
+    </mdc-listitem>`,
+};
+
+export const ListWithDisableState: StoryObj = {
+  render: (args: Args) => html`
+    <mdc-listitem
+      ?disabled="${args.disabled}"
+      label="${args.label}"
+      side-header-text="${args['side-header-text']}"
+    >
+      <mdc-checkbox slot="leading-controls" data-aria-label="mock label" checked></mdc-checkbox>
+      <mdc-icon slot="leading-controls" name="placeholder-bold"></mdc-icon>
+      <mdc-icon slot="trailing-controls" name="placeholder-bold"></mdc-icon>
+      <mdc-button slot="trailing-controls" variant="secondary">Label</mdc-button>
+      <mdc-toggle slot="trailing-controls" data-aria-label="mock label" size="compact"></mdc-toggle>
+      <mdc-badge slot="trailing-controls" type="dot"></mdc-badge>
+    </mdc-listitem>
+  `,
+  args: {
+    disabled: true,
+    label: 'Primary Label',
+    'side-header-text': 'Header',
+  },
+  argTypes: {
+    ...hideControls(['variant', 'secondary-label', 'tertiary-label', 'subline-text', 'data-aria-label', 'tabIndex']),
+  },
+};
+
+export const ListWithIconAndLabels: StoryObj = {
+  render: () => html`
+    <mdc-listitem label="Primary Label" 
+    secondary-label="This is a long Secondary Label" 
+    tertiary-label="Teritary Label">
+      <div slot="leading-controls">
+        <mdc-icon name="placeholder-bold"></mdc-icon>
+      </div>
+    </mdc-listitem>`,
+};
