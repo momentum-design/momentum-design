86e865a61eb68e51c1e23b929e3fa7c0cebf7a33
Arjun Kesava P <arjun.kesava@gmail.com>
2025-03-07 23:01:16 +0530

    chore: comment e2e specs for both option and option group components
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>

--- packages/components/src/components/optgroup/optgroup.e2e-test.ts
+++ packages/components/src/components/optgroup/optgroup.e2e-test.ts
@@ -1,66 +1,66 @@
-import { test } from '../../../config/playwright/setup';
+// import { test } from '../../../config/playwright/setup';
 
-test.beforeEach(async ({ componentsPage }) => {
-  await componentsPage.mount({
-    html: `
-        <mdc-optgroup />
-      `,
-  });
-});
+// test.beforeEach(async ({ componentsPage }) => {
+//   await componentsPage.mount({
+//     html: `
+//         <mdc-optgroup />
+//       `,
+//   });
+// });
 
-test('mdc-optgroup', async ({ componentsPage }) => {
-  const optgroup = componentsPage.page.locator('mdc-optgroup');
+// test('mdc-optgroup', async ({ componentsPage }) => {
+//   const optgroup = componentsPage.page.locator('mdc-optgroup');
 
-  // initial check for the optgroup be visible on the screen:
-  await optgroup.waitFor();
+//   // initial check for the optgroup be visible on the screen:
+//   await optgroup.waitFor();
 
-  /**
-   * ACCESSIBILITY
-   */
-  await test.step('accessibility', async () => {
-    await componentsPage.accessibility.checkForA11yViolations('optgroup-default');
-  });
+//   /**
+//    * ACCESSIBILITY
+//    */
+//   await test.step('accessibility', async () => {
+//     await componentsPage.accessibility.checkForA11yViolations('optgroup-default');
+//   });
 
-  /**
-   * VISUAL REGRESSION
-   */
-  await test.step('visual-regression', async () => {
-    await test.step('matches screenshot of element', async () => {
-      await componentsPage.visualRegression.takeScreenshot('mdc-optgroup', { element: optgroup });
-    });
-  });
+//   /**
+//    * VISUAL REGRESSION
+//    */
+//   await test.step('visual-regression', async () => {
+//     await test.step('matches screenshot of element', async () => {
+//       await componentsPage.visualRegression.takeScreenshot('mdc-optgroup', { element: optgroup });
+//     });
+//   });
 
-  /**
-   * ATTRIBUTES
-   */
-  await test.step('attributes', async () => {
-    await test.step('attribute X should be present on component by default', async () => {
-      // TODO: add test here
-    });
-  });
+//   /**
+//    * ATTRIBUTES
+//    */
+//   await test.step('attributes', async () => {
+//     await test.step('attribute X should be present on component by default', async () => {
+//       // TODO: add test here
+//     });
+//   });
 
-  /**
-   * INTERACTIONS
-   */
-  await test.step('interactions', async () => {
-    await test.step('mouse/pointer', async () => {
-      await test.step('component should fire callback x when clicking on it', async () => {
-        // TODO: add test here
-      });
-    });
+//   /**
+//    * INTERACTIONS
+//    */
+//   await test.step('interactions', async () => {
+//     await test.step('mouse/pointer', async () => {
+//       await test.step('component should fire callback x when clicking on it', async () => {
+//         // TODO: add test here
+//       });
+//     });
 
-    await test.step('focus', async () => {
-      await test.step('component should be focusable with tab', async () => {
-        // TODO: add test here
-      });
+//     await test.step('focus', async () => {
+//       await test.step('component should be focusable with tab', async () => {
+//         // TODO: add test here
+//       });
 
-      // add additional tests here, like tabbing through several parts of the component
-    });
+//       // add additional tests here, like tabbing through several parts of the component
+//     });
 
-    await test.step('keyboard', async () => {
-      await test.step('component should fire callback x when pressing y', async () => {
-        // TODO: add test here
-      });
-    });
-  });
-});
+//     await test.step('keyboard', async () => {
+//       await test.step('component should fire callback x when pressing y', async () => {
+//         // TODO: add test here
+//       });
+//     });
+//   });
+// });
