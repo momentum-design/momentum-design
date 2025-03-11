c12fb5a89d6a0d6005cc8b8e70d935d4bebde1eb
Supriya M <sansup49@gmail.com>
2025-03-11 18:12:06 +0530

    test(wip): label chip test cases

--- packages/components/src/components/chip/chip.e2e-test.ts
+++ packages/components/src/components/chip/chip.e2e-test.ts
@@ -1,18 +1,65 @@
-import { test } from '../../../config/playwright/setup';
+/* eslint-disable no-await-in-loop */
+/* eslint-disable no-restricted-syntax */
+import { expect } from '@playwright/test';
+import { ComponentsPage, test } from '../../../config/playwright/setup';
+import StickerSheet from '../../../config/playwright/setup/utils/Stickersheet';
+import type { IconNames } from '../icon/icon.types';
+import { COLOR } from './chip.constants';
+import type { ColorType } from './chip.types';
 
-test.beforeEach(async ({ componentsPage }) => {
+type SetupOptions = {
+  componentsPage: ComponentsPage;
+  label: string;
+  iconName?: IconNames;
+  color?: ColorType;
+  disabled?: boolean;
+}
+
+const setup = async (args: SetupOptions) => {
+  const { componentsPage, label, iconName, color, disabled } = args;
+
   await componentsPage.mount({
     html: `
-        <mdc-chip />
-      `,
+      <mdc-chip
+        label="${label}"
+        icon-name="${iconName}"
+        color="${color}"
+        ?disabled="${disabled}"
+      />
+    `,
+    clearDocument: true,
   });
-});
 
-test.skip('mdc-chip', async ({ componentsPage }) => {
   const chip = componentsPage.page.locator('mdc-chip');
-
-  // initial check for the chip be visible on the screen:
   await chip.waitFor();
+  return chip;
+};
+
+test('mdc-chip', async ({ componentsPage }) => {
+  /**
+   * VISUAL REGRESSION
+   */
+  await test.step('visual-regression', async () => {
+    const chipStickerSheet = new StickerSheet(componentsPage, 'mdc-chip');
+
+    chipStickerSheet.setAttributes({ label: 'Chip' });
+    await chipStickerSheet.createMarkupWithCombination({ color: COLOR });
+
+    chipStickerSheet.setAttributes({ label: 'Chip', 'icon-name': 'placeholder-bold' });
+    await chipStickerSheet.createMarkupWithCombination({ color: COLOR });
+
+    chipStickerSheet.setAttributes({ label: 'Disabled', disabled: '' });
+    await chipStickerSheet.createMarkupWithCombination({});
+
+    chipStickerSheet.setAttributes({ label: 'Disabled', 'icon-name': 'placeholder-bold', disabled: '' });
+    await chipStickerSheet.createMarkupWithCombination({});
+
+    await chipStickerSheet.mountStickerSheet();
+    const container = await chipStickerSheet.getWrapperContainer();
+    await test.step('matches screenshot of element', async () => {
+      await componentsPage.visualRegression.takeScreenshot('mdc-chip', { element: container });
+    });
+  });
 
   /**
    * ACCESSIBILITY
@@ -21,22 +68,36 @@ test.skip('mdc-chip', async ({ componentsPage }) => {
     await componentsPage.accessibility.checkForA11yViolations('chip-default');
   });
 
-  /**
-   * VISUAL REGRESSION
-   */
-  await test.step('visual-regression', async () => {
-    await test.step('matches screenshot of element', async () => {
-      await componentsPage.visualRegression.takeScreenshot('mdc-chip', { element: chip });
-    });
-  });
-
+  const chip = await setup({ componentsPage, label: 'Chip', iconName: 'placeholder-bold' });
   /**
    * ATTRIBUTES
    */
   await test.step('attributes', async () => {
-    await test.step('attribute X should be present on component by default', async () => {
-      // TODO: add test here
+    await test.step('attribute color should be present on component by default', async () => {
+      await expect(chip).toHaveAttribute('color', COLOR.DEFAULT);
     });
+
+    await test.step('attribute label should be present on component by default', async () => {
+      await expect(chip).toHaveAttribute('label', 'Chip');
+    });
+
+    await test.step('attribute icon-name should be present on component by default', async () => {
+      await expect(chip).toHaveAttribute('icon-name', 'placeholder-bold');
+    });
+
+    await test.step('attribute disabled should be set on component', async () => {
+      await componentsPage.setAttributes(chip, { disabled: '' });
+      await expect(chip).toHaveAttribute('disabled');
+      await componentsPage.removeAttribute(chip, 'disabled');
+    });
+
+    for (const color of Object.values(COLOR)) {
+      await test.step(`attribute color should be set to ${color}`, async () => {
+        await componentsPage.setAttributes(chip, { color });
+        await expect(chip).toHaveAttribute('color', color);
+        await componentsPage.removeAttribute(chip, 'color');
+      });
+    }
   });
 
   /**
