35d2ba2abc6e6355c4caacc2cdb7fc7db6c26e62
Arjun Kesava P <arjun.kesava@gmail.com>
2025-03-12 10:49:44 +0530

    perf: add specs for option group component
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>

--- packages/components/src/components/optgroup/optgroup.e2e-test.ts
+++ packages/components/src/components/optgroup/optgroup.e2e-test.ts
@@ -1,66 +1,83 @@
-// import { test } from '../../../config/playwright/setup';
+import { expect } from '@playwright/test';
+import { ComponentsPage, test } from '../../../config/playwright/setup';
+import StickerSheet from '../../../config/playwright/setup/utils/Stickersheet';
 
-// test.beforeEach(async ({ componentsPage }) => {
-//   await componentsPage.mount({
-//     html: `
-//         <mdc-optgroup />
-//       `,
-//   });
-// });
+type SetupOptions = {
+  componentsPage: ComponentsPage;
+  label?: string;
+  disabled?: boolean;
+  'data-aria-label'?: string;
+}
 
-// test('mdc-optgroup', async ({ componentsPage }) => {
-//   const optgroup = componentsPage.page.locator('mdc-optgroup');
+const setup = async (args: SetupOptions) => {
+  const { componentsPage, ...restArgs } = args;
+  await componentsPage.mount({
+    html: `
+      <mdc-optgroup
+        ${restArgs['data-aria-label'] ? `data-aria-label="${restArgs['data-aria-label']}"` : ''}
+        ${restArgs.disabled ? `disabled="${restArgs.disabled}"` : ''}
+        ${restArgs.label ? `label="${restArgs.label}"` : ''}
+      >
+        <mdc-option>Option label</mdc-option>
+      </mdc-optgroup>
+    `,
+    clearDocument: true,
+  });
+  const option = componentsPage.page.locator('mdc-optgroup');
+  await option.waitFor();
+  return option;
+};
 
-//   // initial check for the optgroup be visible on the screen:
-//   await optgroup.waitFor();
+test('mdc-optgroup', async ({ componentsPage }) => {
+  /**
+   * VISUAL REGRESSION
+   */
+  await test.step('visual-regression', async () => {
+    const markUpOptions = { createNewRow: true };
+    const optionGroupChildren = `
+      <mdc-option>Boston</mdc-option>
+      <mdc-option selected>Mexico</mdc-option>
+      <mdc-option>Toronto</mdc-option>
+      <mdc-option disabled="true">Canada</mdc-option>
+    `;
+    const optionSheet = new StickerSheet(componentsPage, 'mdc-optgroup', 'margin: 0.25rem');
+    optionSheet.setAttributes({ label: 'List options' });
+    optionSheet.setChildren(optionGroupChildren);
+    await optionSheet.createMarkupWithCombination({}, markUpOptions);
+    optionSheet.setAttributes({ disabled: 'true' });
+    optionSheet.setChildren(optionGroupChildren);
+    await optionSheet.createMarkupWithCombination({}, markUpOptions);
 
-//   /**
-//    * ACCESSIBILITY
-//    */
-//   await test.step('accessibility', async () => {
-//     await componentsPage.accessibility.checkForA11yViolations('optgroup-default');
-//   });
+    await optionSheet.mountStickerSheet({ role: 'listbox' });
+    await test.step('matches screenshot of element', async () => {
+      await componentsPage.visualRegression.takeScreenshot('mdc-optgroup', {
+        element: optionSheet.getWrapperContainer(),
+      });
+    });
+  });
 
-//   /**
-//    * VISUAL REGRESSION
-//    */
-//   await test.step('visual-regression', async () => {
-//     await test.step('matches screenshot of element', async () => {
-//       await componentsPage.visualRegression.takeScreenshot('mdc-optgroup', { element: optgroup });
-//     });
-//   });
+  /**
+   * ACCESSIBILITY
+   */
+  await test.step('accessibility', async () => {
+    await componentsPage.accessibility.checkForA11yViolations('option-default');
+  });
 
-//   /**
-//    * ATTRIBUTES
-//    */
-//   await test.step('attributes', async () => {
-//     await test.step('attribute X should be present on component by default', async () => {
-//       // TODO: add test here
-//     });
-//   });
+  /**
+   * ATTRIBUTES
+   */
+  await test.step('attributes', async () => {
+    const optgroup = await setup({ componentsPage });
 
-//   /**
-//    * INTERACTIONS
-//    */
-//   await test.step('interactions', async () => {
-//     await test.step('mouse/pointer', async () => {
-//       await test.step('component should fire callback x when clicking on it', async () => {
-//         // TODO: add test here
-//       });
-//     });
+    await test.step('should have header text label when the label attribute is passed', async () => {
+      await componentsPage.setAttributes(optgroup, { label: 'Primary Label' });
+      const textContent = await componentsPage.page.locator('mdc-text[part="header-text"]').textContent();
+      expect(textContent?.trim()).toBe('Primary Label');
+    });
 
-//     await test.step('focus', async () => {
-//       await test.step('component should be focusable with tab', async () => {
-//         // TODO: add test here
-//       });
-
-//       // add additional tests here, like tabbing through several parts of the component
-//     });
-
-//     await test.step('keyboard', async () => {
-//       await test.step('component should fire callback x when pressing y', async () => {
-//         // TODO: add test here
-//       });
-//     });
-//   });
-// });
+    await test.step('should be disabled when the disabled attribute is passed', async () => {
+      await componentsPage.setAttributes(optgroup, { disabled: 'true' });
+      await expect(optgroup).toHaveAttribute('disabled');
+    });
+  });
+});
