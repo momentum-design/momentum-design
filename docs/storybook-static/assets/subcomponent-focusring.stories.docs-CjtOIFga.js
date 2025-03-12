246c40ee2fb8840d72999975a63b49e824d71703
Momentum Design Automation <automation@momentum.design>
2025-03-12 14:53:02 +0000

    ci(docs): deploy documentation [skip ci] [skip release]

---
+++ docs/storybook-static/assets/subcomponent-focusring.stories.docs-CjtOIFga.js
@@ -0,0 +1,16 @@
+import{j as o}from"./jsx-runtime-N83kn9-W.js";import{useMDXComponents as i}from"./index-g1djAheZ.js";import{ae as n,af as s,ag as p,ah as m}from"./index-DXHtW7qR.js";import a from"./subcomponent-focusring.stories-BAKEYsWs.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-CR1wgCnq.js";import"../sb-preview/runtime.js";import"./index-D-8MO0q_.js";import"./index-Cef7vbu6.js";import"./index-DrFu-skq.js";import"./lit-element-CHllvULs.js";import"./index-HW6KrQZO.js";import"./index-BGjwsboe.js";import"./utils-D1SE5bce.js";import"./index-BzhWeMB0.js";import"./state-CWckTc1X.js";import"./buttonsimple.component-BPMM56DO.js";import"./DisabledMixin-DBt9didn.js";import"./button.constants-BtWRRBoE.js";import"./button.utils-CM5PFBkb.js";import"./index-DMeMarQJ.js";import"./index-BYMLCX4c.js";import"./iconprovider.component-BsPZ_dvP.js";import"./create-context-89xeped_.js";import"./index-CH7X7j54.js";import"./if-defined-C4tJgJ33.js";import"./index-Df2_Bjpk.js";import"./IconNameMixin-DXlfdGtk.js";import"./index-CATyfFxt.js";import"./text.constants-DJu2q-6E.js";import"./index-BGkqZMsC.js";import"./index-2qOrnSGO.js";import"./index-0B0Wdqby.js";import"./class-map-DVSk4OQh.js";import"./directive-Ctav8iJK.js";import"./index-QBanmcwD.js";import"./index-MDQUzAfz.js";import"./index-CFZpA2vE.js";import"./formfieldwrapper.component-63e8oEkZ.js";import"./FormInternalsMixin-CITZKMWK.js";import"./index-jA_v4E9Z.js";import"./index-BPoaZRae.js";import"./index-B0bXckmV.js";function e(r){const t={blockquote:"blockquote",p:"p",strong:"strong",...i(),...r.components};return o.jsxs(o.Fragment,{children:[o.jsx(n,{of:a}),`
+`,o.jsx(s,{children:"Focus Ring"}),`
+`,o.jsx(t.p,{children:o.jsx(t.strong,{children:"The Focus Ring provides a visual indication of focus for users navigating via keyboard or mouse."})}),`
+`,o.jsx(t.p,{children:`When the shared styles of the Focus Ring are applied to a component,
+that component in its focus state will automatically have the focus ring wrap
+around every form (like rounded corners, edges, etc.) without increasing its overall size.`}),`
+`,o.jsxs(t.blockquote,{children:[`
+`,o.jsxs(t.p,{children:[o.jsx(t.strong,{children:"Note:"}),`
+Currently, the Focus Ring is not exported as part of our Global Web Library.
+However, once there is an accessible component that requires the Focus Ring
+in its focus state (triggered by tab or click), the focus ring will wrap around that web component.`]}),`
+`]}),`
+`,o.jsx(t.p,{children:`For now, there is a temporary component that consumes the shared styles.
+As you select any shape from the options below, you will see the focus ring wrap around the component.`}),`
+`,o.jsx(p,{}),`
+`,o.jsx(m,{})]})}function $(r={}){const{wrapper:t}={...i(),...r.components};return t?o.jsx(t,{...r,children:o.jsx(e,{...r})}):e(r)}export{$ as default};
