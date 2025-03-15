c61a1b58ca6fbde825a90ddc0b545e88b38f1d55
Momentum Design Automation <automation@momentum.design>
2025-03-15 18:36:28 +0000

    ci(docs): deploy documentation [skip ci] [skip release]

---
+++ docs/storybook-static/assets/radio.stories-CcZS1RC3.js
@@ -0,0 +1,183 @@
+import"./index-vJaOFQTP.js";import{k as o}from"./lit-element-CHllvULs.js";import{a}from"./chunk-D5ZWXAHU-CGElDDNX.js";import{c as G,s as z}from"./commonArgTypes-BluK8w5L.js";import{h as B,d as I}from"./utils-D1SE5bce.js";import"./index-0an-ES2q.js";import"./index-HW6KrQZO.js";import"./if-defined-C4tJgJ33.js";import"./index-BIROzany.js";import"./formfieldwrapper.component-D9MUAnNV.js";import"./DisabledMixin-DBt9didn.js";import"./text.constants-DJu2q-6E.js";import"./FormInternalsMixin-DyD0hFmN.js";import"./v4-BnQ50LvX.js";import"./v4-CQkTLCs1.js";import"./state-CWckTc1X.js";import"./buttonsimple.component-SgL7LhI3.js";import"./button.constants-BXf5IXRf.js";import"./button.utils-ajxTJbst.js";import"./index-x2rBOadR.js";import"./iframe-DIMbNX8I.js";import"../sb-preview/runtime.js";import"./index-BYMLCX4c.js";import"./iconprovider.component-BsPZ_dvP.js";import"./create-context-89xeped_.js";const C=e=>o`
+  <mdc-radio
+    @change="${a("onchange")}"
+    @keydown="${a("onkeydown")}"
+    @focus="${a("onfocus")}"
+    @click="${a("onclick")}"
+    label="${e.label}"
+    name="radio"
+    value="option1"
+    help-text="${e["help-text"]}"
+    ?checked=${e.checked}
+    ?disabled=${e.disabled}
+    ?readonly=${e.readonly}
+    required-label="${e["required-label"]}"
+    data-aria-label="${e["data-aria-label"]}"
+  ></mdc-radio>
+`,se={title:"Components/radio",tags:["autodocs"],component:"mdc-radio",render:C,parameters:{badges:["stable"]},argTypes:{...G,...z,readonly:{control:{type:"boolean"}},disabled:{control:{type:"boolean"}},checked:{control:{type:"boolean"}},label:{control:{type:"text"}},name:{control:{type:"text"}},value:{control:{type:"text"}},"help-text":{control:{type:"text"}},"data-aria-label":{control:{type:"text"}},"required-label":{control:{type:"text"}},...B(["help-text-type"]),...I(["--mdc-radio-inner-circle-size","--mdc-radio-text-disabled-color","--mdc-radio-disabled-border-color","--mdc-radio-normal-border-color","--mdc-radio-inner-circle-normal-background","--mdc-radio-inner-circle-disabled-background","--mdc-radio-control-inactive-color","--mdc-radio-control-inactive-hover","--mdc-radio-control-inactive-pressed-color","--mdc-radio-control-inactive-disabled-background","--mdc-radio-control-active-color","--mdc-radio-control-active-hover-color","--mdc-radio-control-active-pressed-color","--mdc-radio-control-active-disabled-background"])}},r={args:{label:"Standard Plan",checked:!1,readonly:!1,disabled:!1}},l={render:()=>o`
+    <div style="display: flex; flex-direction: column;">
+      Radio Group for selecting plans
+      <mdc-radio name="plan" value="option1" label="Standard Plan" ></mdc-radio>
+      <mdc-radio name="plan" value="option2" label="Disabled Premium Plan" disabled></mdc-radio>
+      <mdc-radio name="plan" value="option3" label="Premium Plan"></mdc-radio>
+      <mdc-radio name="plan" value="option4" label="Premium Plan for family"></mdc-radio>
+      <mdc-radio name="plan" value="option5" label="Business Plan"></mdc-radio>
+      <mdc-radio name="plan" value="option5" label="Enterprise Plan"></mdc-radio>
+    </div>
+    <div style="display: flex; flex-direction: column; margin-top: 10px">
+      Radio Group for selecting billing cycle
+      <mdc-radio name="billing-cycle" value="option1" label="Weekly" disabled></mdc-radio>
+      <mdc-radio name="billing-cycle" value="option1" label="Monthly" checked></mdc-radio>
+      <mdc-radio name="billing-cycle" value="option2" label="Quaterly"></mdc-radio>
+      <mdc-radio name="billing-cycle" value="option2" label="Yearly"></mdc-radio>
+    </div>
+    `,parameters:{docs:{description:{story:"Radio buttons with the same `name` attribute are grouped together. When radio buttons share the same `name`, selecting one will automatically deselect the others in the group. The example includes various states of radio buttons such as disabled, readonly, and different labels. We can use flex layout to display radio buttons in a column."}}}},t={args:{"data-aria-label":"Radio button without label"},parameters:{docs:{description:{story:"If we don't want to display a label, we can omit the 'label' attribute, but it is recommended to pass an aria label through data-aria-label for accessibility. "}}}},d={render:()=>o`
+    <div style="display: flex; flex-direction: column;">
+      <mdc-radio label="Disabled Standard Plan" disabled></mdc-radio>
+      <mdc-radio label="Selected But Disabled Standard Plan" disabled checked></mdc-radio>
+    </div>`,parameters:{docs:{description:{story:"To disable the radio button, use the `disabled` attribute. "}}}},i={render:()=>o`
+    <div style="display: flex; flex-direction: column;">
+      <mdc-radio name="read" value="option1" label="Unselected" readonly></mdc-radio>
+      <mdc-radio name="read" value="option2" label="Selected" readonly checked></mdc-radio>
+    </div>`,parameters:{docs:{description:{story:"To make the radio button read-only, use the `readonly` attribute. "}}}},n={args:{label:"Standard Plan","help-text":"Standard plan provides basic features",checked:!1,disabled:!1,readonly:!1},parameters:{docs:{description:{story:"To add help text to the radio button, use the `help-text` attribute. "}}}},c=()=>o`
+    <form @submit=${s=>{s.preventDefault();const q=new FormData(s.target).get("course-plan");a("Form Submitted")({value:q})}}>
+      <fieldset>
+        <legend>Form Example</legend>
+      <mdc-radio name="course-plan" value="standard" data-aria-label="Standard Plan" label="Standard Plan">
+      </mdc-radio>
+      <mdc-radio name="course-plan" value="premium" data-aria-label="Premium Plan" label="Premium Plan" >
+      </mdc-radio>
+      <mdc-radio
+        name="course-plan"
+        value="enterprise"
+        required-label="required"
+        data-aria-label="Enterprise Plan"
+        label="Enterprise Plan"
+        validation-message="Select a plan to continue">
+      </mdc-radio>
+      <br />
+      <div style='display: flex; gap: 0.25rem;'>
+        <mdc-button type="submit" size='24'>Submit</mdc-button>
+        <mdc-button type="reset" size='24' variant='secondary'>Reset</mdc-button>
+      </div>
+      </fieldset>
+    </form>
+  `;var m,p,u;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    label: 'Standard Plan',
+    checked: false,
+    readonly: false,
+    disabled: false
+  }
+}`,...(u=(p=r.parameters)==null?void 0:p.docs)==null?void 0:u.source}}};var b,y,h;l.parameters={...l.parameters,docs:{...(b=l.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  render: () => html\`
+    <div style="display: flex; flex-direction: column;">
+      Radio Group for selecting plans
+      <mdc-radio name="plan" value="option1" label="Standard Plan" ></mdc-radio>
+      <mdc-radio name="plan" value="option2" label="Disabled Premium Plan" disabled></mdc-radio>
+      <mdc-radio name="plan" value="option3" label="Premium Plan"></mdc-radio>
+      <mdc-radio name="plan" value="option4" label="Premium Plan for family"></mdc-radio>
+      <mdc-radio name="plan" value="option5" label="Business Plan"></mdc-radio>
+      <mdc-radio name="plan" value="option5" label="Enterprise Plan"></mdc-radio>
+    </div>
+    <div style="display: flex; flex-direction: column; margin-top: 10px">
+      Radio Group for selecting billing cycle
+      <mdc-radio name="billing-cycle" value="option1" label="Weekly" disabled></mdc-radio>
+      <mdc-radio name="billing-cycle" value="option1" label="Monthly" checked></mdc-radio>
+      <mdc-radio name="billing-cycle" value="option2" label="Quaterly"></mdc-radio>
+      <mdc-radio name="billing-cycle" value="option2" label="Yearly"></mdc-radio>
+    </div>
+    \`,
+  parameters: {
+    docs: {
+      description: {
+        story: 'Radio buttons with the same \`name\` attribute are grouped together. ' + 'When radio buttons share the same \`name\`, selecting one will automatically deselect the others in the ' + 'group. The example includes various states of radio buttons such as disabled, readonly, ' + 'and different labels. We can use flex layout to display radio buttons in a column.'
+      }
+    }
+  }
+}`,...(h=(y=l.parameters)==null?void 0:y.docs)==null?void 0:h.source}}};var v,f,g;t.parameters={...t.parameters,docs:{...(v=t.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    'data-aria-label': 'Radio button without label'
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: 'If we don\\'t want to display a label, we can omit the \\'label\\' attribute, ' + 'but it is recommended to pass an aria label through data-aria-label for accessibility. '
+      }
+    }
+  }
+}`,...(g=(f=t.parameters)==null?void 0:f.docs)==null?void 0:g.source}}};var x,P,S;d.parameters={...d.parameters,docs:{...(x=d.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  render: () => html\`
+    <div style="display: flex; flex-direction: column;">
+      <mdc-radio label="Disabled Standard Plan" disabled></mdc-radio>
+      <mdc-radio label="Selected But Disabled Standard Plan" disabled checked></mdc-radio>
+    </div>\`,
+  parameters: {
+    docs: {
+      description: {
+        story: 'To disable the radio button, use the \`disabled\` attribute. '
+      }
+    }
+  }
+}`,...(S=(P=d.parameters)==null?void 0:P.docs)==null?void 0:S.source}}};var k,w,D;i.parameters={...i.parameters,docs:{...(k=i.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  render: () => html\`
+    <div style="display: flex; flex-direction: column;">
+      <mdc-radio name="read" value="option1" label="Unselected" readonly></mdc-radio>
+      <mdc-radio name="read" value="option2" label="Selected" readonly checked></mdc-radio>
+    </div>\`,
+  parameters: {
+    docs: {
+      description: {
+        story: 'To make the radio button read-only, use the \`readonly\` attribute. '
+      }
+    }
+  }
+}`,...(D=(w=i.parameters)==null?void 0:w.docs)==null?void 0:D.source}}};var R,T,E;n.parameters={...n.parameters,docs:{...(R=n.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  args: {
+    label: 'Standard Plan',
+    'help-text': 'Standard plan provides basic features',
+    checked: false,
+    disabled: false,
+    readonly: false
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: 'To add help text to the radio button, use the \`help-text\` attribute. '
+      }
+    }
+  }
+}`,...(E=(T=n.parameters)==null?void 0:T.docs)==null?void 0:E.source}}};var $,W,F;c.parameters={...c.parameters,docs:{...($=c.parameters)==null?void 0:$.docs,source:{originalSource:`() => {
+  const handleSubmit = (event: Event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target as HTMLFormElement);
+    const selectedValue = formData.get('course-plan');
+    action('Form Submitted')({
+      value: selectedValue
+    });
+  };
+  return html\`
+    <form @submit=\${handleSubmit}>
+      <fieldset>
+        <legend>Form Example</legend>
+      <mdc-radio name="course-plan" value="standard" data-aria-label="Standard Plan" label="Standard Plan">
+      </mdc-radio>
+      <mdc-radio name="course-plan" value="premium" data-aria-label="Premium Plan" label="Premium Plan" >
+      </mdc-radio>
+      <mdc-radio
+        name="course-plan"
+        value="enterprise"
+        required-label="required"
+        data-aria-label="Enterprise Plan"
+        label="Enterprise Plan"
+        validation-message="Select a plan to continue">
+      </mdc-radio>
+      <br />
+      <div style='display: flex; gap: 0.25rem;'>
+        <mdc-button type="submit" size='24'>Submit</mdc-button>
+        <mdc-button type="reset" size='24' variant='secondary'>Reset</mdc-button>
+      </div>
+      </fieldset>
+    </form>
+  \`;
+}`,...(F=(W=c.parameters)==null?void 0:W.docs)==null?void 0:F.source}}};const me=["Example","withNameGroup","WithoutLabel","Disabled","ReadOnly","WithHelpText","RadioInForm"];export{d as Disabled,r as Example,c as RadioInForm,i as ReadOnly,n as WithHelpText,t as WithoutLabel,me as __namedExportsOrder,se as default,l as withNameGroup};
---
+++ docs/storybook-static/assets/subcomponent-focusring.stories.docs-DLynQH57.js
@@ -0,0 +1,16 @@
+import{j as o}from"./jsx-runtime-N83kn9-W.js";import{useMDXComponents as i}from"./index-g1djAheZ.js";import{ae as n,af as s,ag as p,ah as m}from"./index-BzQ4M8lz.js";import a from"./subcomponent-focusring.stories-DypkTmfx.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-DIMbNX8I.js";import"../sb-preview/runtime.js";import"./index-D-8MO0q_.js";import"./index-Cef7vbu6.js";import"./index-DrFu-skq.js";import"./lit-element-CHllvULs.js";import"./index-HW6KrQZO.js";import"./index-BIROzany.js";import"./utils-D1SE5bce.js";import"./index-0an-ES2q.js";import"./state-CWckTc1X.js";import"./buttonsimple.component-SgL7LhI3.js";import"./DisabledMixin-DBt9didn.js";import"./button.constants-BXf5IXRf.js";import"./button.utils-ajxTJbst.js";import"./index-x2rBOadR.js";import"./index-BYMLCX4c.js";import"./iconprovider.component-BsPZ_dvP.js";import"./create-context-89xeped_.js";import"./index-BrtaSY_d.js";import"./if-defined-C4tJgJ33.js";import"./index-CJXjf_Gb.js";import"./IconNameMixin-DXlfdGtk.js";import"./index-BpQqNgHt.js";import"./text.constants-DJu2q-6E.js";import"./index-BGkqZMsC.js";import"./index-CZTcb1kv.js";import"./index-BKnARENY.js";import"./class-map-DVSk4OQh.js";import"./directive-Ctav8iJK.js";import"./index-D3e8Qy4X.js";import"./index-DUT3wFRH.js";import"./index-DuhXl23L.js";import"./formfieldwrapper.component-D9MUAnNV.js";import"./input.constants-6ff2MBeN.js";import"./FormInternalsMixin-DyD0hFmN.js";import"./v4-BnQ50LvX.js";import"./index-vJaOFQTP.js";import"./index-CRnLc7w-.js";import"./index-C9n0Y5XE.js";function e(r){const t={blockquote:"blockquote",p:"p",strong:"strong",...i(),...r.components};return o.jsxs(o.Fragment,{children:[o.jsx(n,{of:a}),`
+`,o.jsx(s,{children:"Focus Ring"}),`
+`,o.jsx(t.p,{children:o.jsx(t.strong,{children:"The Focus Ring provides a visual indication of focus for users navigating via keyboard or mouse."})}),`
+`,o.jsx(t.p,{children:`When the shared styles of the Focus Ring are applied to a component,
+that component in its focus state will automatically have the focus ring wrap
+around every form (like rounded corners, edges, etc.) without increasing its overall size.`}),`
+`,o.jsxs(t.blockquote,{children:[`
+`,o.jsxs(t.p,{children:[o.jsx(t.strong,{children:"Note:"}),`
+Currently, the Focus Ring is not exported as part of our Global Web Library.
+However, once there is an accessible component that requires the Focus Ring
+in its focus state (triggered by tab or click), the focus ring will wrap around that web component.`]}),`
+`]}),`
+`,o.jsx(t.p,{children:`For now, there is a temporary component that consumes the shared styles.
+As you select any shape from the options below, you will see the focus ring wrap around the component.`}),`
+`,o.jsx(p,{}),`
+`,o.jsx(m,{})]})}function to(r={}){const{wrapper:t}={...i(),...r.components};return t?o.jsx(t,{...r,children:o.jsx(e,{...r})}):e(r)}export{to as default};
