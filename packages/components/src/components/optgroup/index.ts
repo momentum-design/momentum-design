3a581a8128d1c0cafff174ec263f2e7c6289e4a7
Arjun Kesava <arjun.kesava@gmail.com>
2025-03-17 12:25:13 +0530

    perf: build options and options group components. (#1139)
    * perf: generate options component
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * perf: build option component
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore: add role of listbox for lisitem
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore: add a11y fields for option component
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * perf: build option group component
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore: fix a11y issues on option and remove selected value update on click
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * perf: add stories for optgroup component
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore: add disabled color in cssproperty for optiongroup
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore: comment e2e specs for both option and option group components
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore: add events for option
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * fix: rm optgroup types valueof
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore: unset background on disabled for listitem
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * perf: extend option with listitem
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * chore: address review comments
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    ---------
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    Co-authored-by: Supriya M <30731236+supminn@users.noreply.github.com>

---
+++ packages/components/src/components/optgroup/index.ts
@@ -0,0 +1,13 @@
+import OptGroup from './optgroup.component';
+import { TAG_NAME } from './optgroup.constants';
+import '../text';
+
+OptGroup.register(TAG_NAME);
+
+declare global {
+    interface HTMLElementTagNameMap {
+        ['mdc-optgroup']: OptGroup
+    }
+}
+
+export default OptGroup;
