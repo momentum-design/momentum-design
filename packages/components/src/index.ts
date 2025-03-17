10e8d78cb06168c3e9618395801b62ac4450f6e8
Gabriel Lee <chihonglee777@gmail.com>
2025-03-17 10:54:01 +0000

    fix(coachmark): export placement type

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -30,6 +30,7 @@ import Textarea from './components/textarea';
 
 import type { SpinnerSize, SpinnerVariant } from './components/spinner/spinner.types';
 import type { TextType } from './components/text/text.types';
+import type { PopoverPlacement } from './components/popover/popover.types';
 
 export {
   Avatar,
@@ -65,5 +66,6 @@ export type {
   TextType,
   SpinnerSize,
   SpinnerVariant,
+  PopoverPlacement,
 };
 export { inMemoryCache, webAPIIconsCache };
