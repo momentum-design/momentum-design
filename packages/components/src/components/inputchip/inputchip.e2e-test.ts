d505b1fcfb4af11d64993d2b9bcab27ebe9120ff
Supriya M <30731236+supminn@users.noreply.github.com>
2025-03-18 14:36:04 +0530

    test: chip component test cases (#1157)
    * chore: generated component
    
    * feat: label chip with icon and colors
    
    * feat: alertchip comp
    
    * refactor: updated to support avatar
    
    * refactor: chip to extend buttonsimple
    
    * feat: filter chip
    
    * chore: created input chip
    
    * refactor(wip): input chip
    
    * feat: input chip component
    
    * refactor: alertchip extended from button
    
    * fix: controls on storybook
    
    * docs: updated events on docs
    
    * refactor: interactions on filterchip
    
    * refactor: removed avatar and char limit from chip
    
    * docs: updated docs and stories
    
    * fix: dispatch custom event for inputchip
    
    * chore: cleanup unused file
    
    * revert: test case for chip
    
    * style(fix): font color and icon styling
    
    * refactor: updated inputchip
    
    * fix: events on chip and storybook
    
    * refactor: added stories; addressed comments
    
    * fix: style issues and theme token
    
    * chore: updated index
    
    * style: css props added
    
    * fix: cssproperty naming
    
    * refactor: review comments
    
    * test(wip): label chip test cases
    
    * test(wip): alert chip test
    
    * test: chip component test cases
    
    * test: test cases with interactions
    
    * chore: updated snapshots
    
    * chore: cleanup code
    
    * fix: inputchip test cases
    
    * test: attribute to support icon in inputchip
    
    * fix: defining args
    
    * fix: alertchip icon colors
    
    * style: fixed inputchip styling on hover and active
    
    * test: updated stickersheet and interactions
    
    ---------
    
    Co-authored-by: Deeya Upadhyay <58636958+2020Deeya@users.noreply.github.com>
    Co-authored-by: Shakti Singh <30585570+shakti97@users.noreply.github.com>

--- packages/components/src/components/inputchip/inputchip.e2e-test.ts
+++ packages/components/src/components/inputchip/inputchip.e2e-test.ts
@@ -1,18 +1,89 @@
-import { test } from '../../../config/playwright/setup';
+import { expect } from '@playwright/test';
+import { ComponentsPage, test } from '../../../config/playwright/setup';
+import StickerSheet from '../../../config/playwright/setup/utils/Stickersheet';
+import type { IconNames } from '../icon/icon.types';
 
-test.beforeEach(async ({ componentsPage }) => {
+type SetupOptions = {
+  componentsPage: ComponentsPage;
+  label: string;
+  iconName?: IconNames;
+  error?: boolean;
+  disabled?: boolean;
+  clearAriaLabel: string;
+  secondChipForFocus?: boolean;
+};
+
+const setup = async (args: SetupOptions) => {
+  const { componentsPage, ...restArgs } = args;
   await componentsPage.mount({
     html: `
-        <mdc-inputchip />
-      `,
+    ${restArgs.secondChipForFocus ? '<div id="wrapper">' : ''}
+    <mdc-inputchip
+      label="${restArgs.label}"
+      clear-aria-label="${restArgs.clearAriaLabel}"
+      ${restArgs.iconName ? `icon-name="${restArgs.iconName}"` : ''}
+      ${restArgs.error ? 'error' : ''}
+      ${restArgs.disabled ? 'disabled' : ''}
+    ></mdc-inputchip>
+    ${restArgs.secondChipForFocus ? '<mdc-inputchip label="Chip"></mdc-inputchip></div>' : ''}
+    `,
+    clearDocument: true,
   });
-});
 
-test.skip('mdc-inputchip', async ({ componentsPage }) => {
-  const inputchip = componentsPage.page.locator('mdc-inputchip');
+  const element = restArgs.secondChipForFocus
+    ? componentsPage.page.locator('div#wrapper')
+    : componentsPage.page.locator('mdc-inputchip');
+  await element.waitFor();
 
-  // initial check for the inputchip be visible on the screen:
-  await inputchip.waitFor();
+  const firstChip = await componentsPage.page.locator('mdc-inputchip').first();
+  return firstChip;
+};
+
+test('mdc-inputchip', async ({ componentsPage }) => {
+  /**
+   * VISUAL REGRESSION
+   */
+  await test.step('visual-regression', async () => {
+    const inputchipStickerSheet = new StickerSheet(componentsPage, 'mdc-inputchip');
+    inputchipStickerSheet.setAttributes({ label: 'Label', 'clear-aria-label': 'Clear' });
+    await inputchipStickerSheet.createMarkupWithCombination({});
+    inputchipStickerSheet.setAttributes({ label: 'Error', error: '', 'clear-aria-label': 'Clear' });
+    await inputchipStickerSheet.createMarkupWithCombination({});
+    inputchipStickerSheet.setAttributes({ label: 'Disabled', disabled: '', 'clear-aria-label': 'Clear' });
+    await inputchipStickerSheet.createMarkupWithCombination({});
+    inputchipStickerSheet.setAttributes({ label: 'Error Disabled',
+      error: '',
+      disabled: '',
+      'clear-aria-label': 'Clear' });
+    await inputchipStickerSheet.createMarkupWithCombination({});
+    inputchipStickerSheet.setAttributes({ label: 'Label',
+      'clear-aria-label': 'Clear',
+      'icon-name': 'placeholder-bold' });
+    await inputchipStickerSheet.createMarkupWithCombination({});
+    inputchipStickerSheet.setAttributes({ label: 'Error',
+      error: '',
+      'clear-aria-label': 'Clear',
+      'icon-name': 'placeholder-bold' });
+    await inputchipStickerSheet.createMarkupWithCombination({});
+    inputchipStickerSheet.setAttributes({ label: 'Disabled',
+      disabled: '',
+      'clear-aria-label': 'Clear',
+      'icon-name': 'placeholder-bold' });
+    await inputchipStickerSheet.createMarkupWithCombination({});
+    inputchipStickerSheet.setAttributes({ label: 'Error Disabled',
+      disabled: '',
+      error: '',
+      'clear-aria-label': 'Clear',
+      'icon-name': 'placeholder-bold' });
+    await inputchipStickerSheet.createMarkupWithCombination({});
+    await inputchipStickerSheet.mountStickerSheet({
+      wrapperStyle: 'display: flex; flex-direction: column; gap: 0.5rem',
+    });
+    const container = inputchipStickerSheet.getWrapperContainer();
+    await test.step('matches screenshot of element', async () => {
+      await componentsPage.visualRegression.takeScreenshot('mdc-inputchip', { element: container });
+    });
+  });
 
   /**
    * ACCESSIBILITY
@@ -22,20 +93,31 @@ test.skip('mdc-inputchip', async ({ componentsPage }) => {
   });
 
   /**
-   * VISUAL REGRESSION
-   */
-  await test.step('visual-regression', async () => {
-    await test.step('matches screenshot of element', async () => {
-      await componentsPage.visualRegression.takeScreenshot('mdc-inputchip', { element: inputchip });
-    });
-  });
-
-  /**
    * ATTRIBUTES
    */
   await test.step('attributes', async () => {
-    await test.step('attribute X should be present on component by default', async () => {
-      // TODO: add test here
+    const inputchip = await setup({ componentsPage,
+      label: 'Label',
+      clearAriaLabel: 'Clear',
+      iconName: 'placeholder-bold' });
+    await test.step('attribute label should be present on component by default', async () => {
+      await expect(inputchip).toHaveAttribute('label', 'Label');
+    });
+
+    await test.step('attribute error should be set on component', async () => {
+      await componentsPage.setAttributes(inputchip, { error: '' });
+      await expect(inputchip).toHaveAttribute('error');
+      await componentsPage.removeAttribute(inputchip, 'error');
+    });
+
+    await test.step('attribute disabled should be set on component', async () => {
+      await componentsPage.setAttributes(inputchip, { disabled: '' });
+      await expect(inputchip).toHaveAttribute('disabled');
+      await componentsPage.removeAttribute(inputchip, 'disabled');
+    });
+
+    await test.step('attribute icon-name should be present on component by default', async () => {
+      await expect(inputchip).toHaveAttribute('icon-name', 'placeholder-bold');
     });
   });
 
@@ -43,23 +125,44 @@ test.skip('mdc-inputchip', async ({ componentsPage }) => {
    * INTERACTIONS
    */
   await test.step('interactions', async () => {
-    await test.step('mouse/pointer', async () => {
-      await test.step('component should fire callback x when clicking on it', async () => {
-        // TODO: add test here
+    const inputchip = await setup({
+      componentsPage,
+      label: 'Label',
+      clearAriaLabel: 'Clear',
+      secondChipForFocus: true });
+    const btn = inputchip.locator('mdc-button');
+
+    await componentsPage.page.evaluate(() => {
+      const chip = document.getElementsByTagName('mdc-inputchip')[0];
+      chip.addEventListener('click', () => {
+        chip.classList.toggle('chip-listener');
       });
+      (chip as HTMLElement).onclick = () => {
+        chip.classList.toggle('chip-onclick');
+      };
     });
 
     await test.step('focus', async () => {
       await test.step('component should be focusable with tab', async () => {
-        // TODO: add test here
+        await componentsPage.actionability.pressTab();
+        await expect(btn).toBeFocused();
+        await componentsPage.actionability.pressTab();
+        await expect(btn).not.toBeFocused();
       });
+    });
 
-      // add additional tests here, like tabbing through several parts of the component
+    await test.step('mouse/pointer', async () => {
+      await test.step('component should fire onclick when clicking chip', async () => {
+        await btn.click();
+        await expect(inputchip).toHaveClass('chip-listener chip-onclick');
+      });
     });
 
     await test.step('keyboard', async () => {
-      await test.step('component should fire callback x when pressing y', async () => {
-        // TODO: add test here
+      await test.step('component should fire onclick when pressing enter', async () => {
+        await componentsPage.actionability.pressTab();
+        await componentsPage.page.keyboard.press('Enter');
+        await expect(inputchip).toHaveClass('chip-listener chip-onclick');
       });
     });
   });
