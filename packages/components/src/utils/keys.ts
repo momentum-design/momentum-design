a6edec35d4f10c06deb1de1c1be497d3aa7a3fd4
Arjun Kesava P <arjun.kesava@gmail.com>
2025-03-19 08:17:02 +0530

    perf: move keys from list to a utils file
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>

---
+++ packages/components/src/utils/keys.ts
@@ -0,0 +1,11 @@
+const KEYS = {
+  ARROW_DOWN: 'ArrowDown',
+  ARROW_UP: 'ArrowUp',
+  END: 'End',
+  ENTER: 'Enter',
+  ESCAPE: 'Escape',
+  HOME: 'Home',
+  SPACE: ' ',
+} as const;
+
+export { KEYS };
