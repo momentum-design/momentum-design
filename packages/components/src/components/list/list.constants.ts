a153a741c27b1b8255127f711a926de554e4c39a
Arjun Kesava P <arjun.kesava@gmail.com>
2025-03-19 08:17:02 +0530

    perf: move keys from list to a utils file
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>

--- packages/components/src/components/list/list.constants.ts
+++ packages/components/src/components/list/list.constants.ts
@@ -2,13 +2,6 @@ import utils from '../../utils/tag-name';
 
 const TAG_NAME = utils.constructTagName('list');
 
-const KEYS = {
-  ARROW_DOWN: 'ArrowDown',
-  ARROW_UP: 'ArrowUp',
-  HOME: 'Home',
-  END: 'End',
-} as const;
-
 const HEADER_ID = 'header-id';
 
-export { TAG_NAME, KEYS, HEADER_ID };
+export { TAG_NAME, HEADER_ID };
