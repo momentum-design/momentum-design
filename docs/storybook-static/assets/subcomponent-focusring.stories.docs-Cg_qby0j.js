7a80c3f3ae9271e2e3de64a45b239564bcb25a18
Momentum Design Automation <automation@momentum.design>
2025-03-25 10:07:31 +0000

    ci(docs): deploy documentation [skip ci] [skip release]

---
+++ docs/storybook-static/assets/subcomponent-focusring.stories.docs-Cg_qby0j.js
@@ -0,0 +1,16 @@
+import{j as o}from"./jsx-runtime-N83kn9-W.js";import{useMDXComponents as e}from"./index-g1djAheZ.js";import{ae as n,af as s,ag as p,ah as m}from"./index-CZTX4HxW.js";import a from"./subcomponent-focusring.stories-CpDZPnSk.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-DQTlCPth.js";import"../sb-preview/runtime.js";import"./index-D-8MO0q_.js";import"./index-Cef7vbu6.js";import"./index-DrFu-skq.js";import"./lit-element-D5KKan5q.js";import"./index-C9z9WAEj.js";import"./index-BX3Tn-Br.js";import"./utils-D1SE5bce.js";import"./index-C5s8T0-k.js";import"./state-CSSggq2n.js";import"./buttonsimple.component-CNln8tgM.js";import"./DisabledMixin-BZruwOeC.js";import"./TabIndexMixin-CsrHswKP.js";import"./button.constants-CBmzmO0q.js";import"./button.utils-DKnTcKwe.js";import"./index-BQJhEbqW.js";import"./index-2PiEGcHV.js";import"./iconprovider.component-Bvm-70EG.js";import"./create-context-89xeped_.js";import"./index-W9VCAbUC.js";import"./if-defined-D5BV9-c0.js";import"./index-Du_TMo4x.js";import"./IconNameMixin-CujGSn0t.js";import"./index-CsVoa1Ay.js";import"./text.constants-ZF0jM5wn.js";import"./index-D3DhXfcW.js";import"./index-ByTVkBzw.js";import"./index-D3X7Ln_7.js";import"./class-map-BcCVsMyu.js";import"./directive-Ctav8iJK.js";import"./index-DSuZjFpw.js";import"./index-D-aOYwDK.js";import"./index-7QET6-5o.js";import"./formfieldwrapper.component-CzpAXlhk.js";import"./input.constants-BmYxuDFu.js";import"./DataAriaLabelMixin-D53Z5CZ7.js";import"./FormInternalsMixin-Dg5j8Yib.js";import"./base-DIkkzJ-c.js";import"./v4-CmTdKEVZ.js";import"./index-DeuyWmK_.js";import"./index-Bh_vAGdG.js";import"./index-Qnylws7B.js";function i(r){const t={blockquote:"blockquote",p:"p",strong:"strong",...e(),...r.components};return o.jsxs(o.Fragment,{children:[o.jsx(n,{of:a}),`
+`,o.jsx(s,{children:"Focus Ring"}),`
+`,o.jsx(t.p,{children:o.jsx(t.strong,{children:"The Focus Ring provides a visual indication of focus for users navigating via keyboard or mouse."})}),`
+`,o.jsx(t.p,{children:`When the shared styles of the Focus Ring are applied to a component,
+that component in its focus state will automatically have the focus ring wrap
+around every form (like rounded corners, edges, etc.) without increasing its overall size.`}),`
+`,o.jsxs(t.blockquote,{children:[`
+`,o.jsxs(t.p,{children:[o.jsx(t.strong,{children:"Note:"}),`
+Currently, the Focus Ring is not exported as part of our Global Web Library.
+However, once there is an accessible component that requires the Focus Ring
+in its focus state (triggered by tab or click), the focus ring will wrap around that web component.`]}),`
+`]}),`
+`,o.jsx(t.p,{children:`For now, there is a temporary component that consumes the shared styles.
+As you select any shape from the options below, you will see the focus ring wrap around the component.`}),`
+`,o.jsx(p,{}),`
+`,o.jsx(m,{})]})}function eo(r={}){const{wrapper:t}={...e(),...r.components};return t?o.jsx(t,{...r,children:o.jsx(i,{...r})}):i(r)}export{eo as default};
