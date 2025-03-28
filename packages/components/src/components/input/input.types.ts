224459357efd126179fafae2f72a26f93d5479e0
Supriya M <30731236+supminn@users.noreply.github.com>
2025-03-28 15:57:42 +0530

    docs: search field component, storybook, docs and test cases (#1180)
    * chore: initial structure of component
    
    * fix: added disabled mode
    
    * docs: storybook and docs
    
    * test(wip): e2e tests for search field
    
    * refactor: support input chip
    
    * feat: including slot for filters using inputchip
    
    * test(wip): test cases and snapshots
    
    * test(wip): added one more snapshot
    
    * fix: hiding native close button
    
    * fix: restrict required field
    
    * refactor(wip): restrict using inputchips only
    
    * refactor: restrict to render only inputchips
    
    * test: fixed all test cases and stories
    
    * test: with filterchip
    
    * fix: adding a button for firefox focus fix
    
    * chore: removed unused type file
    
    * fix: styling for not wrapping text on chip
    
    * fix: scrollable input with chip filters
    
    * style: container width
    
    * fix: styling issue
    
    * fix: min width of input
    
    * chore: updated snapshots
    
    * fix: styling and attributes
    
    * chore: removed unused style
    
    * chore: updated stories
    
    * refactor: updated input and searchfield as per review comments
    
    * chore: removed code
    
    * refactor: input chip to support custom remove event
    
    * fix: focus ring issue on searchfield with chips
    
    * docs: updated docs
    
    * chore: removed extra chips

--- packages/components/src/components/input/input.types.ts
+++ packages/components/src/components/input/input.types.ts
@@ -1,6 +1,8 @@
 import type { ValueOf } from '../../utils/types';
-import { AUTO_CAPITALIZE } from './input.constants';
+import { AUTO_CAPITALIZE, AUTO_COMPLETE, INPUT_TYPE } from './input.constants';
 
 type AutoCapitalizeType = ValueOf<typeof AUTO_CAPITALIZE>;
+type AutoCompleteType = ValueOf<typeof AUTO_COMPLETE>;
+type InputType = ValueOf<typeof INPUT_TYPE>;
 
-export { AutoCapitalizeType };
+export { AutoCapitalizeType, AutoCompleteType, InputType };
---
+++ packages/components/src/components/searchfield/index.ts
@@ -0,0 +1,15 @@
+import Searchfield from './searchfield.component';
+import { TAG_NAME } from './searchfield.constants';
+import '../button';
+import '../icon';
+import '../text';
+
+Searchfield.register(TAG_NAME);
+
+declare global {
+    interface HTMLElementTagNameMap {
+        ['mdc-searchfield']: Searchfield
+    }
+}
+
+export default Searchfield;
