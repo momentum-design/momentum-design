21017e797cddd3da8ee2665a0b66ed1c6fe2c3ea
Pankaj Kumar <98078859+kpankaj3@users.noreply.github.com>
2025-04-02 10:30:51 +0530

    feat: screenreader announcement component (#1184)
    * chore: screenreader announcement component
    
    * fix: pr review - added identity attribute and updated tests
    
    * fix: updated storybook page + component description
    
    * docs: updated the storybook page note
    
    * chore: refactored docs indentation:
    
    ---------
    
    Co-authored-by: Supriya M <30731236+supminn@users.noreply.github.com>
    Co-authored-by: Supriya M <sansup49@gmail.com>

---
+++ packages/components/src/components/screenreaderannouncer/screenreaderannouncer.constants.ts
@@ -0,0 +1,18 @@
+import utils from '../../utils/tag-name';
+
+const TAG_NAME = utils.constructTagName('screenreaderannouncer');
+
+const ARIA_LIVE_VALUES = {
+  ASSERTIVE: 'assertive',
+  POLITE: 'polite',
+  OFF: 'off',
+} as const;
+
+const DEFAULTS = {
+  ARIA_LIVE: ARIA_LIVE_VALUES.POLITE,
+  DELAY: 150,
+  IDENTITY: 'mdc-screenreaderannouncer-identity',
+  TIMEOUT: 20_000,
+} as const;
+
+export { ARIA_LIVE_VALUES, DEFAULTS, TAG_NAME };
