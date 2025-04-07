88c2ebf78cf445c7ef4138945c1667da40d61239
Devesh <deveshpr@cisco.com>
2025-04-07 11:29:58 +0530

    fix: export events

--- packages/components/src/components/tablist/tablist.types.ts
+++ packages/components/src/components/tablist/tablist.types.ts
@@ -1,4 +1,10 @@
 import { ARROW_BUTTON_DIRECTION } from './tablist.constants';
 import type { ValueOf } from '../../utils/types';
 
-export type ArrowButtonDirectionType = ValueOf<typeof ARROW_BUTTON_DIRECTION>;
+interface Events {
+  onChangeEvent: CustomEvent;
+}
+
+type ArrowButtonDirectionType = ValueOf<typeof ARROW_BUTTON_DIRECTION>;
+
+export { ArrowButtonDirectionType, Events };
