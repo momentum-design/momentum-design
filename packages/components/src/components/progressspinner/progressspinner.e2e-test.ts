5b696b8db634a8fea89cfabdddbe90ec75cb853f
Shakti Singh <30585570+shakti97@users.noreply.github.com>
2025-04-09 14:29:27 +0530

    feat: build progress spinner component (#1203)
    * chore: added progress spinner
    
    * chore: build progress spinner component
    
    * chore: added storybook
    
    * chore: added E2E specs for progress spinner
    
    * feat: build progress spinner component
    
    * chore: updated default progress in story"
    
    * chore: addressed review comments

---
+++ packages/components/src/components/progressspinner/progressspinner.e2e-test.ts
@@ -0,0 +1,100 @@
+import { expect } from '@playwright/test';
+import { ComponentsPage, test } from '../../../config/playwright/setup';
+import StickerSheet from '../../../config/playwright/setup/utils/Stickersheet';
+
+type SetupOptions = {
+  componentsPage: ComponentsPage;
+  value?: string;
+  error?: boolean;
+  'data-aria-label'?: string;
+};
+
+const setup = async (args: SetupOptions) => {
+  const { componentsPage, ...restArgs } = args;
+  await componentsPage.mount({
+    html: `
+      <mdc-progressspinner
+        ${restArgs.value ? `value="${restArgs.value}"` : ''}
+        ${restArgs.error ? 'error' : ''}
+        ${restArgs['data-aria-label'] ? `data-aria-label="${restArgs['data-aria-label']}"` : ''}
+      >
+      </mdc-progressspinner>
+    `,
+    clearDocument: true,
+  });
+  const progressspinner = componentsPage.page.locator('mdc-progressspinner');
+  await progressspinner.waitFor();
+  return progressspinner;
+};
+
+const attributeTestCases = async (componentsPage: ComponentsPage) => {
+  const progressspinner = await setup({ componentsPage });
+  await test.step('default attributes', async () => {
+    await expect(progressspinner).toHaveAttribute('value', '0');
+  });
+  await test.step('attributes are set properly', async () => {
+    await componentsPage.setAttributes(progressspinner, { value: '30' });
+    await expect(progressspinner).toHaveAttribute('value', '30');
+  });
+};
+
+const runVisualRegressionTest = async (componentsPage: ComponentsPage, progressspinnerStickerSheet: any) => {
+  progressspinnerStickerSheet.setAttributes({
+    'data-aria-label': 'Progress spinner',
+  });
+
+  // Default progress spinner
+  await progressspinnerStickerSheet.createMarkupWithCombination(
+    {
+      value: [0, 1, 25, 50, 75, 99],
+    },
+    { createNewRow: true },
+  );
+
+  // Success state
+  await progressspinnerStickerSheet.createMarkupWithCombination(
+    {
+      value: [100],
+    },
+    { createNewRow: true },
+  );
+
+  // Error state- only set error if `error` is true
+  await progressspinnerStickerSheet.setAttributes({
+    error: true,
+    'data-aria-label': 'Error progress bar',
+  });
+  await progressspinnerStickerSheet.createMarkupWithCombination(
+    {
+      value: [10],
+    },
+    { createNewRow: true },
+  );
+
+  await progressspinnerStickerSheet.mountStickerSheet({ wrapperStyle: 'padding: 1.25rem' });
+  const container = progressspinnerStickerSheet.getWrapperContainer();
+
+  await test.step('Matches screenshot for progress bar variants', async () => {
+    await componentsPage.visualRegression.takeScreenshot('mdc-progressspinner-variants', { element: container });
+  });
+
+  await test.step('Accessibility check for progress bar variants', async () => {
+    await componentsPage.accessibility.checkForA11yViolations('mdc-progressspinner-variants');
+  });
+};
+
+const testToRun = async (componentsPage: ComponentsPage) => {
+  await test.step('Attributes for Progress Spinner Component', async () => {
+    await attributeTestCases(componentsPage);
+  });
+
+  await test.step('Visual Regression and Accessibility tests', async () => {
+    const progressSpinnerStickerSheet = new StickerSheet(componentsPage, 'mdc-progressspinner');
+    await runVisualRegressionTest(componentsPage, progressSpinnerStickerSheet);
+  });
+};
+
+test.use({ viewport: { width: 1000, height: 2250 } });
+test('Standalone Progress Spinner Tests', async ({ componentsPage }) => {
+  await testToRun(componentsPage);
+});
---
+++ packages/components/src/components/progressspinner/progressspinner.stories.ts
@@ -0,0 +1,86 @@
+import type { Meta, StoryObj, Args } from '@storybook/web-components';
+import '.';
+import { html } from 'lit';
+import { classArgType, styleArgType } from '../../../config/storybook/commonArgTypes';
+import { disableControls, hideControls } from '../../../config/storybook/utils';
+
+const render = (args: Args) => html`
+  <mdc-progressspinner
+   value="${args.value}" 
+   ?error="${args.error}" 
+   data-aria-label="${args['data-aria-label']}">
+  </mdc-progressspinner>`;
+
+const meta: Meta = {
+  title: 'Components/progressspinner',
+  tags: ['autodocs'],
+  component: 'mdc-progressspinner',
+  render,
+  parameters: {
+    badges: ['stable'],
+  },
+  argTypes: {
+    value: {
+      control: 'number',
+    },
+    error: {
+      control: 'boolean',
+    },
+    'data-aria-label': {
+      control: 'text',
+    },
+    ...hideControls(['help-text-type',
+      'required-label',
+      'label', 'disabled',
+      'help-text',
+      'variant',
+      'id',
+      'label-info',
+      '--mdc-progressbar-default-background-color',
+      '--mdc-progressbar-default-active-background-color',
+      '--mdc-progressbar-success-background-color',
+      '--mdc-progressbar-error-background-color',
+      '--mdc-progressbar-height',
+      '--mdc-progressbar-border-radius',
+      '--mdc-progressbar-label-color',
+      '--mdc-progressbar-label-lineheight',
+      '--mdc-progressbar-label-fontsize',
+      '--mdc-progressbar-label-fontweight',
+      '--mdc-progressbar-help-text-color',
+    ]),
+    ...disableControls([
+      '--mdc-spinner-size',
+      '--mdc-track-color',
+      '--mdc-progress-color',
+      '--mdc-progress-success-color',
+      '--mdc-progress-error-color',
+    ]),
+    ...classArgType,
+    ...styleArgType,
+  },
+};
+
+export default meta;
+
+export const Example: StoryObj = {
+  args: {
+    value: 0,
+    'data-aria-label': 'progress spinner',
+  },
+};
+
+export const Error: StoryObj = {
+  args: {
+    value: 25,
+    error: true,
+    'data-aria-label': 'Error progress spinner',
+  },
+};
+
+export const Success: StoryObj = {
+  args: {
+    value: 100,
+    error: false,
+    'data-aria-label': 'Success progress spinner',
+  },
+};
