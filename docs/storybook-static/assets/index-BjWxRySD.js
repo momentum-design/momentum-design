c6b2fbd77f960358ee462d37c7eb01e086c1935b
Momentum Design Automation <automation@momentum.design>
2025-04-11 05:50:09 +0000

    ci(docs): deploy documentation [skip ci] [skip release]

---
+++ docs/storybook-static/assets/index-BjWxRySD.js
@@ -0,0 +1,1 @@
+import{I as r}from"./input.component-Cl7j1CCB.js";import{T as m}from"./input.constants-CqRoqhA-.js";import"./index-wTW8tYJC.js";import"./index-CjvXDYhD.js";import"./index-D3DhXfcW.js";r.register(m);
---
+++ docs/storybook-static/assets/index-wTW8tYJC.js
@@ -0,0 +1,189 @@
+import{i as f,k as m}from"./lit-element-D5KKan5q.js";import{n as c}from"./index-C9z9WAEj.js";import{r as g}from"./state-CSSggq2n.js";import{a as p}from"./buttonsimple.component-Djhz-GfJ.js";import{D as a,B as l,b as d,I as y,P as I,a as k,T as x}from"./button.constants--xhTiFYY.js";import{g as v,a as z}from"./button.utils-CP-c2W9c.js";import"./index-CjvXDYhD.js";const T=f`
+  :host {
+    background-color: transparent;
+    border-radius: 1.25rem;
+    font-weight: var(--mds-font-apps-body-large-medium-font-weight);
+    outline: none;
+
+    --mdc-button-primary-color: var(--mds-color-theme-inverted-text-primary-normal);
+    --mdc-button-primary-background-color: var(--mds-color-theme-button-primary-normal);
+    --mdc-button-primary-hover-background-color: var(--mds-color-theme-button-primary-hover);
+    --mdc-button-primary-pressed-background-color: var(--mds-color-theme-button-primary-pressed);
+    --mdc-button-primary-disabled-background-color: var(--mds-color-theme-button-primary-disabled);
+    --mdc-button-primary-disabled-color: var(--mds-color-theme-text-primary-disabled);
+    
+    --mdc-button-secondary-color: var(--mds-color-theme-text-primary-normal);
+    --mdc-button-secondary-border-color: var(--mds-color-theme-outline-button-normal);
+    --mdc-button-secondary-hover-background-color: var(--mds-color-theme-button-secondary-hover);
+    --mdc-button-secondary-pressed-background-color: var(--mds-color-theme-button-secondary-pressed);
+    --mdc-button-secondary-disabled-background-color: var(--mds-color-theme-button-secondary-disabled);
+    --mdc-button-secondary-disabled-color: var(--mds-color-theme-text-primary-disabled);
+    --mdc-button-secondary-disabled-border-color: var(--mds-color-theme-outline-primary-disabled);
+
+    --mdc-button-tertiary-color: var(--mds-color-theme-text-primary-normal);
+    --mdc-button-tertiary-hover-background-color: var(--mds-color-theme-button-secondary-hover);
+    --mdc-button-tertiary-pressed-background-color: var(--mds-color-theme-button-secondary-pressed);
+    --mdc-button-tertiary-disabled-background-color: var(--mds-color-theme-button-secondary-disabled);
+    --mdc-button-tertiary-disabled-color: var(--mds-color-theme-text-primary-disabled);
+
+    --mdc-button-line-height-size-40: var(--mds-font-lineheight-body-large);
+    --mdc-button-line-height-size-32: var(--mds-font-lineheight-body-large);
+    --mdc-button-line-height-size-28: var(--mds-font-lineheight-body-midsize);
+    --mdc-button-line-height-size-24: var(--mds-font-lineheight-body-small);
+  }
+
+  :host([active]){
+    font-weight: var(--mds-font-apps-body-large-bold-font-weight);
+  }
+
+  :host([variant="primary"]){
+    background: var(--mdc-button-primary-background-color);
+    color: var(--mdc-button-primary-color);
+  }
+  :host([variant="primary"]:hover){
+    background: var(--mdc-button-primary-hover-background-color);
+  }
+  :host([variant="primary"]:active), :host([variant="primary"].pressed){
+    background: var(--mdc-button-primary-pressed-background-color);
+  }
+  :host([variant="primary"][disabled]), :host([variant="primary"][soft-disabled]){
+    background: var(--mdc-button-primary-disabled-background-color);
+    color: var(--mdc-button-primary-disabled-color);
+    cursor: auto;
+  }
+
+  :host([variant="secondary"]){
+    color: var(--mdc-button-secondary-color);
+    border-color: var(--mdc-button-secondary-border-color);
+  }
+  :host([variant="secondary"]:hover){
+    background: var(--mdc-button-secondary-hover-background-color);
+  }
+  :host([variant="secondary"]:active), :host([variant="secondary"].pressed){
+    background: var(--mdc-button-secondary-pressed-background-color);
+  }
+  :host([variant="secondary"][disabled]),
+   :host([variant="secondary"][soft-disabled]){
+    color: var(--mdc-button-primary-disabled-color);
+    border-color: var(--mdc-button-secondary-disabled-border-color);
+    background: var(--mdc-button-secondary-disabled-background-color);
+    cursor: auto;
+  }
+
+  :host([variant="tertiary"]){
+    border-color: transparent;
+    color: var(--mdc-button-tertiary-color);
+  }
+  :host([variant="tertiary"]:hover){
+    background: var(--mdc-button-tertiary-hover-background-color);
+  }
+  :host([variant="tertiary"]:active), :host([variant="tertiary"].pressed){
+    background: var(--mdc-button-tertiary-pressed-background-color);
+  }
+  :host([variant="tertiary"][disabled]), 
+  :host([variant="tertiary"][soft-disabled]){
+    color: var(--mdc-button-tertiary-disabled-color);
+    background: var(--mdc-button-tertiary-disabled-background-color);
+    cursor: auto;
+  }
+
+  :host([size="64"][data-btn-type='icon']), 
+  :host([size="52"][data-btn-type='icon']), 
+  :host([size="40"][data-btn-type='icon']), 
+  :host([size="32"][data-btn-type='icon']),
+  :host([size="28"][data-btn-type='icon']),
+  :host([size="24"][data-btn-type='icon']){
+    border-radius: 6.25rem;
+    aspect-ratio: 1;
+    padding: unset;
+  }
+  :host([size="40"]){
+    font-size: var(--mds-font-size-body-large);
+    line-height: var(--mdc-button-line-height-size-40);
+    padding: 0 1rem;
+    gap: 0.5rem;
+  }
+  :host([size="32"]){
+    font-size: var(--mds-font-size-body-large);
+    line-height: var(--mdc-button-line-height-size-32);
+    padding: 0 0.75rem;
+    gap: 0.375rem;
+  }
+  :host([size="28"]){
+    font-size: var(--mds-font-size-body-midsize);
+    line-height: var(--mdc-button-line-height-size-28);
+    padding: 0 0.75rem;
+    gap: 0.375rem;
+  }
+  :host([size="24"]){
+    font-size: var(--mds-font-size-body-small);
+    line-height: var(--mdc-button-line-height-size-24);
+    padding: 0 0.625rem;
+    gap: 0.25rem;
+  }
+  :host([size="20"]){
+    padding: 0.0625rem;
+    aspect-ratio: 1;
+  }
+
+  :host([color="accent"]){
+    --mdc-button-primary-color: var(--mds-color-theme-common-text-primary-normal);
+    --mdc-button-primary-background-color: var(--mds-color-theme-button-accent-normal);
+    --mdc-button-primary-hover-background-color: var(--mds-color-theme-button-accent-hover);
+    --mdc-button-primary-pressed-background-color: var(--mds-color-theme-button-accent-pressed);
+
+    --mdc-button-secondary-color: var(--mds-color-theme-text-accent-normal);
+    --mdc-button-secondary-border-color: var(--mds-color-theme-outline-theme-normal);
+    --mdc-button-secondary-hover-background-color: var(--mds-color-theme-button-secondary-hover);
+    --mdc-button-secondary-pressed-background-color: var(--mds-color-theme-button-secondary-pressed);
+  }
+  :host([color="positive"]){
+    --mdc-button-primary-color: var(--mds-color-theme-common-text-primary-normal);
+    --mdc-button-primary-background-color: var(--mds-color-theme-button-join-normal);
+    --mdc-button-primary-hover-background-color: var(--mds-color-theme-button-join-hover);
+    --mdc-button-primary-pressed-background-color: var(--mds-color-theme-button-join-pressed);
+
+    --mdc-button-secondary-color: var(--mds-color-theme-text-success-normal);
+    --mdc-button-secondary-border-color: var(--mds-color-theme-outline-join-normal);
+    --mdc-button-secondary-hover-background-color: var(--mds-color-theme-button-secondary-hover);
+    --mdc-button-secondary-pressed-background-color: var(--mds-color-theme-button-secondary-pressed);
+  }
+  :host([color="negative"]){
+    --mdc-button-primary-color: var(--mds-color-theme-common-text-primary-normal);
+    --mdc-button-primary-background-color: var(--mds-color-theme-button-cancel-normal);
+    --mdc-button-primary-hover-background-color: var(--mds-color-theme-button-cancel-hover);
+    --mdc-button-primary-pressed-background-color: var(--mds-color-theme-button-cancel-pressed);
+
+    --mdc-button-secondary-color: var(--mds-color-theme-text-error-normal);
+    --mdc-button-secondary-border-color: var(--mds-color-theme-outline-cancel-normal);
+    --mdc-button-secondary-hover-background-color: var(--mds-color-theme-button-secondary-hover);
+    --mdc-button-secondary-pressed-background-color: var(--mds-color-theme-button-secondary-pressed);
+  }
+  :host([color="promotional"]){
+    --mdc-button-primary-color: var(--mds-color-theme-common-text-primary-normal);
+    --mdc-button-primary-background-color: var(--mds-color-theme-common-button-promotion-normal);
+    --mdc-button-primary-hover-background-color: var(--mds-color-theme-common-button-promotion-hover);
+    --mdc-button-primary-pressed-background-color: var(--mds-color-theme-common-button-promotion-active);
+
+    --mdc-button-secondary-color: var(--mds-color-theme-text-primary-normal);
+    --mdc-button-secondary-border-color: var(--mds-color-theme-outline-promotion-normal);
+    --mdc-button-secondary-hover-background-color: var(--mds-color-theme-button-secondary-hover);
+    --mdc-button-secondary-pressed-background-color: var(--mds-color-theme-button-secondary-pressed);
+  }
+`,S=[T];var N=Object.defineProperty,e=(h,o,n,i)=>{for(var r=void 0,s=h.length-1,u;s>=0;s--)(u=h[s])&&(r=u(o,n,r)||r);return r&&N(o,n,r),r};const b=class b extends p{constructor(){super(...arguments),this.variant=a.VARIANT,this.size=a.SIZE,this.color=a.COLOR,this.role="button",this.typeInternal=a.TYPE_INTERNAL,this.iconSize=1}update(o){super.update(o),o.has("active")&&this.modifyIconName(this.active),o.has("size")&&this.setSize(this.size),o.has("variant")&&(this.setVariant(this.variant),this.setSize(this.size)),o.has("color")&&this.setColor(this.color),o.has("typeInternal")&&this.setSize(this.size),(o.has("prefixIcon")||o.has("postfixIcon"))&&this.inferButtonType()}modifyIconName(o){o?(this.prefixIcon&&(this.prevPrefixIcon=this.prefixIcon,this.prefixIcon=`${v(this.prefixIcon)}-filled`),this.postfixIcon&&(this.prevPostfixIcon=this.postfixIcon,this.postfixIcon=`${v(this.postfixIcon)}-filled`)):(this.prevPrefixIcon&&(this.prefixIcon=this.prevPrefixIcon),this.prevPostfixIcon&&(this.postfixIcon=this.prevPostfixIcon))}setVariant(o){this.setAttribute("variant",Object.values(l).includes(o)?o:a.VARIANT)}setSize(o){const i=this.typeInternal===d.ICON?Object.values(y).includes(o)&&!(o===y[20]&&this.variant!==l.TERTIARY):Object.values(I).includes(o);this.setAttribute("size",i?`${o}`:`${a.SIZE}`),this.iconSize=z(o)}setColor(o){!Object.values(k).includes(o)||this.variant===l.TERTIARY?this.setAttribute("color",`${a.COLOR}`):this.setAttribute("color",o)}inferButtonType(){var n,i;const o=(i=(n=this.shadowRoot)==null?void 0:n.querySelector("slot"))==null?void 0:i.assignedNodes().filter(r=>{var s;return r.nodeType!==Node.TEXT_NODE||((s=r.textContent)==null?void 0:s.trim())}).length;o&&(this.prefixIcon||this.postfixIcon)?(this.typeInternal=d.PILL_WITH_ICON,this.setAttribute("data-btn-type","pill-with-icon")):!o&&(this.prefixIcon||this.postfixIcon)?(this.typeInternal=d.ICON,this.setAttribute("data-btn-type","icon")):(this.typeInternal=d.PILL,this.setAttribute("data-btn-type","pill"))}render(){return m`
+      ${this.prefixIcon?m` <mdc-icon
+            name="${this.prefixIcon}"
+            part="prefix-icon"
+            size=${this.iconSize}
+            length-unit="rem"
+          >
+          </mdc-icon>`:""}
+      <slot @slotchange=${this.inferButtonType}></slot>
+      ${this.postfixIcon?m` <mdc-icon
+            name="${this.postfixIcon}"
+            part="postfix-icon"
+            size=${this.iconSize}
+            length-unit="rem"
+          >
+          </mdc-icon>`:""}
+    `}};b.styles=[...p.styles,...S];let t=b;e([c({type:String,attribute:"prefix-icon",reflect:!0})],t.prototype,"prefixIcon");e([c({type:String,attribute:"postfix-icon",reflect:!0})],t.prototype,"postfixIcon");e([c({type:String})],t.prototype,"variant");e([c({type:Number})],t.prototype,"size");e([c({type:String})],t.prototype,"color");e([c({type:String,reflect:!0})],t.prototype,"role");e([g()],t.prototype,"typeInternal");e([g()],t.prototype,"iconSize");t.register(x);export{t as B};
