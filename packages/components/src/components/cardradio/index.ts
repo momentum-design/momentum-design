8a14ef40511037c61053fc308b164bc677ed7ece
Supriya M <sansup49@gmail.com>
2025-04-16 12:56:08 +0530

    feat: card in radio type

---
+++ packages/components/src/components/cardradio/index.ts
@@ -0,0 +1,15 @@
+import CardRadio from './cardradio.component';
+import { TAG_NAME } from './cardradio.constants';
+import '../icon';
+import '../text';
+import '../staticradio';
+
+CardRadio.register(TAG_NAME);
+
+declare global {
+    interface HTMLElementTagNameMap {
+        ['mdc-cardradio']: CardRadio
+    }
+}
+
+export default CardRadio;
