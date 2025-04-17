a6c9209b8adeda8db7ab9ac36685131a523bbee9
Supriya M <sansup49@gmail.com>
2025-04-17 17:32:29 +0530

    feat: cardbutton using mixin

--- packages/components/src/components/card/card.component.ts
+++ packages/components/src/components/card/card.component.ts
@@ -1,12 +1,10 @@
 import { CSSResult, html, nothing, PropertyValues } from 'lit';
-import { property, queryAssignedElements } from 'lit/decorators.js';
+import { queryAssignedElements } from 'lit/decorators.js';
 import styles from './card.styles';
 import { Component } from '../../models';
-import type { CardOrientation, CardVariant } from './card.types';
 import { VARIANTS, DEFAULTS } from './card.constants';
 import { BUTTON_COLORS } from '../button/button.constants';
-import { IconNameMixin } from '../../utils/mixins/IconNameMixin';
-import type { TagName as TagNameType } from '../text/text.types';
+import { CardComponentMixin } from '../../utils/mixins/CardComponentMixin';
 
 /**
  * The card component allows users to organize information in a structured and tangible
@@ -52,63 +50,7 @@ import type { TagName as TagNameType } from '../text/text.types';
  * @slot after-body - This slot is for passing the content after the body
  *
  */
-class Card extends IconNameMixin(Component) {
-  /**
-   * The title of the card - part of header section
-   * @default ''
-   */
-  @property({ type: String, attribute: 'card-title' })
-  cardTitle: string = '';
-
-  /**
-   * The subtitle of the card - part of header section
-   * @default ''
-   */
-  @property({ type: String })
-  subtitle: string = '';
-
-  /**
-   * The image source URL to render on the card
-   * @default ''
-   */
-  @property({ type: String, attribute: 'image-src' })
-  imageSrc: string = '';
-
-  /**
-   * The image alt for accessibility support
-   * @default ''
-   */
-  @property({ type: String, attribute: 'image-alt' })
-  imageAlt: string = '';
-
-  /**
-   * The variant of the card. It can either be set to 'border' or 'ghost'
-   * @default 'border'
-   */
-  @property({ type: String })
-  variant: CardVariant = DEFAULTS.VARIANT;
-
-  /**
-   * The orientation of the card. It can either be set to 'vertical' or 'horizontal'
-   * @default 'vertical'
-   */
-  @property({ type: String, reflect: true })
-  orientation: CardOrientation = DEFAULTS.ORIENTATION;
-
-  /**
-   * The tag name for the card title. It supports all the types that `msc-text` supports
-   * @default 'span'
-   */
-  @property({ type: String, attribute: 'title-tag-name' })
-  titleTagName: TagNameType = DEFAULTS.TAGNAME;
-
-  /**
-   * The tag name for the subtitle. It supports all the types that `msc-text` supports
-   * @default 'span'
-   */
-  @property({ type: String, attribute: 'subtitle-tag-name' })
-  subtitleTagName: TagNameType = DEFAULTS.TAGNAME;
-
+class Card extends CardComponentMixin(Component) {
    /**
    * The icon buttons in the header section
    * @internal
@@ -222,42 +164,6 @@ class Card extends IconNameMixin(Component) {
      });
    }
  };
-
- /**
-   * Renders the image on the card if image source is provided
-   * @returns The image element
-   */
- protected renderImage() {
-   if (!this.imageSrc) {
-     return nothing;
-   }
-   return html`<img part="image" src="${this.imageSrc}" alt="${this.imageAlt}"/>`;
- }
-
- /**
-   * Renders the icon on the card if icon name is provided
-   * @returns The icon element
-   */
- protected renderIcon() {
-   return this.iconName ? html`<mdc-icon part="icon"
-      size="${DEFAULTS.ICON_SIZE}" 
-      length-unit="${DEFAULTS.ICON_LENGTH_UNIT}" 
-      name="${this.iconName}"></mdc-icon>`
-     : nothing;
- }
-
- /**
-   * Renders the title and subtitle on the card
-   * @returns The title and subtitle elements
-   */
- protected renderTitle() {
-   return html`<div part="title">
-    <mdc-text part="title" type="${DEFAULTS.TITLE_TYPE}" tagname="${this.titleTagName}">${this.cardTitle}</mdc-text>
-    ${this.subtitle ? html`<mdc-text part="subtitle"
-       type="${DEFAULTS.SUBTITLE_TYPE}" 
-       tagname="${this.subtitleTagName}">${this.subtitle}</mdc-text>` : nothing}
-    </div>`;
- }
 
  /**
    * Renders the header of the card if title is provided
