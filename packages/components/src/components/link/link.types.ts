9e4e17843065657dfc37757270f7465cf10347e6
Deeya Upadhyay <58636958+2020Deeya@users.noreply.github.com>
2025-04-21 13:53:43 +0530

    fix: refactor link component to use host approach (#1220)
    * fix: refactor link component to use host approach
    
    * fix: updated e2e tests
    
    * fix: refactor e2e tests and update it
    
    * minor cleanup
    
    * updated snapshots for clarity
    
    * updated tsdocs
    
    * resolved review comments
    
    * fix: resolve minor typo error
    
    * Added event in tsdocs
    
    * Minor cleanup
    
    * Updated link types
    
    * resolve review comments
    
    * remove variable declarations for those with default values

--- packages/components/src/components/link/link.types.ts
+++ packages/components/src/components/link/link.types.ts
@@ -2,5 +2,11 @@ import type { ValueOf } from '../../utils/types';
 import { LINK_SIZES } from './link.constants';
 
 type LinkSize = ValueOf<typeof LINK_SIZES>;
+interface Events {
+    onClickEvent: MouseEvent;
+    onBlurEvent: Event;
+    onKeyDownEvent: KeyboardEvent;
+    onFocusEvent: FocusEvent;
+}
 
-export type { LinkSize };
+export type { LinkSize, Events };
