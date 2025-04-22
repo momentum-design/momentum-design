3950f12f88df19fa46b47bda811db70f23f7095f
Supriya M <sansup49@gmail.com>
2025-04-22 15:21:09 +0530

    fix: updated naming convention

--- packages/components/src/components/cardbutton/index.ts
+++ packages/components/src/components/cardbutton/index.ts
@@ -1,12 +1,12 @@
-import Cardbutton from './cardbutton.component';
+import CardButton from './cardbutton.component';
 import { TAG_NAME } from './cardbutton.constants';
 
-Cardbutton.register(TAG_NAME);
+CardButton.register(TAG_NAME);
 
 declare global {
     interface HTMLElementTagNameMap {
-        ['mdc-cardbutton']: Cardbutton
+        ['mdc-cardbutton']: CardButton
     }
 }
 
-export default Cardbutton;
+export default CardButton;
