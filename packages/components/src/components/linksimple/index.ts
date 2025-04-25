46be0c9f4f1c4c75c205bda1694e6352e0bdb11a
Deeya Upadhyay <58636958+2020Deeya@users.noreply.github.com>
2025-04-25 13:03:17 +0530

    feat: create Linksimple component  (#1234)
    * fix: create linksimple component and extend it inside link component
    
    * added visual regression snapshots
    
    * minor tsdocs cleanup
    
    * minor tsdoc update
    
    * Revert "minor tsdoc update"
    
    This reverts commit 1103c30286f1fa333695332c89a8505d8e63cdfd.
    
    * minor tsdocs update in Linksimple component
    
    * no message
    
    * address review comments
    
    * update e2e tests and snapshots
    
    * release linksimple
    
    ---------
    
    Co-authored-by: Supriya M <30731236+supminn@users.noreply.github.com>

---
+++ packages/components/src/components/linksimple/index.ts
@@ -0,0 +1,12 @@
+import Linksimple from './linksimple.component';
+import { TAG_NAME } from './linksimple.constants';
+
+Linksimple.register(TAG_NAME);
+
+declare global {
+    interface HTMLElementTagNameMap {
+        ['mdc-linksimple']: Linksimple
+    }
+}
+
+export default Linksimple;
