f959818b594d54b8665562d2b81a92d18288d84d
Arjun Kesava P <arjun.kesava@gmail.com>
2025-04-26 10:43:40 +0530

    perf: add e2e specs with screenshots for static toggle
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>

--- packages/components/src/components/statictoggle/statictoggle.e2e-test.ts
+++ packages/components/src/components/statictoggle/statictoggle.e2e-test.ts
@@ -1,18 +1,52 @@
-import { test } from '../../../config/playwright/setup';
+import { expect } from '@playwright/test';
+import { ComponentsPage, test } from '../../../config/playwright/setup';
+import StickerSheet from '../../../config/playwright/setup/utils/Stickersheet';
+import { ToggleSize } from './statictoggle.types';
+import { TOGGLE_SIZE } from './statictoggle.constants';
 
-test.beforeEach(async ({ componentsPage }) => {
+type SetupOptions = {
+  componentsPage: ComponentsPage;
+  checked?: boolean;
+  disabled?: boolean;
+  size?: ToggleSize;
+};
+
+const setup = async (args: SetupOptions) => {
+  const { componentsPage, ...restArgs } = args;
   await componentsPage.mount({
     html: `
-        <mdc-statictoggle />
-      `,
+      <mdc-statictoggle
+        ${restArgs.checked ? 'checked' : ''}
+        ${restArgs.disabled ? 'disabled' : ''}
+        ${restArgs.size ? `size="${restArgs.size}"` : ''}
+      ></mdc-statictoggle>
+    `,
+    clearDocument: true,
   });
-});
+  const statictoggle = componentsPage.page.locator('mdc-statictoggle');
+  await statictoggle.waitFor();
+  return statictoggle;
+};
 
 test('mdc-statictoggle', async ({ componentsPage }) => {
-  const statictoggle = componentsPage.page.locator('mdc-statictoggle');
-
-  // initial check for the statictoggle be visible on the screen:
-  await statictoggle.waitFor();
+  /**
+   * VISUAL REGRESSION
+   */
+  await test.step('visual-regression', async () => {
+    const stickerSheet = new StickerSheet(componentsPage, 'mdc-statictoggle');
+    stickerSheet.setAttributes({ checked: '' });
+    await stickerSheet.createMarkupWithCombination({ size: TOGGLE_SIZE });
+    stickerSheet.setAttributes({});
+    await stickerSheet.createMarkupWithCombination({ size: TOGGLE_SIZE });
+    stickerSheet.setAttributes({ disabled: '' });
+    await stickerSheet.createMarkupWithCombination({ size: TOGGLE_SIZE });
+    await stickerSheet.mountStickerSheet();
+    await test.step('matches screenshot of element', async () => {
+      await componentsPage.visualRegression.takeScreenshot('mdc-statictoggle', {
+        element: stickerSheet.getWrapperContainer(),
+      });
+    });
+  });
 
   /**
    * ACCESSIBILITY
@@ -22,45 +56,22 @@ test('mdc-statictoggle', async ({ componentsPage }) => {
   });
 
   /**
-   * VISUAL REGRESSION
-   */
-  await test.step('visual-regression', async () => {
-    await test.step('matches screenshot of element', async () => {
-      await componentsPage.visualRegression.takeScreenshot('mdc-statictoggle', { element: statictoggle });
-    });
-  });
-
-  /**
    * ATTRIBUTES
    */
   await test.step('attributes', async () => {
-    await test.step('attribute X should be present on component by default', async () => {
-      // TODO: add test here
+    const statictoggle = await setup({ componentsPage });
+    await test.step('attribute size should be present on component by default', async () => {
+      await expect(statictoggle).toHaveAttribute('size', 'default');
     });
-  });
-
-  /**
-   * INTERACTIONS
-   */
-  await test.step('interactions', async () => {
-    await test.step('mouse/pointer', async () => {
-      await test.step('component should fire callback x when clicking on it', async () => {
-        // TODO: add test here
-      });
+    await test.step('attribute checked should be present on component when set', async () => {
+      await componentsPage.setAttributes(statictoggle, { checked: '' });
+      await expect(statictoggle).toHaveAttribute('checked', '');
+      await componentsPage.removeAttribute(statictoggle, 'checked');
     });
-
-    await test.step('focus', async () => {
-      await test.step('component should be focusable with tab', async () => {
-        // TODO: add test here
-      });
-
-      // add additional tests here, like tabbing through several parts of the component
-    });
-
-    await test.step('keyboard', async () => {
-      await test.step('component should fire callback x when pressing y', async () => {
-        // TODO: add test here
-      });
+    await test.step('attribute disabled should be present on component when set', async () => {
+      await componentsPage.setAttributes(statictoggle, { disabled: '' });
+      await expect(statictoggle).toHaveAttribute('disabled', '');
+      await componentsPage.removeAttribute(statictoggle, 'disabled');
     });
   });
 });
