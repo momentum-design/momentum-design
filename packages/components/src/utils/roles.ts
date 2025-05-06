20b804743848d161273ded0225e290dbaa326934
Arjun Kesava P <arjun.kesava@gmail.com>
2025-05-06 13:49:48 +0530

    perf: add roles utils file
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>

---
+++ packages/components/src/utils/roles.ts
@@ -0,0 +1,19 @@
+import type { ValueOf } from './types';
+
+// When adding a new role in this list, please sort this list.
+const ROLE = {
+  BUTTON: 'button',
+  CHECKBOX: 'checkbox',
+  GROUP: 'group',
+  LIST: 'list',
+  LISTITEM: 'listitem',
+  MENU: 'menu',
+  MENUBAR: 'menubar',
+  MENUITEM: 'menuitem',
+  MENUITEMCHECKBOX: 'menuitemcheckbox',
+  MENUITEMRADIO: 'menuitemradio',
+} as const;
+
+type RoleType = ValueOf<typeof ROLE>;
+
+export { ROLE, RoleType };
