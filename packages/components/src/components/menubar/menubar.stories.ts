8457122b1c8be9fddecd8c6f10fde6a73582b0b4
Arjun Kesava P <arjun.kesava@gmail.com>
2025-05-08 12:13:15 +0530

    perf: add new mixin for menu which acts as utils for navigation
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>

--- packages/components/src/components/menubar/menubar.stories.ts
+++ packages/components/src/components/menubar/menubar.stories.ts
@@ -8,7 +8,11 @@ import '../menupopover';
 import { ORIENTATION } from './menubar.constants';
 
 const render = (args: Args) => html`
-  <mdc-menubar aria-orientation="${args['aria-orientation']}" style="width: 50rem;">
+  <mdc-menubar
+    aria-orientation="${args['aria-orientation']}"
+    style="width: 50rem; background: linear-gradient(to right, red, orange);"
+  >
+    <mdc-menuitem label="Home"></mdc-menuitem>
     <mdc-menuitem id="file-id" label="File"></mdc-menuitem>
     <mdc-menupopover triggerid="file-id">
       <mdc-menuitem label="New File"></mdc-menuitem>
@@ -16,9 +20,36 @@ const render = (args: Args) => html`
       <mdc-divider></mdc-divider>
       <mdc-menuitem label="Open"></mdc-menuitem>
       <mdc-menuitem label="Open Folder"></mdc-menuitem>
+      <mdc-menuitem id="open-recent" label="Open Recent">
+        <mdc-icon slot="trailing-controls" name="arrow-right-bold"></mdc-icon>
+      </mdc-menuitem>
+      <mdc-menupopover triggerID="open-recent">
+        <mdc-menuitem label="~/Documents/menu.ts"></mdc-menuitem>
+        <mdc-menuitem label="~/Documents/popover.ts"></mdc-menuitem>
+        <mdc-menuitem id="button-file" label="~/Documents/button">
+          <mdc-icon slot="trailing-controls" name="arrow-right-bold"></mdc-icon>
+        </mdc-menuitem>
+        <mdc-menupopover triggerid="button-file">
+          <mdc-menuitem label="~/Documents/button/button.ts"></mdc-menuitem>
+          <mdc-menuitem label="~/Documents/button/button.html"></mdc-menuitem>
+          <mdc-menuitem label="~/Documents/button/button.css"></mdc-menuitem>
+        </mdc-menupopover>
+        <mdc-menuitem label="~/Documents/popover.css"></mdc-menuitem>
+      </mdc-menupopover>
       <mdc-divider></mdc-divider>
       <mdc-menuitem label="Save"></mdc-menuitem>
       <mdc-menuitem label="Save As..."></mdc-menuitem>
+      <mdc-divider></mdc-divider>
+      <mdc-menuitem id="share-id" label="Share">
+        <mdc-icon slot="trailing-controls" name="arrow-right-bold"></mdc-icon>
+      </mdc-menuitem>
+      <mdc-menupopover triggerID="share-id">
+        <mdc-menuitem label="AirDrop"></mdc-menuitem>
+        <mdc-menuitem label="Messages"></mdc-menuitem>
+        <mdc-menuitem label="Notes"></mdc-menuitem>
+        <mdc-menuitem label="Freeform"></mdc-menuitem>
+        <mdc-menuitem label="Reminders"></mdc-menuitem>
+      </mdc-menupopover>
     </mdc-menupopover>
     <mdc-menuitem id="edit-id" label="Edit"></mdc-menuitem>
     <mdc-menupopover triggerid="edit-id">
@@ -47,7 +78,11 @@ const render = (args: Args) => html`
     </mdc-menupopover>
     <mdc-menuitem label="Window"></mdc-menuitem>
     <mdc-menuitem label="Help"></mdc-menuitem>
-  </mdc-menubar>`;
+  </mdc-menubar>
+  <div style="height: 250px; width: 100%; background: linear-gradient(to right, red, orange);">
+    <h1>This is the home page </h1>
+  </div>
+  `;
 
 const meta: Meta = {
   title: 'Work In Progress/menu/menubar',
@@ -76,6 +111,73 @@ export const Example: StoryObj = {
 };
 
 export const VerticalMenuBar: StoryObj = {
+  render: (args: Args) => html`
+    <mdc-menubar
+      aria-orientation="${args['aria-orientation']}"
+      style="width: 10rem; background: linear-gradient(to right, red, orange);"
+    >
+      <mdc-menuitem label="Home"></mdc-menuitem>
+      <mdc-menuitem id="file-id" label="File"></mdc-menuitem>
+      <mdc-menupopover triggerid="file-id">
+        <mdc-menuitem label="New File"></mdc-menuitem>
+        <mdc-menuitem label="New Window"></mdc-menuitem>
+        <mdc-divider></mdc-divider>
+        <mdc-menuitem label="Open"></mdc-menuitem>
+        <mdc-menuitem label="Open Folder"></mdc-menuitem>
+        <mdc-menuitem id="open-recent" label="Open Recent">
+          <mdc-icon slot="trailing-controls" name="arrow-right-bold"></mdc-icon>
+        </mdc-menuitem>
+        <mdc-menupopover triggerID="open-recent">
+          <mdc-menuitem label="~/Documents/menu.ts"></mdc-menuitem>
+          <mdc-menuitem label="~/Documents/popover.ts"></mdc-menuitem>
+          <mdc-menuitem id="button-file" label="~/Documents/button">
+            <mdc-icon slot="trailing-controls" name="arrow-right-bold"></mdc-icon>
+          </mdc-menuitem>
+          <mdc-menupopover triggerid="button-file">
+            <mdc-menuitem label="~/Documents/button/button.ts"></mdc-menuitem>
+            <mdc-menuitem label="~/Documents/button/button.html"></mdc-menuitem>
+           <mdc-menuitem label="~/Documents/button/button.css"></mdc-menuitem>
+          </mdc-menupopover>
+          <mdc-menuitem label="~/Documents/popover.css"></mdc-menuitem>
+        </mdc-menupopover>
+        <mdc-divider></mdc-divider>
+        <mdc-menuitem label="Save"></mdc-menuitem>
+        <mdc-menuitem label="Save As..."></mdc-menuitem>
+        <mdc-divider></mdc-divider>
+        <mdc-menuitem id="share-id" label="Share">
+          <mdc-icon slot="trailing-controls" name="arrow-right-bold"></mdc-icon>
+      </mdc-menuitem>
+      <mdc-menupopover triggerID="share-id">
+        <mdc-menuitem label="AirDrop"></mdc-menuitem>
+        <mdc-menuitem label="Messages"></mdc-menuitem>
+        <mdc-menuitem label="Notes"></mdc-menuitem>
+        <mdc-menuitem label="Freeform"></mdc-menuitem>
+        <mdc-menuitem label="Reminders"></mdc-menuitem>
+      </mdc-menupopover>
+    </mdc-menupopover>
+    <mdc-menuitem id="edit-id" label="Edit"></mdc-menuitem>
+    <mdc-menupopover triggerid="edit-id">
+      <mdc-menuitem label="Undo"></mdc-menuitem>
+      <mdc-menuitem label="Redo"></mdc-menuitem>
+      <mdc-divider></mdc-divider>
+      <mdc-menuitem label="Cut"></mdc-menuitem>
+      <mdc-menuitem label="Copy"></mdc-menuitem>
+      <mdc-menuitem label="Paste"></mdc-menuitem>
+    </mdc-menupopover>
+    <mdc-menuitem id="view-id" label="View"></mdc-menuitem>
+    <mdc-menupopover triggerid="view-id">
+      <mdc-menuitem label="Zoom In"></mdc-menuitem>
+      <mdc-menuitem label="Zoom Out"></mdc-menuitem>
+    </mdc-menupopover>
+    <mdc-menuitem label="Tools"></mdc-menuitem>
+    <mdc-menuitem label="Terminal"></mdc-menuitem>
+    <mdc-menuitem label="Window"></mdc-menuitem>
+    <mdc-menuitem label="Help"></mdc-menuitem>
+  </mdc-menubar>
+  <div style="height: 250px; width: 100%; background: linear-gradient(to right, red, orange);">
+    <h1>This is the home page </h1>
+  </div>
+  `,
   args: {
     'aria-orientation': ORIENTATION.VERTICAL,
   },
--- packages/components/src/components/menupopover/menupopover.component.ts
+++ packages/components/src/components/menupopover/menupopover.component.ts
@@ -1,6 +1,9 @@
-import type { CSSResult } from 'lit';
+import { CSSResult, PropertyValues } from 'lit';
+import { MenuMixin } from '../../utils/mixins/MenuMixin';
 import { ROLE } from '../../utils/roles';
 import Popover from '../popover/popover.component';
+import { POPOVER_PLACEMENT, TRIGGER } from '../popover/popover.constants';
+import { TAG_NAME as MENU_POPOVER } from './menupopover.constants';
 
 /**
  * menupopover component, which ...
@@ -9,17 +12,26 @@ import Popover from '../popover/popover.component';
  *
  * @slot default - This is a default/unnamed slot
  */
-class MenuPopover extends Popover {
+class MenuPopover extends MenuMixin(Popover) {
   override connectedCallback() {
     super.connectedCallback();
     this.role = ROLE.MENU;
-    this.trigger = 'click';
-    this.placement = 'bottom-start';
-    this.hideOnOutsideClick = true;
-    this.hideOnBlur = true;
+
+    this.focusTrap = true;
     this.hideOnEscape = true;
+    this.hideOnOutsideClick = true;
+    this.placement = POPOVER_PLACEMENT.BOTTOM_START;
     this.showArrow = false;
-    this.interactive = false;
+  }
+
+  override async firstUpdated(changedProperties: PropertyValues) {
+    await super.firstUpdated(changedProperties);
+
+    if (this.parentElement?.tagName?.toLowerCase() === MENU_POPOVER) {
+      this.interactive = true;
+      this.placement = POPOVER_PLACEMENT.RIGHT_START;
+      this.trigger = TRIGGER.MOUSEENTER;
+    }
   }
 
   public static override styles: Array<CSSResult> = [...Popover.styles];
