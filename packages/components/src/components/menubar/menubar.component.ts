9a3aed5543bf691d6a52675c849b8b3be7f241ed
Arjun Kesava P <arjun.kesava@gmail.com>
2025-05-12 21:48:41 +0530

    chore: add enter and esc key events for menubar
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>

--- packages/components/src/components/menubar/menubar.component.ts
+++ packages/components/src/components/menubar/menubar.component.ts
@@ -1,4 +1,5 @@
-import { CSSResult, html } from 'lit';
+import { html } from 'lit';
+import type { CSSResult, PropertyValues } from 'lit';
 import { Component } from '../../models';
 import { MenuMixin } from '../../utils/mixins/MenuMixin';
 import { ROLE } from '../../utils/roles';
@@ -20,30 +21,17 @@ class MenuBar extends MenuMixin(Component) {
     this.addEventListener('click', this.handleMouseClick);
   }
 
-  /**
-   * Handles the mouse click event on the menubar slot which targets one of the menu items.
-   * Finds the index of the target element in the list items array and calls
-   * `resetTabIndexAndSetActiveTabIndex` with that index.
-   * @param event - The mouse event.
-   */
-  private handleMouseClick(event: MouseEvent): void {
-    this.setTabIndexOnMouseClick(event);
-    // const activeMenuItem = event.target as HTMLElement;
-    // const hasCurrentMenuItemHasChildren = activeMenuItem?.hasAttribute('aria-expanded');
-    // if (hasCurrentMenuItemHasChildren) {}
-    // const isMenuPopoverOpen = this.menuPopoverItems
-    //   .some((node) => node.getAttribute('triggerid') === activeMenuItem.id);
-    // console.log(isMenuPopoverOpen);
-    // if (isMenuPopoverOpen) {
-    //   this.menuPopoverItems.forEach((node) => {
-    //     node.setAttribute('trigger', 'mouseenter');
-    //   });
-    // }
-  }
-
   public override firstUpdated(): void {
     // For the first, we set the first element only as active.
     this.resetTabIndexAndSetActiveTabIndex(0);
+  }
+
+  public override update(changedProperties: PropertyValues): void {
+    super.update(changedProperties);
+
+    if (changedProperties.has('ariaOrientation')) {
+      this.updatePopoverPlacementBasedOnOrientation();
+    }
   }
 
   public override render() {
