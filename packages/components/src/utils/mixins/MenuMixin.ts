0b6b8d048f50dc693e7fc4a718a883eebbddaa71
Arjun Kesava P <arjun.kesava@gmail.com>
2025-05-12 22:15:31 +0530

    docs: add tsdoc for menu mixin
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>

--- packages/components/src/utils/mixins/MenuMixin.ts
+++ packages/components/src/utils/mixins/MenuMixin.ts
@@ -17,7 +17,6 @@ interface IParentMenuItem {
 export interface MenuMixinInterface {
   ariaOrientation: Orientation;
   handleKeyDown(event: KeyboardEvent): void;
-  handleMouseClick(event: MouseEvent): void;
   resetTabIndexAndSetActiveTabIndex(newIndex: number): void;
   setTabIndexOnMouseClick(event: MouseEvent): void;
   updatePopoverPlacementBasedOnOrientation(): void;
@@ -53,6 +52,9 @@ export const MenuMixin = <T extends Constructor<Component>>(superClass: T) => {
       );
     }
 
+    /**
+     * Updates the placement of the popover based on the aria-orientation property.
+     */
     public updatePopoverPlacementBasedOnOrientation(): void {
       if (this.ariaOrientation === ORIENTATION.HORIZONTAL) {
         this.menuPopoverItems.forEach((node) => {
@@ -65,32 +67,24 @@ export const MenuMixin = <T extends Constructor<Component>>(superClass: T) => {
       }
     }
 
-    public setTabIndexOnMouseClick(event: MouseEvent): void {
-      const newIndex = this.getCurrentIndex(event.target);
-      this.resetTabIndexAndSetActiveTabIndex(newIndex);
-      this.menuItems[newIndex]?.focus();
-    }
-
     /**
-     * Resets all list items tabindex to -1 and sets the tabindex of the
-     * element at the given index to 0, effectively setting the active
-     * element. This is used when navigating the list via keyboard.
-     *
+     * Resets the tabindex of the current index to -1 and sets the tabindex of the new index to 0,
+     * effectively setting the active element. This is used when navigating the menu via keyboard.
+     * @param menuItems - The list of menu items.
+     * @param currentIndex - The current index of the focused menu item.
      * @param newIndex - The index of the new active element in the list.
      */
-    public resetTabIndexAndSetActiveTabIndex(newIndex: number) {
-      this.menuItems.forEach((node, index) => {
-        const newTabindex = newIndex === index ? '0' : '-1';
-        node?.setAttribute('tabindex', newTabindex);
-      });
-    }
-
     private updateTabIndexAndFocusNewIndex(menuItems: Array<HTMLElement>, currentIndex: number, newIndex: number) {
       menuItems[currentIndex].setAttribute('tabindex', '-1');
       menuItems[newIndex].setAttribute('tabindex', '0');
       menuItems[newIndex]?.focus();
     }
 
+    /**
+     * Opens the popover at the given index if it exists.
+     * @param index - The index of the menu item to open the popover for.
+     * @returns True if the popover was opened, false if not.
+     */
     private openPopover(index: number): boolean {
       if (this.menuItems[index].nextElementSibling?.tagName?.toLowerCase() === MENUPOPOVER_TAGNAME) {
         const currentMenuId = this.menuItems[index].getAttribute('id');
@@ -103,16 +97,37 @@ export const MenuMixin = <T extends Constructor<Component>>(superClass: T) => {
       return false;
     }
 
+    /**
+     * Navigates to the previous menu item and updates the tabindex of the current index
+     * to -1 and the tabindex of the new index to 0.
+     * @param currentIndex - The current index of the focused menu item.
+     * @param firstMenuIndex - The index of the first menu item.
+     * @param lastMenuIndex - The index of the last menu item.
+     */
     private navigateToPrevMenuItem(currentIndex: number, firstMenuIndex: number, lastMenuIndex: number) {
       const newIndex = currentIndex === firstMenuIndex ? lastMenuIndex : currentIndex - 1;
       this.updateTabIndexAndFocusNewIndex(this.menuItems, currentIndex, newIndex);
     }
 
+    /**
+     * Navigates to the next menu item and updates the tabindex of the current index
+     * to -1 and the tabindex of the new index to 0.
+     * @param currentIndex - The current index of the focused menu item.
+     * @param firstMenuIndex - The index of the first menu item.
+     * @param lastMenuIndex - The index of the last menu item.
+     */
     private navigateToNextMenuItem(currentIndex: number, firstMenuIndex: number, lastMenuIndex: number): void {
       const newIndex = currentIndex === lastMenuIndex ? firstMenuIndex : currentIndex + 1;
       this.updateTabIndexAndFocusNewIndex(this.menuItems, currentIndex, newIndex);
     }
 
+    /**
+     * Returns the parent menu item details for the given menu child id.
+     * It recursively traverses up the DOM tree until it finds a menu or menubar element.
+     * @param menuChildId - The id of the menu child element.
+     * @param menu - The current menu element to start traversing from.
+     * @returns An object containing the parent menu element and the menu child id.
+     */
     private getParentMenuItemDetails(menuChildId: string, menu?: HTMLElement | null): IParentMenuItem {
       if (
         menu?.tagName?.toLowerCase() === MENU_TAGNAME
@@ -123,6 +138,10 @@ export const MenuMixin = <T extends Constructor<Component>>(superClass: T) => {
       return this.getParentMenuItemDetails(menu?.previousElementSibling?.getAttribute('id') ?? '', menu?.parentElement);
     }
 
+    /**
+     * Recursively hides all the popovers up the DOM tree until it finds a menu or menubar element.
+     * @param menu - The current menu element to start traversing from.
+     */
     private hideAllPopovers(menu?: HTMLElement | null): void {
       if (
         menu?.tagName?.toLowerCase() === MENU_TAGNAME
@@ -136,6 +155,12 @@ export const MenuMixin = <T extends Constructor<Component>>(superClass: T) => {
       this.hideAllPopovers(menu?.parentElement);
     }
 
+    /**
+     * Recursively navigates up the DOM tree until it finds the previous menu item
+     * and updates the tabindex of the current index to -1 and the tabindex of the new index to 0.
+     * Also opens the popover of the previous menu item.
+     * @param currentIndex - The current index of the focused menu item.
+     */
     private navigateToPrevParentMenuItem(currentIndex: number): void {
       const parentMenuItem = this.menuItems[currentIndex].parentElement?.previousElementSibling;
       const parentMenuItemsChildren = Array.from(this.parentElement?.children || []).filter(
@@ -150,6 +175,10 @@ export const MenuMixin = <T extends Constructor<Component>>(superClass: T) => {
       parentMenuItemsChildren[parentMenuItemIndex - 1].nextElementSibling?.toggleAttribute('visible');
     }
 
+    /**
+     * Closes the current menu popover and navigates to the previous parent menu item.
+     * @param currentIndex - The current index of the focused menu item.
+     */
     private closePopoverAndNavigateToPrevParentMenuItem(currentIndex: number): void {
       // - close popover first
       this.toggleAttribute('visible');
@@ -157,6 +186,12 @@ export const MenuMixin = <T extends Constructor<Component>>(superClass: T) => {
       this.navigateToPrevParentMenuItem(currentIndex);
     }
 
+    /**
+     * Opens the popover of the next children menu item if there are children.
+     * If there are no children, then it closes all popovers recursively and
+     * navigates to the next menu item from the menu bar.
+     * @param currentIndex - The current index of the focused menu item.
+     */
     private openPopoverAndNavigateToNextChildrenMenuItem(currentIndex: number): void {
       const isMenuPopoverOpen = this.openPopover(currentIndex);
       if (isMenuPopoverOpen) {
@@ -182,6 +217,58 @@ export const MenuMixin = <T extends Constructor<Component>>(superClass: T) => {
       parentMenuItemsChildren[parentMenuItemIndex + 1].nextElementSibling?.toggleAttribute('visible');
     }
 
+    /**
+     * Handles the mouse click event on the menu items.
+     * Resets all the menu items tabindex to -1 and sets the tabindex of the
+     * element at the given index to 0, effectively setting the active
+     * element. This is used when clicking on the menu items.
+     * @param event - The mouse click event.
+     */
+    public setTabIndexOnMouseClick(event: MouseEvent): void {
+      const newIndex = this.getCurrentIndex(event.target);
+      this.resetTabIndexAndSetActiveTabIndex(newIndex);
+      this.menuItems[newIndex]?.focus();
+    }
+
+    /**
+     * Resets all list items tabindex to -1 and sets the tabindex of the
+     * element at the given index to 0, effectively setting the active
+     * element. This is used when navigating the list via keyboard.
+     *
+     * @param newIndex - The index of the new active element in the list.
+     */
+    public resetTabIndexAndSetActiveTabIndex(newIndex: number) {
+      this.menuItems.forEach((node, index) => {
+        const newTabindex = newIndex === index ? '0' : '-1';
+        node?.setAttribute('tabindex', newTabindex);
+      });
+    }
+
+    /**
+     * Handles the keydown event on the menu bar.
+     * The keys are as follows:
+     * - HOME: Sets focus to the first menu item.
+     * - END: Sets focus to the last menu item.
+     * - ARROW_LEFT:
+     *   - If orientation is horizontal, then it navigates to the previous menu item.
+     *   - If orientation is vertical, then it closes the popover of the current menu item and
+     *     navigates to the previous parent menu item.
+     * - ARROW_RIGHT:
+     *   - If orientation is horizontal, then it navigates to the next menu item.
+     *   - If orientation is vertical, then it opens the popover of the current menu item and
+     *     navigates to the next children menu item.
+     * - ARROW_UP: Navigates to the previous menu item.
+     * - ARROW_DOWN:
+     *   - If orientation is horizontal, then it opens the popover of the current menu item.
+     *   - If orientation is vertical, then it navigates to the next menu item.
+     * - ENTER:
+     *   - If the next element sibling is a menu popover, then it opens the popover of the current
+     *     menu item and navigates to the next children menu item.
+     *   - If the target element is a menu item, then it closes all popovers recursively and
+     *     navigates to the previous parent menu item.
+     * - ESCAPE: Closes all popovers recursively and navigates to the previous parent menu item.
+     * @param event - The keyboard event.
+     */
     public handleKeyDown(event: KeyboardEvent): void {
       const firstMenuIndex = 0;
       const lastMenuIndex = this.menuItems.length - 1;
@@ -244,10 +331,6 @@ export const MenuMixin = <T extends Constructor<Component>>(superClass: T) => {
         default:
           break;
       }
-    }
-
-    public handleMouseClick(event: MouseEvent): void {
-      console.log(event);
     }
   }
   // Cast return type to your mixin's interface intersected with the superClass type
