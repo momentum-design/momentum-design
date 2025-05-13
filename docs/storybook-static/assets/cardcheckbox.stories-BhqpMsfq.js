b7869c6b7aa4eff875c6aafd79405c9f57179a9f
Momentum Design Automation <automation@momentum.design>
2025-05-13 11:29:13 +0000

    ci(docs): deploy documentation [skip ci] [skip release]

---
+++ docs/storybook-static/assets/cardcheckbox.stories-BhqpMsfq.js
@@ -0,0 +1,264 @@
+import{i as Z,D as x,k as o}from"./lit-element-D5KKan5q.js";import{u as W,n as z}from"./index-C9z9WAEj.js";import{a as q}from"./index-CcXofQbA.js";import{D as Q}from"./DisabledMixin-BZruwOeC.js";import{T as J}from"./TabIndexMixin-CsrHswKP.js";import{C as f}from"./card.component-U5cYJvzG.js";import"./index-BQmnd5ax.js";import"./index-D3DhXfcW.js";import"./index-Sq7Ki8AB.js";import{Q as ee}from"./repeat-CNQ7t7Vw.js";import{a as c}from"./chunk-D5ZWXAHU-CGElDDNX.js";import{c as te,s as ae}from"./commonArgTypes-BluK8w5L.js";import{h as oe,d as re}from"./utils-CO8B6ZcN.js";import{O as T,D as t}from"./CardAndDialogFooterMixin-COm20Ox1.js";import{V as k}from"./text.constants-ZF0jM5wn.js";import"./index-B_EqOnoU.js";import"./query-assigned-elements-uEuc3rg8.js";import"./base-DIkkzJ-c.js";import"./button.constants-D0QAbDxL.js";import"./buttonsimple.constants-CGc1uJX6.js";import"./iframe-BFvqgBhr.js";import"../sb-preview/runtime.js";import"./state-CSSggq2n.js";import"./index-2PiEGcHV.js";import"./iconprovider.component-Bvm-70EG.js";import"./create-context-89xeped_.js";import"./directive-Ctav8iJK.js";import"./directive-helpers-Bb6vhctq.js";import"./v4-CQkTLCs1.js";import"./link.constants-CqyNsepP.js";import"./if-defined-D5BV9-c0.js";import"./DataAriaLabelMixin-D53Z5CZ7.js";import"./roles-DCpIf2k8.js";import"./formfieldwrapper.component-5n2-mkV5.js";const ie=Z`
+  :host {
+    cursor: pointer;
+    user-select: none;
+  }
+
+  :host::part(check){
+    margin-left: auto;
+  }
+
+  :host(:hover) {
+    background-color: var(--mds-color-theme-background-primary-hover);
+  }
+
+  :host(:active) {
+    background-color: var(--mds-color-theme-background-primary-active);
+  }
+
+  :host([checked]){
+    border: 1px solid var(--mds-color-theme-outline-input-active);
+    box-shadow: 0 0 0 1px var(--mds-color-theme-outline-input-active);
+    background-color: var(--mds-color-theme-background-primary-active);
+  }
+
+  :host([checked]:hover){
+    background-color: var(--mds-color-theme-background-primary-hover);
+  }
+
+  :host([checked]:active){
+    background-color: var(--mds-color-theme-background-primary-ghost);
+  }
+
+  :host::part(check-icon){
+    color: var(--mds-color-theme-text-secondary-normal);
+  }
+
+  :host([checked])::part(check-icon){
+    color: var(--mds-color-theme-text-accent-normal);
+  }
+
+  :host([disabled]) {
+    background-color: var(--mds-color-theme-background-primary-ghost);
+    color: var(--mds-color-theme-text-primary-disabled);
+    border-color: var(--mds-color-theme-outline-primary-disabled);
+  }
+
+  :host([disabled])::part(image){
+    opacity: 0.5;
+  }
+  
+  :host([disabled])::part(check-icon){
+    color: var(--mds-color-theme-text-primary-disabled);
+  }
+`,le=[ie,...q()],ce=W.constructTagName("cardcheckbox"),y={DEFAULT:"check-circle-bold",CHECKED:"check-circle-filled"},r={CHECK:"check",CHECKBOX:"checkbox"},A={SELECTION_TYPE:r.CHECK,ICON_SIZE:1.5,ICON_LENGTH_UNIT:"rem"};var ne=Object.defineProperty,j=(e,a,I,de)=>{for(var i=void 0,g=e.length-1,C;g>=0;g--)(C=e[g])&&(i=C(a,I,i)||i);return i&&ne(a,I,i),i};const E=class E extends Q(J(f)){constructor(){super(),this.checked=!1,this.selectionType=A.SELECTION_TYPE,this.addEventListener("click",this.toggleChecked),this.addEventListener("keydown",this.toggleOnEnter),this.addEventListener("keyup",this.toggleOnSpace)}connectedCallback(){super.connectedCallback(),this.role="checkbox"}update(a){super.update(a),a.has("checked")&&this.setAttribute("aria-checked",`${this.checked}`),a.has("disabled")&&this.setAttribute("aria-disabled",`${this.disabled}`)}toggleChecked(){this.disabled||(this.checked=!this.checked)}toggleOnEnter(a){a.key==="Enter"&&this.toggleChecked()}toggleOnSpace(a){a.key===" "&&this.toggleChecked()}renderSelection(){const a=this.checked?y.CHECKED:y.DEFAULT;switch(this.selectionType){case r.CHECK:return o`<mdc-icon part="check check-icon" 
+                   size="${A.ICON_SIZE}"
+                   length-unit="${A.ICON_LENGTH_UNIT}" 
+                   name="${a}"></mdc-icon>`;case r.CHECKBOX:return o`<mdc-staticcheckbox part="check" 
+                   ?checked="${this.checked}" 
+                   ?disabled="${this.disabled}"></mdc-staticcheckbox>`;default:return x}}renderHeader(){return this.cardTitle?o`<div part="header">
+     ${this.renderIcon()}
+     ${this.renderTitle()}
+     ${this.renderSelection()}
+   </div>`:x}render(){return o`
+  ${this.renderImage()}
+    <div part="body">
+    ${this.renderHeader()}
+      <slot name="before-body"></slot>
+      <slot name="body"></slot>
+      <slot name="after-body"></slot>
+    </div>
+  `}};E.styles=[...f.styles,...le];let n=E;j([z({type:Boolean,reflect:!0})],n.prototype,"checked");j([z({type:String,attribute:"selection-type"})],n.prototype,"selectionType");n.register(ce);const se=e=>o`
+  <mdc-cardcheckbox
+    @click="${c("onclick")}"
+    @keydown="${c("onkeydown")}"
+    @keyup="${c("onkeyup")}"
+    @focus="${c("onfocus")}"
+    @change="${c("onchange")}"
+    variant="${e.variant}"
+    orientation="${e.orientation}"
+    card-title="${e["card-title"]}"
+    subtitle="${e.subtitle}"
+    image-src="${e["image-src"]}"
+    image-alt="${e["image-alt"]}"
+    icon-name="${e["icon-name"]}"
+    ?checked="${e.checked}"
+    ?disabled="${e.disabled}"
+    aria-label="${e["aria-label"]}"
+    selection-type="${e["selection-type"]}"
+    title-tag-name="${e["title-tag-name"]}"
+    subtitle-tag-name="${e["subtitle-tag-name"]}"
+    tabindex="${e.tabIndex}"
+    class="${e.class}"
+    style="${e.style}">${e.children}</mdc-cardcheckbox>`,Xe={title:"Work In Progress/card/cardcheckbox",tags:["autodocs"],component:"mdc-cardcheckbox",render:se,parameters:{badges:["wip"]},argTypes:{variant:{control:"select",options:["border","ghost"]},orientation:{control:"select",options:Object.values(T)},"card-title":{control:"text"},subtitle:{control:"text"},"image-src":{control:"text"},"image-alt":{control:"text"},"icon-name":{control:"text"},checked:{control:"boolean"},disabled:{control:"boolean"},tabIndex:{control:"number"},"aria-label":{control:"text"},"selection-type":{control:"select",options:Object.values(r)},"title-tag-name":{control:"select",options:Object.values(k)},"subtitle-tag-name":{control:"select",options:Object.values(k)},...oe(["children"]),...te,...ae}},l=o`<mdc-text slot='body' 
+type="body-midsize-medium" tagname="span">Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+Nam vulputate aliquet risus, eget auctor ante egestas facilisis. Curabitur malesuada tempor pulvinar. 
+Quisque sollicitudin magna leo, gravida ultrices lacus lobortis at. 
+Praesent gravida dui diam, non elementum risus laoreet vitae. 
+Sed sed nunc ullamcorper, porttitor dui id, posuere justo. Curabitur laoreet sem ut pharetra hendrerit. 
+Vivamus mattis ligula eget imperdiet tempor. 
+Ut in massa luctus lacus sodales accumsan. Praesent at aliquam leo. Ut a scelerisque turpis.</mdc-text>`,s={args:{"card-title":"Title",subtitle:"Subtitle","image-src":"https://placehold.co/320x200","image-alt":"Image Alt","icon-name":"placeholder-bold","title-tag-name":t.TAGNAME,"subtitle-tag-name":t.TAGNAME,variant:t.VARIANT,orientation:t.ORIENTATION,checked:!1,disabled:!1,tabIndex:0,"selection-type":r.CHECK,children:l}},d={args:{"card-title":"Title",subtitle:"Subtitle","image-src":"https://placehold.co/160x280","image-alt":"Image Alt","icon-name":"placeholder-bold","aria-label":"Aria Label","title-tag-name":t.TAGNAME,"subtitle-tag-name":t.TAGNAME,"selection-type":r.CHECKBOX,checked:!1,disabled:!1,tabIndex:0,variant:t.VARIANT,orientation:T.HORIZONTAL,children:l}},m={args:{"card-title":"Title",subtitle:"Subtitle","icon-name":"placeholder-bold","title-tag-name":t.TAGNAME,"subtitle-tag-name":t.TAGNAME,"aria-label":"Aria Label","selection-type":r.CHECK,checked:!1,disabled:!1,tabIndex:0,variant:t.VARIANT,orientation:t.ORIENTATION,children:l}},p={args:{"image-src":"https://placehold.co/320x200","image-alt":"Image Alt","card-title":"Title",subtitle:"Subtitle","icon-name":"placeholder-bold","selection-type":r.CHECK,"title-tag-name":t.TAGNAME,"subtitle-tag-name":t.TAGNAME,checked:!1,disabled:!1,tabIndex:0,variant:t.VARIANT,orientation:t.ORIENTATION}},h={args:{"card-title":"Title",subtitle:"Subtitle","image-src":"https://placehold.co/320x200","image-alt":"Image Alt","icon-name":"placeholder-bold",variant:t.VARIANT,"title-tag-name":t.TAGNAME,"subtitle-tag-name":t.TAGNAME,orientation:t.ORIENTATION,"selection-type":r.CHECKBOX,checked:!1,disabled:!1,tabIndex:0,children:o`
+    <mdc-text slot='before-body' type="body-midsize-medium" tagname="span">Content Before Body</mdc-text>
+    <img src="https://placehold.co/100x50" alt="Image Alt" slot="before-body"/>
+    ${l}`}},b={args:{"card-title":"Title",subtitle:"Subtitle","image-src":"https://placehold.co/160x540","image-alt":"Image Alt","icon-name":"placeholder-bold",variant:t.VARIANT,"title-tag-name":t.TAGNAME,"subtitle-tag-name":t.TAGNAME,orientation:T.HORIZONTAL,"selection-type":r.CHECKBOX,checked:!1,disabled:!1,tabIndex:0,children:o`
+    <img src="https://placehold.co/100x50" alt="Image Alt" slot="after-body"/>
+    <mdc-text slot='after-body' type="body-midsize-medium" tagname="span">Content After Body</mdc-text>
+    ${l}`}},u={render:()=>o`
+  <mdc-formfieldgroup
+  label="Checkbox Cards"
+  help-text="Help Text"
+  data-aria-label="Checkbox Cards">
+  <div style="display: flex; gap: 0.5rem; flex-wrap: wrap">
+  ${ee([1,2,3,4],e=>o`
+    <mdc-cardcheckbox
+    name="checkbox cards"
+    variant="border"
+    orientation="vertical"
+    card-title="Title ${e}"
+    subtitle="Subtitle"
+    image-src="https://placehold.co/320x200"
+    image-alt="Image Alt"
+    icon-name="placeholder-bold"
+    tabIndex="0"
+    >
+    ${l}
+    </mdc-cardcheckbox>
+  `)}
+  </div>
+  </mdc-formfieldgroup>
+  `,argTypes:{...re(["variant","orientation","card-title","subtitle","image-src","image-alt","icon-name","checked","disabled","tabIndex","data-aria-label"])}};var N,v,S;s.parameters={...s.parameters,docs:{...(N=s.parameters)==null?void 0:N.docs,source:{originalSource:`{
+  args: {
+    'card-title': 'Title',
+    subtitle: 'Subtitle',
+    'image-src': 'https://placehold.co/320x200',
+    'image-alt': 'Image Alt',
+    'icon-name': 'placeholder-bold',
+    'title-tag-name': DEFAULTS.TAGNAME,
+    'subtitle-tag-name': DEFAULTS.TAGNAME,
+    variant: DEFAULTS.VARIANT,
+    orientation: DEFAULTS.ORIENTATION,
+    checked: false,
+    disabled: false,
+    tabIndex: 0,
+    'selection-type': SELECTION_TYPE.CHECK,
+    children: defaultChildren
+  }
+}`,...(S=(v=s.parameters)==null?void 0:v.docs)==null?void 0:S.source}}};var O,L,$;d.parameters={...d.parameters,docs:{...(O=d.parameters)==null?void 0:O.docs,source:{originalSource:`{
+  args: {
+    'card-title': 'Title',
+    subtitle: 'Subtitle',
+    'image-src': 'https://placehold.co/160x280',
+    'image-alt': 'Image Alt',
+    'icon-name': 'placeholder-bold',
+    'aria-label': 'Aria Label',
+    'title-tag-name': DEFAULTS.TAGNAME,
+    'subtitle-tag-name': DEFAULTS.TAGNAME,
+    'selection-type': SELECTION_TYPE.CHECKBOX,
+    checked: false,
+    disabled: false,
+    tabIndex: 0,
+    variant: DEFAULTS.VARIANT,
+    orientation: ORIENTATIONS.HORIZONTAL,
+    children: defaultChildren
+  }
+}`,...($=(L=d.parameters)==null?void 0:L.docs)==null?void 0:$.source}}};var D,H,G;m.parameters={...m.parameters,docs:{...(D=m.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  args: {
+    'card-title': 'Title',
+    subtitle: 'Subtitle',
+    'icon-name': 'placeholder-bold',
+    'title-tag-name': DEFAULTS.TAGNAME,
+    'subtitle-tag-name': DEFAULTS.TAGNAME,
+    'aria-label': 'Aria Label',
+    'selection-type': SELECTION_TYPE.CHECK,
+    checked: false,
+    disabled: false,
+    tabIndex: 0,
+    variant: DEFAULTS.VARIANT,
+    orientation: DEFAULTS.ORIENTATION,
+    children: defaultChildren
+  }
+}`,...(G=(H=m.parameters)==null?void 0:H.docs)==null?void 0:G.source}}};var U,M,R;p.parameters={...p.parameters,docs:{...(U=p.parameters)==null?void 0:U.docs,source:{originalSource:`{
+  args: {
+    'image-src': 'https://placehold.co/320x200',
+    'image-alt': 'Image Alt',
+    'card-title': 'Title',
+    subtitle: 'Subtitle',
+    'icon-name': 'placeholder-bold',
+    'selection-type': SELECTION_TYPE.CHECK,
+    'title-tag-name': DEFAULTS.TAGNAME,
+    'subtitle-tag-name': DEFAULTS.TAGNAME,
+    checked: false,
+    disabled: false,
+    tabIndex: 0,
+    variant: DEFAULTS.VARIANT,
+    orientation: DEFAULTS.ORIENTATION
+  }
+}`,...(R=(M=p.parameters)==null?void 0:M.docs)==null?void 0:R.source}}};var F,_,B;h.parameters={...h.parameters,docs:{...(F=h.parameters)==null?void 0:F.docs,source:{originalSource:`{
+  args: {
+    'card-title': 'Title',
+    subtitle: 'Subtitle',
+    'image-src': 'https://placehold.co/320x200',
+    'image-alt': 'Image Alt',
+    'icon-name': 'placeholder-bold',
+    variant: DEFAULTS.VARIANT,
+    'title-tag-name': DEFAULTS.TAGNAME,
+    'subtitle-tag-name': DEFAULTS.TAGNAME,
+    orientation: DEFAULTS.ORIENTATION,
+    'selection-type': SELECTION_TYPE.CHECKBOX,
+    checked: false,
+    disabled: false,
+    tabIndex: 0,
+    children: html\`
+    <mdc-text slot='before-body' type="body-midsize-medium" tagname="span">Content Before Body</mdc-text>
+    <img src="https://placehold.co/100x50" alt="Image Alt" slot="before-body"/>
+    \${defaultChildren}\`
+  }
+}`,...(B=(_=h.parameters)==null?void 0:_.docs)==null?void 0:B.source}}};var K,V,P;b.parameters={...b.parameters,docs:{...(K=b.parameters)==null?void 0:K.docs,source:{originalSource:`{
+  args: {
+    'card-title': 'Title',
+    subtitle: 'Subtitle',
+    'image-src': 'https://placehold.co/160x540',
+    'image-alt': 'Image Alt',
+    'icon-name': 'placeholder-bold',
+    variant: DEFAULTS.VARIANT,
+    'title-tag-name': DEFAULTS.TAGNAME,
+    'subtitle-tag-name': DEFAULTS.TAGNAME,
+    orientation: ORIENTATIONS.HORIZONTAL,
+    'selection-type': SELECTION_TYPE.CHECKBOX,
+    checked: false,
+    disabled: false,
+    tabIndex: 0,
+    children: html\`
+    <img src="https://placehold.co/100x50" alt="Image Alt" slot="after-body"/>
+    <mdc-text slot='after-body' type="body-midsize-medium" tagname="span">Content After Body</mdc-text>
+    \${defaultChildren}\`
+  }
+}`,...(P=(V=b.parameters)==null?void 0:V.docs)==null?void 0:P.source}}};var w,X,Y;u.parameters={...u.parameters,docs:{...(w=u.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  render: () => html\`
+  <mdc-formfieldgroup
+  label="Checkbox Cards"
+  help-text="Help Text"
+  data-aria-label="Checkbox Cards">
+  <div style="display: flex; gap: 0.5rem; flex-wrap: wrap">
+  \${repeat([1, 2, 3, 4], index => html\`
+    <mdc-cardcheckbox
+    name="checkbox cards"
+    variant="border"
+    orientation="vertical"
+    card-title="Title \${index}"
+    subtitle="Subtitle"
+    image-src="https://placehold.co/320x200"
+    image-alt="Image Alt"
+    icon-name="placeholder-bold"
+    tabIndex="0"
+    >
+    \${defaultChildren}
+    </mdc-cardcheckbox>
+  \`)}
+  </div>
+  </mdc-formfieldgroup>
+  \`,
+  argTypes: {
+    ...disableControls(['variant', 'orientation', 'card-title', 'subtitle', 'image-src', 'image-alt', 'icon-name', 'checked', 'disabled', 'tabIndex', 'data-aria-label'])
+  }
+}`,...(Y=(X=u.parameters)==null?void 0:X.docs)==null?void 0:Y.source}}};const Ye=["Example","HorizontalCard","CardWithoutImage","CardWithoutBody","ContentBeforeBody","ContentAfterBody","CardsInCheckboxGroup"];export{p as CardWithoutBody,m as CardWithoutImage,u as CardsInCheckboxGroup,b as ContentAfterBody,h as ContentBeforeBody,s as Example,d as HorizontalCard,Ye as __namedExportsOrder,Xe as default};
---
+++ docs/storybook-static/assets/index-Dngu7Afa.js
@@ -0,0 +1,1 @@
+import{I as r}from"./input.component-DIsdWa6J.js";import{T as m}from"./input.constants-BfLXzGxj.js";import"./index-NMeGFEya.js";import"./index-BQmnd5ax.js";import"./index-D3DhXfcW.js";r.register(m);
