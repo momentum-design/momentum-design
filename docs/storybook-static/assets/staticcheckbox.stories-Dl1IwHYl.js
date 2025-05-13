bc55fef4b6a9a3e3421d346abd8224f666917d41
Momentum Design Automation <automation@momentum.design>
2025-05-13 13:09:41 +0000

    ci(docs): deploy documentation [skip ci] [skip release]

---
+++ docs/storybook-static/assets/staticcheckbox.stories-Dl1IwHYl.js
@@ -0,0 +1,14 @@
+import"./index-DVLJFUHY.js";import{k as a}from"./lit-element-D5KKan5q.js";import{c as s,s as d}from"./commonArgTypes-BluK8w5L.js";import{d as i}from"./utils-CO8B6ZcN.js";import"./index-C9z9WAEj.js";import"./DisabledMixin-BZruwOeC.js";import"./index-DtjZzLiR.js";import"./iframe-CH-9ZuZx.js";import"../sb-preview/runtime.js";import"./state-CSSggq2n.js";import"./index-CcXofQbA.js";import"./index-2PiEGcHV.js";import"./iconprovider.component-Bvm-70EG.js";import"./create-context-89xeped_.js";const m=c=>a`
+  <mdc-staticcheckbox 
+    ?checked="${c.checked}"
+    ?indeterminate="${c.indeterminate}"
+    ?disabled="${c.disabled}"
+    class="${c.class}"
+    style="${c.style}"
+  ></mdc-staticcheckbox>`,A={title:"Components/decorator/staticcheckbox",tags:["autodocs"],component:"mdc-staticcheckbox",render:m,parameters:{badges:["stable"]},argTypes:{checked:{control:"boolean"},indeterminate:{control:"boolean"},disabled:{control:"boolean"},...s,...d,...i(["--mdc-staticcheckbox-border-color","--mdc-staticcheckbox-checked-background-color","--mdc-staticcheckbox-disabled-background-color","--mdc-checkbox-disabled-border-color","--mdc-checkbox-disabled-checked-icon-color","--mdc-staticcheckbox-disabled-icon-color","--mdc-staticcheckbox-icon-background-color","--mdc-staticcheckbox-icon-border-color","--mdc-staticcheckbox-icon-color"])}},e={args:{checked:!1,indeterminate:!1,disabled:!1}};var o,t,r;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    checked: false,
+    indeterminate: false,
+    disabled: false
+  }
+}`,...(r=(t=e.parameters)==null?void 0:t.docs)==null?void 0:r.source}}};const C=["Example"];export{e as Example,C as __namedExportsOrder,A as default};
