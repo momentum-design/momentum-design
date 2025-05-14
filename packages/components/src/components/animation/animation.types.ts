2ef0c4cd1a625c46661607ce380509cff24dc92d
László Vadász <lvadasz@cisco.com>
2025-05-14 10:30:08 +0100

    feat(components): animation (#1293)

---
+++ packages/components/src/components/animation/animation.types.ts
@@ -0,0 +1,13 @@
+import type AnimationNames from '@momentum-design/animations/dist/types/types';
+
+/** Event mapping for React */
+interface Events {
+  /** This event is dispatched when all animation loops completed */
+  onCompleteEvent: Event;
+  /** This event is dispatched when animation loading failed */
+  onErrorEvent: Event;
+}
+
+type LoopType = `${boolean | number}`;
+
+export type { Events, AnimationNames, LoopType };
