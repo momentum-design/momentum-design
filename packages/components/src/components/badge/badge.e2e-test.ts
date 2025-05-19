bea8ab9c43d0231b6aeea6c5a2f3c5efd2f3341a
Christoph Scheffauer <scheffauerc@gmail.com>
2025-05-19 09:05:35 +0200

    test: fix badge flakiness by adding timeout

--- packages/components/src/components/badge/badge.e2e-test.ts
+++ packages/components/src/components/badge/badge.e2e-test.ts
@@ -80,10 +80,9 @@ const testToRun = async (componentsPage: ComponentsPage) => {
    * VISUAL REGRESSION
    */
   await test.step('visual-regression', async () => {
-    const loadPromise = componentsPage.page.waitForEvent('load');
     const visualBadge = await visualTestingSetup({ componentsPage });
     // wait for the badges to be rendered
-    await loadPromise;
+    await componentsPage.page.waitForTimeout(1000);
     await test.step('matches screenshot of different types element', async () => {
       await componentsPage.visualRegression.takeScreenshot('mdc-badge', { element: visualBadge });
     });
