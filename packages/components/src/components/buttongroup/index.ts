bd2fe66fc11f2ce78c0871c947cb49cbb0b87500
Supriya M <30731236+supminn@users.noreply.github.com>
2025-05-22 12:46:43 +0530

    feat: create and release buttongroup component (#1307)
    * feat(wip): building buttongroup for icon buttons
    
    * style: compact group
    
    * fix: orientation compact group
    
    * chore: disabled controls on children
    
    * fix: updated vertical story
    
    * chore: story with buttongroup popover
    
    * test: buttongroup test cases
    
    * test: added test case with popover
    
    * docs: updated ts docs on component page
    
    * chore: updated snapshots for component buttongroup
    
    * fix: added tooltip to buttongroup with popover
    
    * fix: buttongroup variants
    
    * fix: updated divider styles
    
    * chore: updated component name
    
    * chore: updated snapshots for component buttongroup
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Christoph Scheffauer <scheffauerc@gmail.com>

---
+++ packages/components/src/components/buttongroup/index.ts
@@ -0,0 +1,12 @@
+import ButtonGroup from './buttongroup.component';
+import { TAG_NAME } from './buttongroup.constants';
+
+ButtonGroup.register(TAG_NAME);
+
+declare global {
+    interface HTMLElementTagNameMap {
+        ['mdc-buttongroup']: ButtonGroup
+    }
+}
+
+export default ButtonGroup;
