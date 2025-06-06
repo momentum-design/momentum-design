ae7ec5f9af211b5598291f1efed8e57a157a244e
Deeya Upadhyay <58636958+2020Deeya@users.noreply.github.com>
2025-06-06 12:58:11 +0530

    chore: create SideNavigation and add storybook docs (#1197)
    * fix: create sideNavigation component, navitemlist component, navitem component
    
    * fix build pipeline
    
    * support focus behavior
    
    * add support for expanded and collapsed states
    
    * added arrow up/down support
    
    * minor eslint fix and syntax change
    
    * add css variables, tsdocs, minor cleanup
    
    * update storybook docs
    
    * minor fix in build process
    
    * update roles and styles and refactor code implementation
    
    * update styling and storybook docs
    
    * minor fix
    
    * extended navItem from menuItem
    
    * minor eslint fix
    
    * fixes based on recent updates
    
    * resolve review comments
    
    * fix build process
    
    * Refactored navList implementation
    
    * Minor build fix
    
    * fix: popover related issue
    
    * fix: add check to fix console failures on menu storybook
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * add arrow, fix nested collapsed, fix Enter/Space key navigation
    
    * minor build and eslint  fix
    
    * updated storybook docs and address review comments
    
    * rename isExpanded to expanded
    
    * chore: set active state to parent navitem when escape
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * minor cleanup
    
    * added docs in storybook
    
    * chore: replacing icons on storybook
    
    * fix: reset active state for all navitems
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * fix fly-out navigation and minor cleanup
    
    * fix build process
    
    * fix: uplift aria-current state and minor cleanup
    
    * added noAriaCurrent attribute
    
    * fix: add recent figma updates
    
    * fix: tooltip position bottom on side navigation
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * perf: add new directory for sidenavgation stories
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * Add user stories and update storybook docs
    
    * minor build fix
    
    * updated user stories and storybook example
    
    * remove dataId attribute
    
    * chore: update tooltip presentation on listitem for navitemlist component
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * address review comments
    
    * renaming and removed unused code
    
    * chore: storybook styling
    
    * fix: ts error on aria-expanded
    
    * added styling to the separator
    
    * revert: button position added to divider
    
    * fix: on vertical menubar disable arrow-right
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * fix: set focusback to trigger on escape
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    
    * fix: arrowkey navigation for rtl
    
    ---------
    
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>
    Co-authored-by: Arjun Kesava P <arjun.kesava@gmail.com>
    Co-authored-by: Supriya M <sansup49@gmail.com>
    Co-authored-by: Supriya M <30731236+supminn@users.noreply.github.com>

--- packages/components/src/components/menupopover/menupopover.component.ts
+++ packages/components/src/components/menupopover/menupopover.component.ts
@@ -39,13 +39,14 @@ class MenuPopover extends MenuMixin(Popover) {
     super.connectedCallback();
     this.role = ROLE.MENU;
 
+    this.focusBackToTrigger = true;
     this.focusTrap = true;
     this.focusBackToTrigger = true;
     this.hideOnEscape = true;
     this.hideOnOutsideClick = true;
+    this.interactive = true;
     this.placement = POPOVER_PLACEMENT.BOTTOM_START;
     this.showArrow = false;
-    this.interactive = true;
     this.ariaOrientation = ORIENTATION.VERTICAL;
   }
 
