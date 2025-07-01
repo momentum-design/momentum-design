ec3252aec3ad6c4c2dbdca5dc6d966977dd45675
Laszlo Vadasz <lvadasz@cisco.com>
2025-07-01 09:39:23 +0100

    feat(menu): menu component improvements
    fix linting

--- packages/components/src/utils/types.ts
+++ packages/components/src/utils/types.ts
@@ -3,8 +3,8 @@ export type ValueOf<T> = T[keyof T];
 
 /**
  * A generic type for strongly typing custom events with their targets
- * @template T - The type of the event target (extends EventTarget)
- * @template D - The type of the detail payload for the custom event
+ * @typeParam T - The type of the event target (extends EventTarget)
+ * @typeParam D - The type of the detail payload for the custom event
  *
  * @see [Strongly Typed Events for Web Components](https://dev.to/stuffbreaker/creating-strongly-typed-events-for-web-components-1jem)
  */
