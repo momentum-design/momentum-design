3b929b998624cf2d0f6dc7ee7f7f4911d8f28015
Christoph Scheffauer <scheffauerc@gmail.com>
2025-07-04 20:13:28 +0200

    fix: change in radio react figma

--- packages/components/src/components/radio/code-connect/radio.react.figma.tsx
+++ packages/components/src/components/radio/code-connect/radio.react.figma.tsx
@@ -4,7 +4,7 @@ import figma from '@figma/code-connect';
 figma.connect('<FIGMA_RADIO_URL>', {
   props: {
     toggletipText: figma.boolean('Info Button', {
-      true: 'This is a toggletip text for the info button.',
+      true: 'This is a toggletip text to let the info button appear.',
       false: undefined,
     }),
   },
