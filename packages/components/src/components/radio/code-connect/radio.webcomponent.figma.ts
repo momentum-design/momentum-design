2af537fc43c0840483e4056f362358c39f9c07e5
Christoph Scheffauer <scheffauerc@gmail.com>
2025-07-04 22:25:54 +0200

    fix: update radio code connect

--- packages/components/src/components/radio/code-connect/radio.webcomponent.figma.ts
+++ packages/components/src/components/radio/code-connect/radio.webcomponent.figma.ts
@@ -2,7 +2,44 @@
 import figma, { html } from '@figma/code-connect/html';
 
 figma.connect('<FIGMA_RADIO_URL>', {
-  props: {},
-  example: () => html`<mdc-radio></mdc-radio>`,
+  props: {
+    label: figma.boolean('Label', {
+      true: figma.textContent('Label'),
+      false: undefined,
+    }),
+    helpText: figma.boolean('Helper Text', {
+      // TODO: Update this to use figma.textContent when available
+      true: 'Helper text',
+      false: undefined,
+    }),
+    toggletipText: figma.boolean('Info Button', {
+      true: 'This is a toggletip text to let the info button appear.',
+      false: undefined,
+    }),
+    readonly: figma.enum('State', {
+      Rest: undefined,
+      Hover: undefined,
+      Pressed: undefined,
+      Focused: undefined,
+      'Read Only': true,
+      Disabled: undefined,
+    }),
+    disabled: figma.enum('State', {
+      Rest: undefined,
+      Hover: undefined,
+      Pressed: undefined,
+      Focused: undefined,
+      'Read Only': undefined,
+      Disabled: true,
+    }),
+  },
+  example: args =>
+    html`<mdc-radio
+      label=${args.label}
+      help-text=${args.helpText}
+      toggletip-text=${args.toggletipText}
+      readonly=${args.readonly}
+      disabled=${args.disabled}
+    ></mdc-radio>`,
   imports: ["import '@momentum-design/components/components/radio';"],
 });
