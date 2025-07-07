22227daa142c2aceab5640dcb3ee6d048468005b
Arjun Kesava P <arjun.kesava@gmail.com>
2025-07-07 11:01:11 +0530

    chore: update menu section story by changing the order
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>

--- packages/components/src/components/menusection/menusection.stories.ts
+++ packages/components/src/components/menusection/menusection.stories.ts
@@ -53,30 +53,34 @@ export const Example: StoryObj = {
 export const MultipleMenuSections: StoryObj = {
   render: () =>
     wrapWithDiv(
-      html` <mdc-menusection>
-          <mdc-menuitemcheckbox label="Checkbox1"></mdc-menuitemcheckbox>
-          <mdc-menuitemcheckbox label="Checkbox2"></mdc-menuitemcheckbox>
-          <mdc-menuitemcheckbox label="Checkbox3"></mdc-menuitemcheckbox>
-          <mdc-menuitemradio label="Radio1"></mdc-menuitemradio>
-          <mdc-menuitemradio label="Radio2"></mdc-menuitemradio>
-          <mdc-menuitemradio label="Radio3"></mdc-menuitemradio>
-        </mdc-menusection>
-        <mdc-divider></mdc-divider>
-        <mdc-menusection>
-          <mdc-menuitemradio label="Radio1"></mdc-menuitemradio>
-          <mdc-menuitemradio label="Radio2"></mdc-menuitemradio>
-          <mdc-menuitemradio label="Radio3"></mdc-menuitemradio>
-        </mdc-menusection>
-        <mdc-divider></mdc-divider>
-        <mdc-menusection header-text="Section with menu items">
+      html`<mdc-menusection header-text="Section with menu items">
           <mdc-menuitem label="MenuItem1"></mdc-menuitem>
           <mdc-menuitem label="MenuItem2"></mdc-menuitem>
         </mdc-menusection>
         <mdc-divider></mdc-divider>
         <mdc-menusection header-text="Section with checkboxes and radio buttons">
           <mdc-menuitemcheckbox label="Toggle6" indicator="toggle"></mdc-menuitemcheckbox>
-          <mdc-menuitemcheckbox label="Checkmark7" indicator="checkmark" aria-label="Checkbox 7"></mdc-menuitemcheckbox>
-        </mdc-menusection>`,
+          <mdc-menuitemcheckbox
+            label="Checkmark7"
+            indicator="checkmark"
+            aria-label="Checkbox 7"
+          ></mdc-menuitemcheckbox> </mdc-menusection
+        ><mdc-divider></mdc-divider
+        ><mdc-menusection>
+          <mdc-menuitemcheckbox name="checkbox" label="Checkbox1"></mdc-menuitemcheckbox>
+          <mdc-menuitemcheckbox name="checkbox" label="Checkbox2"></mdc-menuitemcheckbox>
+          <mdc-menuitemcheckbox name="checkbox" label="Checkbox3"></mdc-menuitemcheckbox>
+          <mdc-menuitemradio name="radio" label="Radio1"></mdc-menuitemradio>
+          <mdc-menuitemradio name="radio" label="Radio2"></mdc-menuitemradio>
+          <mdc-menuitemradio name="radio" label="Radio3"></mdc-menuitemradio>
+        </mdc-menusection>
+        <mdc-divider></mdc-divider>
+        <mdc-menusection>
+          <mdc-menuitemradio name="radio" label="Radio1"></mdc-menuitemradio>
+          <mdc-menuitemradio name="radio" label="Radio2"></mdc-menuitemradio>
+          <mdc-menuitemradio name="radio" label="Radio3"></mdc-menuitemradio>
+        </mdc-menusection>
+        <mdc-divider></mdc-divider> `,
     ),
   ...hideAllControls(),
 };
