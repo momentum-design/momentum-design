77ccb4552b04b1fdb6c21666bc391974f1eecdd0
Supriya M <sansup49@gmail.com>
2025-07-09 13:00:34 +0530

    chore: updated menusection stories

--- packages/components/src/components/menusection/menusection.stories.ts
+++ packages/components/src/components/menusection/menusection.stories.ts
@@ -53,7 +53,7 @@ export const Example: StoryObj = {
 export const MultipleMenuSections: StoryObj = {
   render: () =>
     wrapWithDiv(html`
-      <mdc-menusection>
+      <mdc-menusection show-divider>
         <mdc-menuitemcheckbox label="Checkbox1"></mdc-menuitemcheckbox>
         <mdc-menuitemcheckbox label="Checkbox2"></mdc-menuitemcheckbox>
         <mdc-menuitemcheckbox label="Checkbox3"></mdc-menuitemcheckbox>
@@ -61,27 +61,23 @@ export const MultipleMenuSections: StoryObj = {
         <mdc-menuitemradio label="Radio2"></mdc-menuitemradio>
         <mdc-menuitemradio label="Radio3"></mdc-menuitemradio>
       </mdc-menusection>
-      <mdc-divider></mdc-divider>
-      <mdc-menusection>
+      <mdc-menusection show-divider>
         <mdc-menuitemradio label="Radio1"></mdc-menuitemradio>
         <mdc-menuitemradio label="Radio2"></mdc-menuitemradio>
         <mdc-menuitemradio label="Radio3"></mdc-menuitemradio>
       </mdc-menusection>
-      <mdc-divider></mdc-divider>
-      <mdc-menusection header-text="Section with menu items">
+      <mdc-menusection show-divider header-text="Section with menu items">
         <mdc-menuitem label="MenuItem1"></mdc-menuitem>
         <mdc-menuitem label="MenuItem2"></mdc-menuitem>
       </mdc-menusection>
-      <mdc-divider></mdc-divider>
-      <mdc-menusection header-text="Section with checkboxes and radio buttons">
+      <mdc-menusection show-divider header-text="Section with checkboxes and radio buttons">
         <mdc-menuitemcheckbox label="Toggle6" indicator="toggle"></mdc-menuitemcheckbox>
         <mdc-menuitemcheckbox
           label="Checkmark7"
           indicator="checkmark"
           aria-label="Checkbox 7"
         ></mdc-menuitemcheckbox> </mdc-menusection
-      ><mdc-divider></mdc-divider
-      ><mdc-menusection>
+      ><mdc-menusection show-divider>
         <mdc-menuitemcheckbox name="checkbox" label="Checkbox1"></mdc-menuitemcheckbox>
         <mdc-menuitemcheckbox name="checkbox" label="Checkbox2"></mdc-menuitemcheckbox>
         <mdc-menuitemcheckbox name="checkbox" label="Checkbox3"></mdc-menuitemcheckbox>
@@ -89,13 +85,11 @@ export const MultipleMenuSections: StoryObj = {
         <mdc-menuitemradio name="radio" label="Radio2"></mdc-menuitemradio>
         <mdc-menuitemradio name="radio" label="Radio3"></mdc-menuitemradio>
       </mdc-menusection>
-      <mdc-divider></mdc-divider>
-      <mdc-menusection>
+      <mdc-menusection show-divider>
         <mdc-menuitemradio name="radio" label="Radio1"></mdc-menuitemradio>
         <mdc-menuitemradio name="radio" label="Radio2"></mdc-menuitemradio>
         <mdc-menuitemradio name="radio" label="Radio3"></mdc-menuitemradio>
       </mdc-menusection>
-      <mdc-divider></mdc-divider>
     `),
   ...hideAllControls(),
 };
