b7daccc1bd717fd7295b8eeef4ea2365fab76ecd
arjunkesava <arjun.kesava@gmail.com>
2025-07-17 21:59:53 +0530

    chore: build major chunk of accordion group component
    Signed-off-by: arjunkesava <arjun.kesava@gmail.com>

--- packages/components/src/components/accordiongroup/accordiongroup.stories.ts
+++ packages/components/src/components/accordiongroup/accordiongroup.stories.ts
@@ -2,12 +2,24 @@ import type { Meta, StoryObj, Args } from '@storybook/web-components';
 import '.';
 import { html } from 'lit';
 
+import '../accordionbutton';
 import { classArgType, styleArgType } from '../../../config/storybook/commonArgTypes';
 
-const render = (args: Args) => html` <mdc-accordiongroup args.property="${args.property}"></mdc-accordiongroup>`;
+import { DEFAULTS, SIZE, VARIANT } from './accordiongroup.constants';
+
+const defaultChildren = `Loreum impusm sit amet, consectetur adipiscing elit.`;
+
+const render = (args: Args) =>
+  html` <mdc-accordiongroup size="${args.size}" variant="${args.variant}">
+    <mdc-accordionbutton header-text="Heading 1" prefix-icon="placeholder-bold" expanded
+      >${defaultChildren}</mdc-accordionbutton
+    >
+    <mdc-accordionbutton header-text="Heading 2" prefix-icon="placeholder-bold">${defaultChildren}</mdc-accordionbutton>
+    <mdc-accordionbutton header-text="Heading 3" prefix-icon="placeholder-bold">${defaultChildren}</mdc-accordionbutton>
+  </mdc-accordiongroup>`;
 
 const meta: Meta = {
-  title: 'Work In Progress/accordiongroup',
+  title: 'Work In Progress/accordion/accordiongroup',
   tags: ['autodocs'],
   component: 'mdc-accordiongroup',
   render,
@@ -17,6 +29,14 @@ const meta: Meta = {
   argTypes: {
     ...classArgType,
     ...styleArgType,
+    size: {
+      control: 'select',
+      options: Object.values(SIZE),
+    },
+    variant: {
+      control: 'select',
+      options: Object.values(VARIANT),
+    },
   },
 };
 
@@ -24,7 +44,7 @@ export default meta;
 
 export const Example: StoryObj = {
   args: {
-    class: 'custom-classname',
-    style: 'margin-top: 20px;',
+    size: DEFAULTS.SIZE,
+    variant: DEFAULTS.VARIANT,
   },
 };
--- packages/components/src/components/accordiongroup/accordiongroup.styles.ts
+++ packages/components/src/components/accordiongroup/accordiongroup.styles.ts
@@ -2,6 +2,11 @@ import { css } from 'lit';
 
 const styles = css`
   :host {
+    display: flex;
+    flex-direction: column;
+  }
+  :host([variant='stacked']) {
+    row-gap: 1.5rem;
   }
 `;
 
