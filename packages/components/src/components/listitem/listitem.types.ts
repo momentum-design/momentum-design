5ee39ea4fd3e751ca7baae0d2759f7a94cff26f2
Laszlo Vadasz <lvadasz@cisco.com>
2025-07-18 15:46:32 +0100

    fix(menupopover): update tabindex after menu item(s) changed
    - add created, destroyed, enabled and disabled events to mdc-listitem
    - update tabindexes based on menu item lifecycle events

--- packages/components/src/components/listitem/listitem.types.ts
+++ packages/components/src/components/listitem/listitem.types.ts
@@ -2,13 +2,20 @@ import type { ValueOf } from '../../utils/types';
 
 import { LISTITEM_VARIANTS } from './listitem.constants';
 
-type ListItemVariants = ValueOf<typeof LISTITEM_VARIANTS>;
+export type ListItemVariants = ValueOf<typeof LISTITEM_VARIANTS>;
 
-interface Events {
+export interface Events {
   onClickEvent: MouseEvent;
   onKeyDownEvent: KeyboardEvent;
   onKeyUpEvent: KeyboardEvent;
   onFocusEvent: FocusEvent;
 }
 
-export type { ListItemVariants, Events };
+declare global {
+  interface GlobalEventHandlersEventMap {
+    disabled: Event;
+    enabled: Event;
+    created: Event;
+    destroyed: Event;
+  }
+}
