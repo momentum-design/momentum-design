4f7fa629b4f2eaaca0277f08a9a9194301cac26e
László Vadász <maxinteger@gmail.com>
2025-07-18 16:43:48 +0100

    fix(menupopover): update tabindex after menu item(s) changed (#1408)
    * fix(menupopover): update tabindex after menu item(s) changed
    
    - add created, destroyed, enabled and disabled events to mdc-listitem
    - update tabindexes based on menu item lifecycle events
    
    * fix(menupopover): update tabindex after menu item(s) changed
    
    - add created, destroyed, enabled and disabled events to mdc-listitem
    - update tabindexes based on menu item lifecycle events

--- packages/components/src/components/listitem/listitem.types.ts
+++ packages/components/src/components/listitem/listitem.types.ts
@@ -2,13 +2,24 @@ import type { ValueOf } from '../../utils/types';
 
 import { LISTITEM_VARIANTS } from './listitem.constants';
 
-type ListItemVariants = ValueOf<typeof LISTITEM_VARIANTS>;
+export type ListItemVariants = ValueOf<typeof LISTITEM_VARIANTS>;
 
-interface Events {
+export interface Events {
   onClickEvent: MouseEvent;
   onKeyDownEvent: KeyboardEvent;
   onKeyUpEvent: KeyboardEvent;
   onFocusEvent: FocusEvent;
+  onEnabledEvent: Event;
+  onDisabledEvent: Event;
+  onCreatedEvent: Event;
+  onDestroyedEvent: Event;
 }
 
-export type { ListItemVariants, Events };
+declare global {
+  interface GlobalEventHandlersEventMap {
+    disabled: Event;
+    enabled: Event;
+    created: Event;
+    destroyed: Event;
+  }
+}
