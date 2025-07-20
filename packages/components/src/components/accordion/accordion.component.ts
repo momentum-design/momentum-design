00f809790a5276621b0a1d9765a2e2a3ff2c677c
arjunkesava <arjun.kesava@gmail.com>
2025-07-20 18:00:00 +0530

    chore: build accordion component extending accordion button
    Signed-off-by: arjunkesava <arjun.kesava@gmail.com>

--- packages/components/src/components/accordion/accordion.component.ts
+++ packages/components/src/components/accordion/accordion.component.ts
@@ -1,6 +1,10 @@
 import { CSSResult, html } from 'lit';
+import { queryAssignedElements } from 'lit/decorators.js';
 
-import { Component } from '../../models';
+import { ROLE } from '../../utils/roles';
+import AccordionButton from '../accordionbutton';
+import { ICON_NAME } from '../accordionbutton/accordionbutton.constants';
+import { BUTTON_VARIANTS } from '../button/button.constants';
 
 import styles from './accordion.styles';
 
@@ -14,13 +18,38 @@ import styles from './accordion.styles';
  *
  * @cssproperty --custom-property-name - Description of the CSS custom property
  */
-class Accordion extends Component {
-  public override render() {
-    return html`<p>This is a dummy accordion component!</p>
-      <slot></slot>`;
+class Accordion extends AccordionButton {
+  /** @internal */
+  @queryAssignedElements({ slot: 'leading-controls' })
+  leadingControlsSlot!: Array<HTMLElement>;
+
+  /** @internal */
+  @queryAssignedElements({ slot: 'trailing-controls' })
+  trailingControlsSlot!: Array<HTMLElement>;
+
+  public override renderHeader() {
+    return html`
+      <div part="header-section">
+        <div part="leading-header">
+          ${this.renderIcon(this.prefixIcon)}
+          <div role="${ROLE.HEADING}" aria-level="${this.dataAriaLevel}">${this.renderHeadingText()}</div>
+          <slot name="leading-controls"></slot>
+        </div>
+        <div part="trailing-header">
+          <slot name="trailing-controls"></slot>
+          <mdc-button
+            @click="${this.handleHeaderClick}"
+            aria-controls="${this.bodySectionId}"
+            aria-expanded="${this.expanded}"
+            prefix-icon="${this.expanded ? ICON_NAME.ARROW_UP : ICON_NAME.ARROW_DOWN}"
+            variant="${BUTTON_VARIANTS.TERTIARY}"
+          ></mdc-button>
+        </div>
+      </div>
+    `;
   }
 
-  public static override styles: Array<CSSResult> = [...Component.styles, ...styles];
+  public static override styles: Array<CSSResult> = [...AccordionButton.styles, ...styles];
 }
 
 export default Accordion;
--- packages/components/src/components/accordion/accordion.styles.ts
+++ packages/components/src/components/accordion/accordion.styles.ts
@@ -1,7 +1,21 @@
 import { css } from 'lit';
 
 const styles = css`
-  :host {
+  :host::part(header-section) {
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    justify-content: space-between;
+  }
+  :host::part(header-section):hover,
+  :host::part(header-section):active {
+    background-color: unset;
+  }
+  :host([size='small'])::part(header-section) {
+    padding-right: 0.5rem;
+  }
+  :host([size='large'])::part(header-section) {
+    padding-right: 1rem;
   }
 `;
 
