fc7985647ec42296d983afaab763c980688c3d67
Christoph Scheffauer <scheffauerc@gmail.com>
2025-08-01 11:39:50 +0200

    fix: multiple fixes to popover, tooltip, select, formfieldwrapper (#1429)
    * fix: popover tooltip close issue and types
    
    * fix: several fixes
    
    * fix: styling issue where select wasn't overridding popover size
    
    * fix: memory leaks and more
    
    * fix: small fixes
    
    * fix: more fixes and cleanup

--- packages/components/src/components/cardradio/cardradio.component.ts
+++ packages/components/src/components/cardradio/cardradio.component.ts
@@ -75,9 +75,9 @@ class CardRadio extends DisabledMixin(TabIndexMixin(Card)) {
 
   constructor() {
     super();
-    this.addEventListener('click', this.toggleChecked);
-    this.addEventListener('keydown', this.toggleOnEnter);
-    this.addEventListener('keyup', this.toggleOnSpace);
+    this.addEventListener('click', this.toggleChecked.bind(this));
+    this.addEventListener('keydown', this.toggleOnEnter.bind(this));
+    this.addEventListener('keyup', this.toggleOnSpace.bind(this));
   }
 
   override connectedCallback() {
