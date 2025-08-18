b252ceb60416c40b848a29ce3b402be6c1e7d03d
László Vadász <maxinteger@gmail.com>
2025-08-18 20:21:49 +0100

    fix(menu): improve menu customisation (#1473)
    feat(menu): improve menu customisation
    
    - align menuitemradio and menuitemcheckbox to menuitem by adding "content" slot
    - add "container" part to the menusection's default slot
    - add "action" event to menusection to handle click on grouped menu items

--- packages/components/src/components/menusection/menusection.types.ts
+++ packages/components/src/components/menusection/menusection.types.ts
@@ -2,6 +2,10 @@ import type { TypedCustomEvent } from '../../utils/types';
 import type MenuItem from '../menuitem/menuitem.component';
 
 export type MenuSectionChangeEvent = TypedCustomEvent<MenuItem>;
+
+export type MenuSectionActionEvent = TypedCustomEvent<MenuItem>;
+
 export interface Events {
   onChangeEvent: MenuSectionChangeEvent;
+  onActionEvent: MenuSectionActionEvent;
 }
