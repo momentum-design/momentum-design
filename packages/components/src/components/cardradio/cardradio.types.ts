3ac956baffd046e4b7446b5f0678abaa05a70ff1
Deeya <dishuupadhyay01@gmail.com>
2025-08-19 13:53:05 +0530

    updated types

--- packages/components/src/components/cardradio/cardradio.types.ts
+++ packages/components/src/components/cardradio/cardradio.types.ts
@@ -1,4 +1,4 @@
-import type { OverrideEventTarget } from '../../utils/types';
+import type { OverrideEventTarget, TypedCustomEvent } from '../../utils/types';
 
 import type CardRadio from './cardradio.component';
 
@@ -7,6 +7,7 @@ interface Events {
   onKeyDownEvent: OverrideEventTarget<KeyboardEvent, CardRadio>;
   onKeyUpEvent: OverrideEventTarget<KeyboardEvent, CardRadio>;
   onFocusEvent: OverrideEventTarget<FocusEvent, CardRadio>;
+  onChangeEvent: TypedCustomEvent<CardRadio>;
 }
 
 export type { Events };
