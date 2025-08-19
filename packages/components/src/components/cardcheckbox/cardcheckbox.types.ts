35bdab171f99870b3a911f1873f9183b3114e35e
Deeya <dishuupadhyay01@gmail.com>
2025-08-19 16:05:38 +0530

    address review comments

--- packages/components/src/components/cardcheckbox/cardcheckbox.types.ts
+++ packages/components/src/components/cardcheckbox/cardcheckbox.types.ts
@@ -4,13 +4,14 @@ import type CardCheckbox from './cardcheckbox.component';
 import { SELECTION_TYPE } from './cardcheckbox.constants';
 
 type SelectionType = ValueOf<typeof SELECTION_TYPE>;
+type CardCheckboxChangeEvent = TypedCustomEvent<CardCheckbox>
 
 interface Events {
   onClickEvent: OverrideEventTarget<MouseEvent, CardCheckbox>;
   onKeyDownEvent: OverrideEventTarget<KeyboardEvent, CardCheckbox>;
   onKeyUpEvent: OverrideEventTarget<KeyboardEvent, CardCheckbox>;
   onFocusEvent: OverrideEventTarget<FocusEvent, CardCheckbox>;
-  onChangeEvent: TypedCustomEvent<CardCheckbox>;
+  onChangeEvent: CardCheckboxChangeEvent;
 }
 
 export { SelectionType, Events };
--- packages/components/src/components/cardradio/cardradio.types.ts
+++ packages/components/src/components/cardradio/cardradio.types.ts
@@ -2,12 +2,13 @@ import type { OverrideEventTarget, TypedCustomEvent } from '../../utils/types';
 
 import type CardRadio from './cardradio.component';
 
+type CardRadioChangeEvent = TypedCustomEvent<CardRadio>
 interface Events {
   onClickEvent: OverrideEventTarget<MouseEvent, CardRadio>;
   onKeyDownEvent: OverrideEventTarget<KeyboardEvent, CardRadio>;
   onKeyUpEvent: OverrideEventTarget<KeyboardEvent, CardRadio>;
   onFocusEvent: OverrideEventTarget<FocusEvent, CardRadio>;
-  onChangeEvent: TypedCustomEvent<CardRadio>;
+  onChangeEvent: CardRadioChangeEvent;
 }
 
 export type { Events };
