3e8a1fe7b93c3ce486617ba5c046bf825dceb529
Laszlo Vadasz <lvadasz@cisco.com>
2025-08-26 11:38:39 +0100

    fix(button-group): buttong group height fixed
    Buttong group secondary variant got extra border and its height
    was 2px bigger then a button with the same size.

--- packages/components/src/components/buttongroup/buttongroup.styles.ts
+++ packages/components/src/components/buttongroup/buttongroup.styles.ts
@@ -7,18 +7,20 @@ const styles = css`
     --mdc-buttongroup-border-radius: 1.25rem;
     --mdc-buttongroup-border-color: var(--mds-color-theme-outline-button-normal);
     --mdc-buttongroup-divider-color: var(--mds-color-theme-outline-secondary-normal);
+    --mdc-buttongroup-border-width: 1px;
   }
 
   :host::part(container) {
     display: flex;
     border-radius: var(--mdc-buttongroup-border-radius);
-    border: 1px solid var(--mdc-buttongroup-border-color);
   }
 
+  :host::part(container) {
+    gap: var(--mdc-buttongroup-border-width);
+  }
   :host([variant='primary'])::part(container) {
     border: none;
     background-color: var(--mds-color-theme-outline-primary-normal);
-    gap: 1px;
   }
 
   ::slotted(mdc-button) {
@@ -26,44 +28,59 @@ const styles = css`
     border-radius: 0;
     border: none;
     box-sizing: content-box;
+    position: relative;
   }
 
   :host([orientation='vertical'])::part(container) {
     flex-direction: column;
   }
 
-  :host([orientation='horizontal'][variant='secondary']:dir(ltr)) ::slotted(mdc-button:not(:last-child)) {
-    border-right: 1px solid var(--mdc-buttongroup-divider-color);
-  }
-  :host([orientation='horizontal'][variant='secondary']:dir(rtl)) ::slotted(mdc-button:not(:last-child)) {
-    border-left: 1px solid var(--mdc-buttongroup-divider-color);
-  }
-  :host([orientation='vertical'][variant='secondary']) ::slotted(mdc-button:not(:last-child)) {
-    border-bottom: 1px solid var(--mdc-buttongroup-divider-color);
+  :host([variant='secondary'])::part(container) {
+    outline: var(--mdc-buttongroup-border-width) solid var(--mdc-buttongroup-border-color);
+    outline-offset: calc(-1 * var(--mdc-buttongroup-border-width));
   }
 
-  :host([orientation='vertical']) ::slotted(mdc-button:first-child) {
+  :host([variant='secondary'][orientation='horizontal']) ::slotted(mdc-button:not(:last-of-type))::after {
+    content: '';
+    position: absolute;
+    width: var(--mdc-buttongroup-border-width);
+    height: calc(100% - 2 * var(--mdc-buttongroup-border-width));
+    background: var(--mdc-buttongroup-divider-color);
+    inset-inline-end: calc(-1 * var(--mdc-buttongroup-border-width));
+    inset-block-start: var(--mdc-buttongroup-border-width);
+  }
+  :host([variant='secondary'][orientation='vertical']) ::slotted(mdc-button:not(:last-of-type))::after {
+    content: '';
+    position: absolute;
+    width: calc(100% - 2 * var(--mdc-buttongroup-border-width));
+    height: var(--mdc-buttongroup-border-width);
+    background: var(--mdc-buttongroup-divider-color);
+    inset-block-end: calc(-1 * var(--mdc-buttongroup-border-width));
+    inset-inline-end: var(--mdc-buttongroup-border-width);
+  }
+
+  :host([orientation='vertical']) ::slotted(mdc-button:first-of-type) {
     border-top-left-radius: var(--mdc-buttongroup-border-radius);
     border-top-right-radius: var(--mdc-buttongroup-border-radius);
   }
-  :host([orientation='vertical']) ::slotted(mdc-button:last-child) {
+  :host([orientation='vertical']) ::slotted(mdc-button:last-of-type) {
     border-bottom-left-radius: var(--mdc-buttongroup-border-radius);
     border-bottom-right-radius: var(--mdc-buttongroup-border-radius);
   }
 
-  :host([orientation='horizontal']:dir(ltr)) ::slotted(mdc-button:first-child) {
+  :host([orientation='horizontal']:dir(ltr)) ::slotted(mdc-button:first-of-type) {
     border-top-left-radius: var(--mdc-buttongroup-border-radius);
     border-bottom-left-radius: var(--mdc-buttongroup-border-radius);
   }
-  :host([orientation='horizontal']:dir(rtl)) ::slotted(mdc-button:first-child) {
+  :host([orientation='horizontal']:dir(rtl)) ::slotted(mdc-button:first-of-type) {
     border-top-right-radius: var(--mdc-buttongroup-border-radius);
     border-bottom-right-radius: var(--mdc-buttongroup-border-radius);
   }
-  :host([orientation='horizontal']:dir(ltr)) ::slotted(mdc-button:last-child) {
+  :host([orientation='horizontal']:dir(ltr)) ::slotted(mdc-button:last-of-type) {
     border-top-right-radius: var(--mdc-buttongroup-border-radius);
     border-bottom-right-radius: var(--mdc-buttongroup-border-radius);
   }
-  :host([orientation='horizontal']:dir(rtl)) ::slotted(mdc-button:last-child) {
+  :host([orientation='horizontal']:dir(rtl)) ::slotted(mdc-button:last-of-type) {
     border-top-left-radius: var(--mdc-buttongroup-border-radius);
     border-bottom-left-radius: var(--mdc-buttongroup-border-radius);
   }
