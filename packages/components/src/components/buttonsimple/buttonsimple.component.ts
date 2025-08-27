4a7a72f5defb88d7092e0fc64ffc284f7c0bb869
László Vadász <maxinteger@gmail.com>
2025-08-27 09:55:14 +0100

    fix(button): add missing name and value attributes (#1490)
    * fix(button): add missing name and value attributes
    
    * fix(button): add missing name and value attributes
    
    ---------
    
    Co-authored-by: Christoph Scheffauer <scheffauerc@gmail.com>

--- packages/components/src/components/buttonsimple/buttonsimple.component.ts
+++ packages/components/src/components/buttonsimple/buttonsimple.component.ts
@@ -94,6 +94,18 @@ class Buttonsimple extends AutoFocusOnMountMixin(TabIndexMixin(DisabledMixin(Com
   type: ButtonType = DEFAULTS.TYPE;
 
   /**
+   * The name of the button, submitted as a pair with the button's value as part of the form data,
+   * when that button is used to submit the form.
+   */
+  @property({ type: String, reflect: true }) name?: string;
+
+  /**
+   * Defines the value associated with the button's name when it's submitted with the form data.
+   * This value is passed to the server in params when the form is submitted using this button.
+   */
+  @property({ type: String, reflect: true }) value?: string;
+
+  /**
    * @internal
    */
   private prevTabindex = 0;
