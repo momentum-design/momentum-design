7232c6103173854c9a0f6446b095bba07b604f42
Dominic Carrington <docarrin@cisco.com>
2025-08-29 17:17:31 +0100

    fix(list): update getCurrentIndex to search above the parentElement

--- packages/components/src/utils/mixins/ListNavigationMixin.ts
+++ packages/components/src/utils/mixins/ListNavigationMixin.ts
@@ -175,7 +175,16 @@ export const ListNavigationMixin = <T extends Constructor<Component>>(superClass
      * @returns - The index of the current item in the `navItems` array.
      */
     private getCurrentIndex(target: EventTarget | null): number {
-      return this.navItems.findIndex(node => node === target || node === (target as HTMLElement).parentElement);
+      let el: HTMLElement | null = target as HTMLElement;
+      do {
+        const checkEl = el;
+        const i = this.navItems.findIndex(node => node === checkEl);
+        if (i !== -1) return i;
+
+        el = el.parentElement;
+      } while (el);
+
+      return -1;
     }
 
     /**
