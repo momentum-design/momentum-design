b3f115ae61ad2790ced9a3ef56e3642cba12bc8f
Supriya M <30731236+supminn@users.noreply.github.com>
2025-09-02 15:16:48 +0530

    feat: created static chip component (#1500)
    * feat: created static chip component
    
    * chore: updated snapshots for component staticchip
    
    * style: using common styles between components
    
    * chore: minor fixes
    
    * fix: adding height to chip
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Deeya Upadhyay <58636958+2020Deeya@users.noreply.github.com>

--- packages/components/src/components/staticchip/staticchip.constants.ts
+++ packages/components/src/components/staticchip/staticchip.constants.ts
@@ -1,17 +1,25 @@
-import { ROLE } from '../../utils/roles';
 import utils from '../../utils/tag-name';
-import { BUTTON_SIZES } from '../buttonsimple/buttonsimple.constants';
-import { COLOR } from '../staticchip/staticchip.constants';
 import { TYPE, VALID_TEXT_TAGS } from '../text/text.constants';
 
-const TAG_NAME = utils.constructTagName('chip');
+const TAG_NAME = utils.constructTagName('staticchip');
+
+const COLOR = {
+  DEFAULT: 'default',
+  COBALT: 'cobalt',
+  GOLD: 'gold',
+  LIME: 'lime',
+  MINT: 'mint',
+  ORANGE: 'orange',
+  PINK: 'pink',
+  PURPLE: 'purple',
+  SLATE: 'slate',
+  VIOLET: 'violet',
+} as const;
 
 const DEFAULTS = {
   COLOR: COLOR.DEFAULT,
   TEXT_TYPE: TYPE.BODY_MIDSIZE_REGULAR,
   TAG_NAME: VALID_TEXT_TAGS.SPAN,
-  SIZE: BUTTON_SIZES[24],
-  ROLE: ROLE.BUTTON,
 } as const;
 
 export { TAG_NAME, COLOR, DEFAULTS };
