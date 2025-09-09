724b0b68c43ac2227697dae6aa4a55d93201b14e
Supriya M <30731236+supminn@users.noreply.github.com>
2025-09-09 15:11:06 +0530

    refactor: building slider component  (#1485)
    * chore: generated component
    
    * feat: updated component, stories and styles
    
    * style(wip): continuous slider
    
    * style(wip): progress color; disabled styling
    
    * style: updated firefox and edge
    
    * style(ticks): wip for single slider
    
    * style: positioning ticks and tooltip:
    
    * style: soft-disabled and tooltip rendering
    
    * test: update attirbutes and test cases
    
    * docs: update storybook
    
    * fix: position ticks at the center of thumb
    
    * chore: cleanup
    
    * refactor(wip): updating component to accomodate range slider
    
    * docs: slider range with styles and stories
    
    * fix: event types
    
    * chore: updated snapshots for component slider
    
    * fix: high contrast mode colors
    
    * chore: updated snapshots for component slider
    
    * fix: hide tooltip from sr
    
    * fix: tooltip position
    
    * chore: updated snapshots for component slider
    
    * fix: not showing tick when slider thumb is placed on it
    
    * chore: updated snapshots for component slider
    
    * fix: addressed review comments
    
    * test: updated test to drag thumb
    
    * chore: updated type import
    
    * fix: a11y issues on component
    
    * fix: static id for slider in shadowroot
    
    * style: fixes in HC
    
    * fix: styling ticks positioning logic
    
    * refactor: changes on component
    
    * fix: rightly add/remove listener
    
    * refactor: constants and private state updated
    
    * fix: tooltip position styling
    
    * refactor: utils and functions placements
    
    * fix: calculating tick positions
    
    * chore: updated snapshots for component slider
    
    * style: high contrast for ticks
    
    * chore: updated snapshots for component slider
    
    * refactor: cleanup on removing js style caclulations
    
    * chore: updated snapshots for component slider
    
    * fix: repeat ticks logic
    
    * chore: updated snapshots for component slider
    
    * fix: type errors fixed
    
    * fix: soft-disabled on mousedown
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Deeya Upadhyay <58636958+2020Deeya@users.noreply.github.com>

--- packages/components/src/components/slider/slider.types.ts
+++ packages/components/src/components/slider/slider.types.ts
@@ -1,5 +1,14 @@
-// import type { ValueOf } from '../../utils/types';
+import type { OverrideEventTarget, TypedCustomEvent, ValueOf } from '../../utils/types';
 
-interface Events {}
+import type Slider from './slider.component';
+import { THUMBSTATE } from './slider.constants';
 
-export type { Events };
+type SliderChangeEvent = TypedCustomEvent<Slider>;
+type ThumbStateType = ValueOf<typeof THUMBSTATE>;
+
+interface Events {
+  onInputEvent: OverrideEventTarget<InputEvent, Slider>;
+  onChangeEvent: SliderChangeEvent;
+}
+
+export type { Events, ThumbStateType };
