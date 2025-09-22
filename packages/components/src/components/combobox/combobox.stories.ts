52d1939fc71408add7a24134815ab5f2cacddd8f
supminn <sansup49@gmail.com>
2025-09-22 15:00:03 +0530

    chore: updating storybook actions

--- packages/components/src/components/combobox/combobox.stories.ts
+++ packages/components/src/components/combobox/combobox.stories.ts
@@ -1,4 +1,4 @@
-import { action } from '@storybook/addon-actions';
+import { action } from 'storybook/actions';
 import type { Args, Meta, StoryObj } from '@storybook/web-components';
 import { html, TemplateResult } from 'lit';
 import '.';
