c0e4f60f2ca2f0481d01265ec37f8c3a9df09176
arjunkesava <arjun.kesava@gmail.com>
2025-09-23 18:17:14 +0530

    chore: convert classes to part in typewriter component
    Signed-off-by: arjunkesava <arjun.kesava@gmail.com>

--- packages/components/src/components/typewriter/typewriter.stories.ts
+++ packages/components/src/components/typewriter/typewriter.stories.ts
@@ -15,6 +15,7 @@ import '../selectlistbox';
 import '../option';
 import { classArgType, styleArgType } from '../../../config/storybook/commonArgTypes';
 import { TYPE, VALID_TEXT_TAGS } from '../text/text.constants';
+import { disableCSSParts } from '../../../config/storybook/utils';
 
 import { DEFAULTS } from './typewriter.constants';
 
@@ -59,6 +60,7 @@ const meta: Meta = {
       description:
         'Maximum number of text chunks in queue before dropping oldest chunks. Set to high value for unlimited.',
     },
+    ...disableCSSParts([DEFAULTS.CSS_PART_CONTAINER, DEFAULTS.CSS_PART_TEXT, DEFAULTS.CSS_PART_HIDDEN]),
     ...classArgType,
     ...styleArgType,
   },
