29754978b06bacbc1602b37d92fc7cb03f1177b2
Deeya <dishuupadhyay01@gmail.com>
2025-11-12 06:49:50 +0530

    test: verify stories and e2e changes don't trigger publish trial

--- packages/components/src/components/badge/badge.e2e-test.ts
+++ packages/components/src/components/badge/badge.e2e-test.ts
@@ -14,7 +14,7 @@ type SetupOptions = {
   counter?: number;
   maxCounter?: number;
   ariaLabel?: string;
-  testWorkflow?: boolean; // Test property to verify no dist change
+  testWorkflow2?: boolean; // Test property to verify no dist change
 };
 
 const setup = async (args: SetupOptions) => {
