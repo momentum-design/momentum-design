5f004fed73ac0264f3371aa5125a981f3018de8c
Arjun Kesava P <arjun.kesava@gmail.com>
2025-11-21 14:52:30 +0530

    refactor: add icon name manifest on alertchip story
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>

--- packages/components/src/components/alertchip/alertchip.stories.ts
+++ packages/components/src/components/alertchip/alertchip.stories.ts
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj, Args } from '@storybook/web-components';
 import '.';
+import iconsManifest from '@momentum-design/icons/dist/manifest.json';
 import { html } from 'lit';
 import { action } from 'storybook/actions';
 import { ifDefined } from 'lit/directives/if-defined.js';
@@ -35,7 +36,8 @@ const meta: Meta = {
       control: 'text',
     },
     'icon-name': {
-      control: 'text',
+      control: 'select',
+      options: Object.keys(iconsManifest),
     },
     'auto-focus-on-mount': {
       control: 'boolean',
