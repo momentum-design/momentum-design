ddbd1c9760dfe10953f392bc5f06182d2729a1de
Arjun Kesava P <arjun.kesava@gmail.com>
2025-11-27 17:08:34 +0530

    revert: playwright config changes
    Signed-off-by: Arjun Kesava P <arjun.kesava@gmail.com>

--- packages/components/config/playwright/playwright.config.ts
+++ packages/components/config/playwright/playwright.config.ts
@@ -44,13 +44,7 @@ const config: PlaywrightTestConfig = {
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? '50%' : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI
-    ? [
-        ['html'],
-        ['@estruyf/github-actions-reporter', githubActionsReporterOptions],
-        [require.resolve('./custom-reporter.ts')],
-      ]
-    : 'html',
+  reporter: process.env.CI ? [['html'], ['@estruyf/github-actions-reporter', githubActionsReporterOptions]] : 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
