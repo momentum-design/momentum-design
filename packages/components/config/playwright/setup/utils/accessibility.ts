26cf5ac95cdb71fb5fdaae401ec00eb57b5b5ae0
Arjun Kesava <arjun.kesava@gmail.com>
2025-11-28 12:58:09 +0530

    chore: add browser name and supress console info on a11y html report genaration phase
    Signed-off-by: Arjun Kesava <arjun.kesava@gmail.com>

--- packages/components/config/playwright/setup/utils/accessibility.ts
+++ packages/components/config/playwright/setup/utils/accessibility.ts
@@ -55,12 +55,17 @@ class Accessibility {
    * @param accessibilityScanResults - scan results
    */
   async attachA11yResults(testResultsName: string, accessibilityScanResults: any) {
-    const browserName = this.page.context().browser()?.browserType()?.name() ?? 'unknown';
-    const fileName = `accessibility-scan-results-${testResultsName}-${browserName}.html`;
+    const browserName = this.page.context()?.browser()?.browserType().name() ?? 'unknown';
+    const fileName = `accessibility-scan-results-${testResultsName.split(' ').join('')}-${browserName}.html`;
 
+    // todo: add option to suppress the output of the report if
+    // https://github.com/lpelypenko/axe-html-reporter/issues/40 is resolved
+    // eslint-disable-next-line no-console
+    const originalInfo = console.info;
     try {
-      // todo: add option to suppress the output of the report if
-      // https://github.com/lpelypenko/axe-html-reporter/issues/40 is resolved
+      // We will supress the info temporary and will revert it back after the createHtmlReport called in the finally phase.
+      // eslint-disable-next-line no-console
+      console.info = () => {};
       createHtmlReport({
         results: accessibilityScanResults,
         options: {
@@ -70,8 +75,11 @@ class Accessibility {
         },
       });
     } catch (e) {
-      console.log('HITCH KOK in attachA11yResults ', fileName);
-      console.log(e);
+      // eslint-disable-next-line no-console
+      console.log('CREATE HTML REPORT FAILED ', e);
+    } finally {
+      // eslint-disable-next-line no-console
+      console.info = originalInfo;
     }
 
     await this.attachToReport(fileName);
@@ -95,28 +103,23 @@ class Accessibility {
       inclusions?: string[];
     } = {},
   ) {
-    try {
-      const { exclusions, inclusions, rules, tags } = { ...CONSTANTS.DEFAULT_ACCESSIBILITY_SCAN_OPTIONS, ...options };
-      const accessibilityScanner = new AxeBuilder({ page: this.page }).withTags(tags).disableRules(rules);
+    const { exclusions, inclusions, rules, tags } = { ...CONSTANTS.DEFAULT_ACCESSIBILITY_SCAN_OPTIONS, ...options };
+    const accessibilityScanner = new AxeBuilder({ page: this.page }).withTags(tags).disableRules(rules);
 
-      exclusions?.forEach(exclusion => {
-        accessibilityScanner.exclude(exclusion);
-      });
+    exclusions?.forEach(exclusion => {
+      accessibilityScanner.exclude(exclusion);
+    });
 
-      inclusions?.forEach(inclusion => {
-        accessibilityScanner.include(inclusion);
-      });
+    inclusions?.forEach(inclusion => {
+      accessibilityScanner.include(inclusion);
+    });
 
-      const accessibilityScanResults = await accessibilityScanner.analyze();
+    const accessibilityScanResults = await accessibilityScanner.analyze();
 
-      await this.attachA11yResults(testResultsName, accessibilityScanResults);
+    await this.attachA11yResults(testResultsName, accessibilityScanResults);
 
-      if (shouldCheck) {
-        expect(accessibilityScanResults.violations).toEqual([]);
-      }
-    } catch (e) {
-      console.log('HITCH KOK in checkForA11yViolations ', testResultsName);
-      console.log(e);
+    if (shouldCheck) {
+      expect(accessibilityScanResults.violations).toEqual([]);
     }
   }
 }
