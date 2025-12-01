6a2d21c67a143c82d4d357a78c4183d1aeae863b
Arjun Kesava <arjun.kesava@gmail.com>
2025-12-01 14:45:19 +0530

    refactor: clean up storybook controls for brand visual component
    Signed-off-by: Arjun Kesava <arjun.kesava@gmail.com>

--- packages/components/src/components/brandvisual/brandvisual.stories.ts
+++ packages/components/src/components/brandvisual/brandvisual.stories.ts
@@ -1,17 +1,23 @@
 import type { Meta, StoryObj, Args } from '@storybook/web-components';
 import '.';
 import { html } from 'lit';
+import brandVisualsManifest from '@momentum-design/brand-visuals/dist/manifest.json';
+import { action } from 'storybook/actions';
 
 import { classArgType, styleArgType } from '../../../config/storybook/commonArgTypes';
 import { hideControls } from '../../../config/storybook/utils';
 
 const render = (args: Args) =>
-  html` <mdc-brandvisual
-    class="${args.class}"
-    name="${args.name}"
-    style="${args.style}"
-    altText="${args.altText}"
-  ></mdc-brandvisual>`;
+  html`<div role="main">
+    <mdc-brandvisual
+      @load="${action('load')}"
+      @error="${action('error')}"
+      alt-text="${args['alt-text']}"
+      class="${args.class}"
+      name="${args.name}"
+      style="${args.style}"
+    ></mdc-brandvisual>
+  </div>`;
 
 const meta: Meta = {
   title: 'Components/brandvisual',
@@ -20,9 +26,10 @@ const meta: Meta = {
   render,
   argTypes: {
     name: {
-      control: 'text',
+      control: 'select',
+      options: Object.keys(brandVisualsManifest),
     },
-    altText: {
+    'alt-text': {
       control: 'text',
     },
     ...hideControls(['brandVisualData']),
@@ -36,6 +43,21 @@ export default meta;
 export const Example: StoryObj = {
   args: {
     name: 'momentum-design-logo-dark-color-vertical',
+    'alt-text': 'Momentum design vertical logo',
     style: 'display: block; height: 100px; width: 100px;',
+  },
+};
+
+export const BrandVisualImage: StoryObj = {
+  args: {
+    name: 'device-ip-phone-sixninezeroone',
+    'alt-text': 'Momentum design device ip phone',
+  },
+};
+
+export const BrandVisualSvg: StoryObj = {
+  args: {
+    name: 'webex-app-icon-color-container',
+    'alt-text': 'Webex app icon color logo',
   },
 };
