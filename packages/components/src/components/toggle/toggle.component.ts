bd79bf98cc795c66fa9625729a7e6f37f96d334d
Deeya <dishuupadhyay01@gmail.com>
2025-12-19 14:48:36 +0530

    address review comments

--- packages/components/src/components/toggle/toggle.component.ts
+++ packages/components/src/components/toggle/toggle.component.ts
@@ -11,7 +11,6 @@ import { ROLE } from '../../utils/roles';
 import FormfieldWrapper from '../formfieldwrapper';
 import { DEFAULTS as FORMFIELD_DEFAULTS } from '../formfieldwrapper/formfieldwrapper.constants';
 import type { ValidationType } from '../formfieldwrapper/formfieldwrapper.types';
-import { Component } from '../../models';
 
 import { DEFAULTS, TOGGLE_SIZE } from './toggle.constants';
 import styles from './toggle.styles';
@@ -260,7 +259,7 @@ class Toggle
 
   public static override styles: Array<CSSResult> = [...FormfieldWrapper.styles, ...styles];
 
-  static override shadowRootOptions = { ...Component.shadowRootOptions, delegatesFocus: true };
+  static override shadowRootOptions = { ...FormfieldWrapper.shadowRootOptions, delegatesFocus: true };
 }
 
 export default Toggle;
