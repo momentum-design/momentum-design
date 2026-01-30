ee2c566433fea3a8e6880cd4e75b6ee84d936456
Jordan Rowan <jrowan645@outlook.com>
2026-01-30 16:38:13 +0000

    fix(link): fix css

--- packages/components/src/components/link/link.styles.ts
+++ packages/components/src/components/link/link.styles.ts
@@ -25,7 +25,7 @@ const styles = css`
     vertical-align: -0.1rem;
   }
 
-  :host([size='large'])::part(anchor) {
+  :host([size='large'])::part(text) {
     font-size: var(--mds-font-apps-body-large-regular-font-size);
     font-weight: var(--mds-font-apps-body-large-regular-font-weight);
     line-height: var(--mds-font-apps-body-large-regular-line-height);
@@ -33,7 +33,7 @@ const styles = css`
     text-transform: var(--mds-font-apps-body-large-regular-text-case);
   }
 
-  :host([size='midsize'])::part(anchor) {
+  :host([size='midsize'])::part(text) {
     font-size: var(--mds-font-apps-body-midsize-regular-font-size);
     font-weight: var(--mds-font-apps-body-midsize-regular-font-weight);
     line-height: var(--mds-font-apps-body-midsize-regular-line-height);
@@ -41,7 +41,7 @@ const styles = css`
     text-transform: var(--mds-font-apps-body-midsize-regular-text-case);
   }
 
-  :host([size='small'])::part(anchor) {
+  :host([size='small'])::part(text) {
     font-size: var(--mds-font-apps-body-small-regular-font-size);
     font-weight: var(--mds-font-apps-body-small-regular-font-weight);
     line-height: var(--mds-font-apps-body-small-regular-line-height);
@@ -49,9 +49,9 @@ const styles = css`
     text-transform: var(--mds-font-apps-body-small-regular-text-case);
   }
 
-  :host([size='large']:hover)::part(anchor),
-  :host([size='large']:active)::part(anchor),
-  :host([size='large'][inline])::part(anchor) {
+  :host([size='large']:hover)::part(text),
+  :host([size='large']:active)::part(text),
+  :host([size='large'][inline])::part(text) {
     font-size: var(--mds-font-apps-body-large-regular-underline-font-size);
     font-weight: var(--mds-font-apps-body-large-regular-underline-font-weight);
     line-height: var(--mds-font-apps-body-large-regular-underline-line-height);
@@ -59,9 +59,9 @@ const styles = css`
     text-transform: var(--mds-font-apps-body-large-regular-underline-text-case);
   }
 
-  :host([size='midsize']:hover)::part(anchor),
-  :host([size='midsize']:active)::part(anchor),
-  :host([size='midsize'][inline])::part(anchor) {
+  :host([size='midsize']:hover)::part(text),
+  :host([size='midsize']:active)::part(text),
+  :host([size='midsize'][inline])::part(text) {
     font-size: var(--mds-font-apps-body-midsize-regular-underline-font-size);
     font-weight: var(--mds-font-apps-body-midsize-regular-underline-font-weight);
     line-height: var(--mds-font-apps-body-midsize-regular-underline-line-height);
@@ -69,9 +69,9 @@ const styles = css`
     text-transform: var(--mds-font-apps-body-midsize-regular-underline-text-case);
   }
 
-  :host([size='small']:hover)::part(anchor),
-  :host([size='small']:active)::part(anchor),
-  :host([size='small'][inline])::part(anchor) {
+  :host([size='small']:hover)::part(text),
+  :host([size='small']:active)::part(text),
+  :host([size='small'][inline])::part(text) {
     font-size: var(--mds-font-apps-body-small-regular-underline-font-size);
     font-weight: var(--mds-font-apps-body-small-regular-underline-font-weight);
     line-height: var(--mds-font-apps-body-small-regular-underline-line-height);
