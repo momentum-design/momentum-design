fb0bb48dc9e5c803f872a38f6196f77f8bf20bb6
Christoph Scheffauer <scheffauerc@gmail.com>
2026-03-11 11:09:36 +0100

    fix: remove letterspacing on themeprovider (#1841)
    * fix: remove letterspacing on themeprovider
    
    * test: e2e popover fix for values
    
    * chore: updated snapshots for component
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- packages/components/src/components/themeprovider/themeprovider.styles.ts
+++ packages/components/src/components/themeprovider/themeprovider.styles.ts
@@ -5,8 +5,6 @@ const styles = css`
     --mdc-themeprovider-color-default: var(--mds-color-theme-text-primary-normal);
     --mdc-themeprovider-font-family: var(--mds-font-family-primary);
     --mdc-themeprovider-font-weight: 400;
-    /* adjusting Inter's letter spacing to better match the old CiscoSans */
-    --mdc-themeprovider-letter-spacing-adjustment: -0.25px;
     /* Adjusting font feature settings for accessibility reasons */
     --mdc-themeprovider-font-feature-settings: 'ss02' on;
 
@@ -17,7 +15,6 @@ const styles = css`
     color: var(--mdc-themeprovider-color-default);
     font-family: var(--mdc-themeprovider-font-family);
     font-weight: var(--mdc-themeprovider-font-weight);
-    letter-spacing: var(--mdc-themeprovider-letter-spacing-adjustment);
 
     font-feature-settings: var(--mdc-themeprovider-font-feature-settings);
   }
