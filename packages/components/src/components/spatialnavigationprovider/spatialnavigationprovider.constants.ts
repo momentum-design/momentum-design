0deeab801cc550c1587268c5d0a11e209e080c6a
Laszlo Vadasz <lvadasz@cisco.com>
2026-06-08 17:14:35 +0100

    fix(spatialnavigation): handle scroll to out of view items
    Breaking change: none

--- packages/components/src/components/spatialnavigationprovider/spatialnavigationprovider.constants.ts
+++ packages/components/src/components/spatialnavigationprovider/spatialnavigationprovider.constants.ts
@@ -6,6 +6,18 @@ export const TAG_NAME = utils.constructTagName('spatialnavigationprovider');
 
 export const SPATIAL_NAVIGATION_DIRECTION_KEYS = ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'];
 
+export const DATA_ATTRIBUTES = {
+  DIRECTION_LEFT: 'data-spatial-left',
+  DIRECTION_RIGHT: 'data-spatial-right',
+  DIRECTION_UP: 'data-spatial-up',
+  DIRECTION_DOWN: 'data-spatial-down',
+  GO_BACK: 'data-spatial-go-back',
+  FOCUSABLE: 'data-spatial-focusable',
+  EXCLUDE: 'data-spatial-exclude',
+  NO_SCROLL: 'data-spatial-noscroll',
+  SCROLL_PARENT: 'data-spatial-scroll-parent',
+};
+
 export const DEFAULTS = {
   SPATIAL_NAVIGATION_KEY_MAPPING: {
     left: 'ArrowLeft',
