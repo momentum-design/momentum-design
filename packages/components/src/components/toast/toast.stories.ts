3c92826e6aeae5ba99e6c2cba00717e9050a5b52
Dominic Carrington <docarrin@cisco.com>
2026-07-22 17:15:14 +0100

    fix: eslint

--- packages/components/src/components/toast/toast.stories.ts
+++ packages/components/src/components/toast/toast.stories.ts
@@ -1,14 +1,13 @@
 import type { Meta, StoryObj, Args } from '@storybook/web-components';
 import { html } from 'lit';
-import { ifDefined } from 'lit/directives/if-defined.js';
 import { action } from 'storybook/actions';
+import { ifDefined } from 'lit/directives/if-defined.js';
+import { createRef, ref } from 'lit/directives/ref.js';
 
 import '.';
 import '../avatar';
 import '../spinner';
 import '../button';
-
-import { createRef, ref } from 'lit/directives/ref.js';
 
 import { classArgType, styleArgType } from '../../../config/storybook/commonArgTypes';
 import { disableControls, hideAllControls, hideControls } from '../../../config/storybook/utils';
