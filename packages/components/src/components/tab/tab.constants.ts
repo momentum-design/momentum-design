5599d5fbd96d7867d6f9c3924010dd93043aff68
tricarro <tricarro@cisco.com>
2026-08-27 15:40:22 -0500

    Updated Tab/Tablist animation.

--- packages/components/src/components/tab/tab.constants.ts
+++ packages/components/src/components/tab/tab.constants.ts
@@ -13,10 +13,21 @@ const TAB_SIZES = {
   28: 28,
 } as const;
 
+const INDICATOR_ORIGIN_VALUES = {
+  INLINE_START: 'inline-start',
+  INLINE_END: 'inline-end',
+  CENTER: 'center',
+} as const;
+
+type IndicatorTransformOrigin = (typeof INDICATOR_ORIGIN_VALUES)[keyof typeof INDICATOR_ORIGIN_VALUES];
+
+const INDICATOR_ORIGIN_ATTRIBUTE = 'data-indicator-origin';
+
 const DEFAULTS = {
   VARIANT: TAB_VARIANTS.PILL,
   ACTIVE: false,
   SIZE: TAB_SIZES[32],
 } as const;
 
-export { DEFAULTS, TAG_NAME, TAB_VARIANTS, TAB_SIZES };
+export { DEFAULTS, INDICATOR_ORIGIN_ATTRIBUTE, INDICATOR_ORIGIN_VALUES, TAG_NAME, TAB_SIZES, TAB_VARIANTS };
+export type { IndicatorTransformOrigin };
