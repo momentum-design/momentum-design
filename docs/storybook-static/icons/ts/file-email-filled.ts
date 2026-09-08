ad2cff5eda2979a1fc7d79123bf7683b1de35076
Momentum Design Automation <automation@momentum.design>
2026-09-08 18:40:54 +0000

    ci(docs): deploy documentation [skip ci] [skip release]

---
+++ docs/storybook-static/icons/ts/file-email-filled.ts
@@ -0,0 +1,6 @@
+import { html } from 'lit';
+
+const icon = () =>
+  html`<svg aria-hidden="true" part="icon" data-name="file-email-filled" xmlns="http://www.w3.org/2000/svg" width="30" height="32" viewBox="0 0 30 32"><path d="M17 2a1 1 0 0 1 1 1v8a3 3 0 0 0 3 3h6a1 1 0 0 1 1 1v10a5 5 0 0 1-5 5H9a5 5 0 0 1-5-5V7a5 5 0 0 1 5-5zM9.5 23.5A2.5 2.5 0 0 0 12 26h8a2.5 2.5 0 0 0 2.5-2.5V18.99l-6.506 3.66L9.5 18.902zM12 16c-.78 0-1.477.358-1.935.919l5.942 3.43 5.978-3.363A2.5 2.5 0 0 0 20 16zm8-13.535a.5.5 0 0 1 .895-.306l6.982 9.035a.5.5 0 0 1-.396.806H21a1 1 0 0 1-1-1z"/></svg>`;
+
+export default icon;
